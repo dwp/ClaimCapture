f8caac37a02fe72fdabf28e4d03574728d4ab1f8
Leon Derks <l.a.derks@gmail.com>
2013-05-30 16:02:05 +0100

    updated constructors

--- c3/app/models/view/example/GenericQuestionGroup.scala
+++ c3/app/models/view/example/GenericQuestionGroup.scala
@@ -1,13 +1,8 @@
 package models.view.example
 
-class GenericQuestionGroup(_name:String, _form:Form) {
+class GenericQuestionGroup(var name:String, var form:Form) {
 
   var answered = false
-  var form:Form = _form;
-
-  def name: String = {
-    _name
-  }
 
   def updateForm(form:Form) = {
     this.form = form
--- c3/app/models/view/example/GenericSection.scala
+++ c3/app/models/view/example/GenericSection.scala
@@ -1,15 +1,5 @@
 package models.view.example
 
-class GenericSection(_name:String, _questionGroups: Seq[GenericQuestionGroup]) {
+class GenericSection(var name:String, var questionGroups: Seq[GenericQuestionGroup]) {
   var complete = false
-
-  def name: String = {
-    _name
-  }
-
-  def questionGroups : Seq[GenericQuestionGroup] = {
-     _questionGroups
-  }
-
-
 }
