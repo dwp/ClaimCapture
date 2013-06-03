9489064b0d92db6795b2712bfe68e7b798ee28d4
Leon Derks <l.a.derks@gmail.com>
2013-06-03 16:49:24 +0100

    removed unused code

--- c3/app/models/view/example/Claim.scala
+++ c3/app/models/view/example/Claim.scala
@@ -30,6 +30,7 @@ object Claim {
     ))
   }
 
+
   def findSectionForClaim(sectionId: String, claim: Claim) = {
     claim.sections.find(section => section.name.equals(sectionId))
   }
@@ -42,19 +43,4 @@ object Claim {
   def findFormForQuestionGroup(sectionId: String, questionGroupId: String, claim: Claim) = {
     findQuestionGroupForSection(sectionId, questionGroupId, claim).get.form
   }
-
-//  def updateFormForQuestionGroup(sectionId: String, questionGroupId: String, form: AbstractForm, claim: Claim) = {
-//    val section = findSectionForClaim(sectionId, claim).get
-//    val nrOfQuestionGroups = section.questionGroups.length
-//    var i = 0
-//    while (i < nrOfQuestionGroups) {
-//      val questionGroup = section.questionGroups(i)
-//
-//      if (questionGroup.label.equals(questionGroupId)) {
-//        section.questionGroups(i).form = form
-//      }
-//
-//      i += 1
-//    }
-//  }
 }
