84b03bd8a5f714abf3cae24441cb991d02f6ae2f
Leon Derks <l.a.derks@gmail.com>
2013-06-07 09:33:39 +0100

    updated Build file with more compiler options

--- c3/project/Build.scala
+++ c3/project/Build.scala
@@ -19,7 +19,7 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     scalaVersion := "2.10.0"
-    , scalacOptions ++= Seq("-feature")
+    , scalacOptions ++= Seq("-deprecated", "-unchecked", "-feature", "-Xlint")
 
   )
 
