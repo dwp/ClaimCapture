7b2ff8f8220d6cd9e3b96400780326ed3e297721
Leon Derks <l.a.derks@gmail.com>
2013-06-13 11:07:50 +0100

    added submission urls map

--- c3/app/utils/AppConfigs.scala
+++ c3/app/utils/AppConfigs.scala
@@ -13,10 +13,25 @@ object AppConfigs {
     , ContactDetails.id -> controllers.routes.AboutYou.contactDetails()
   )
 
+  lazy val submissionMap = Map(Benefits.id -> controllers.routes.CarersAllowance.benefitsSubmit()
+    , Hours.id ->  controllers.routes.CarersAllowance.hoursSubmit()
+    , LivesInGB.id ->  controllers.routes.CarersAllowance.livesInGBSubmit()
+    , Over16.id ->  controllers.routes.CarersAllowance.over16Submit()
+    , YourDetails.id -> controllers.routes.AboutYou.yourDetailsSubmit()
+    , ContactDetails.id -> controllers.routes.AboutYou.contactDetailsSubmit()
+  )
+
   def formUrl(formId:String): Call = {
     urlMap.get(formId) match {
         case Some(call) => call
         case None => controllers.routes.CarersAllowance.benefits()
+    }
+  }
+
+  def formSubmissionUrl(formId:String) = {
+    submissionMap.get(formId) match {
+      case Some(call) => call
+      case None => controllers.routes.CarersAllowance.benefits()
     }
   }
 
