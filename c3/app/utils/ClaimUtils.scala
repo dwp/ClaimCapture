74a6db9b713c51b895aeb792eb89aaab3809cd56
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-06-14 14:50:22 +0100

    Fixed a problem with parsing DayMonthYear

--- c3/app/utils/ClaimUtils.scala
+++ c3/app/utils/ClaimUtils.scala
@@ -7,6 +7,6 @@ object ClaimUtils {
   def sectionId(formId: String) = {
     formId.split('.')(0)
   }
-  val dateFormatGeneration: DateTimeFormatter = DateTimeFormat.forPattern("'DayMonthYear'(dd,M,yyyy)")
+  val dateFormatGeneration: DateTimeFormatter = DateTimeFormat.forPattern("'DayMonthYear'('Some'(dd),'Some'(M),'Some'(yyyy))")
   val dateFormatPrint: DateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy")
 }
