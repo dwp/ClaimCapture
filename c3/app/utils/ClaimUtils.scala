436a3487c853253afbec0a48283338bda590a34c
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-06-14 10:00:22 +0100

    All dates are dynamic right now

--- c3/app/utils/ClaimUtils.scala
+++ c3/app/utils/ClaimUtils.scala
@@ -1,8 +1,12 @@
 package utils
 
+import org.joda.time.format.{DateTimeFormatter, DateTimeFormat}
+
 object ClaimUtils {
 
   def sectionId(formId: String) = {
     formId.split('.')(0)
   }
+  val dateFormatGeneration: DateTimeFormatter = DateTimeFormat.forPattern("'DayMonthYear'(dd,M,yyyy)")
+  val dateFormatPrint: DateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy")
 }
