eba17d569cee7da18b854671d0edd80b2abcd8c0
David Ainslie <dainslie@gmail.com>
2013-06-20 16:58:18 +0100

    Breaks in care view of all breaks - dynamic

--- c3/test/integration/s4_careYouProvide/G11BreaksInCare.scala
+++ c3/test/integration/s4_careYouProvide/G11BreaksInCare.scala
@@ -24,19 +24,26 @@ class G11BreaksInCare extends Specification with Tags {
        browser.title() mustEqual "Breaks in Care - Care You Provide"
      }
 
-     """show 1 break in "break table" upon providing 1 break and choosing to "add" another break""" in new WithBrowser {
+     """show 2 breaks in "break table" upon providing 2 break and choosing to "add" another break""" in new WithBrowser {
        browser.goTo("/careYouProvide/breaksInCare")
 
-       browser.click("#break_start_day option[value='1']")
-       browser.click("#break_start_month option[value='1']")
-       browser.fill("#break_start_year") `with` "2001"
+       def addBreak = {
+         browser.click("#break_start_day option[value='1']")
+         browser.click("#break_start_month option[value='1']")
+         browser.fill("#break_start_year") `with` "2001"
 
-       browser.click("#break_end_day option[value='1']")
-       browser.click("#break_end_month option[value='1']")
-       browser.fill("#break_end_year") `with` "2001"
+         browser.click("#break_end_day option[value='1']")
+         browser.click("#break_end_month option[value='1']")
+         browser.fill("#break_end_year") `with` "2001"
 
-       browser.click("#moreBreaks_yes")
-       browser.submit("button[value='next']")
+         browser.click("#moreBreaks_yes")
+         browser.submit("button[value='next']")
+       }
+
+       addBreak
+       addBreak
+
+       browser.$("#trips table tbody tr").size() mustEqual 2
      }
    } section "integration"
  }
