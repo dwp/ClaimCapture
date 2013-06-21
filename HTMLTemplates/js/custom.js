6680edb3e296e761c0fa088357e12b025f20d0e1
Chris Dennett <chris@chris-dennetts-macbook-pro.local>
2013-06-21 13:58:14 +0100

    Updated JS - content helper

--- HTMLTemplates/js/custom.js
+++ HTMLTemplates/js/custom.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 
 // date picker
 $('.date').each(function() {
-        $(this).datepicker({ dateFormat: 'yy-mm-dd' });
+        $(this).datepicker({ dateFormat: 'dd-mm-yy' });
     });
+    
+// content help    
+$(".helper-more").click(function(){
+        $(".helper-info").slideToggle("slow");
+   	});    
 
 // smooth scroll    
 $('a[href^="#"]').bind('click.smoothscroll',function (e) {
