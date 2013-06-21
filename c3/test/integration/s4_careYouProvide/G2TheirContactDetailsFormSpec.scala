8d74cf59fedf1cf7c48c944d3ceff158ac9f91fe
Leon Derks <l.a.derks@gmail.com>
2013-06-21 13:00:20 +0100

    added tests

--- c3/test/integration/s4_careYouProvide/G2TheirContactDetailsFormSpec.scala
+++ c3/test/integration/s4_careYouProvide/G2TheirContactDetailsFormSpec.scala
@@ -11,8 +11,9 @@ class G2TheirContactDetailsFormSpec extends Specification {
       CareYouProvide.theirContactDetailsForm.bind(
         Map("address.lineOne" -> "lineOne", "address.lineTwo" -> "lineTwo", "address.lineThree" -> "lineThree", "postcode" -> "SE1 6EH", "phoneNumber" -> "020-76542059")
       ).fold(
-        formWithErrors => formWithErrors.errors must equalTo("Valid"),
-        theirContactDetails => { theirContactDetails.address.lineOne must equalTo(Some("lineOne"))
+        formWithErrors => "This mapping should not happen." must equalTo("Error"),
+        theirContactDetails => {
+          theirContactDetails.address.lineOne must equalTo(Some("lineOne"))
           theirContactDetails.address.lineTwo must equalTo(Some("lineTwo"))
           theirContactDetails.address.lineThree must equalTo(Some("lineThree"))
           theirContactDetails.postcode must equalTo(Some("SE1 6EH"))
@@ -32,7 +33,7 @@ class G2TheirContactDetailsFormSpec extends Specification {
 
     "reject an invalid postcode" in {
       CareYouProvide.theirContactDetailsForm.bind(
-        Map("address.lineOne" -> "Walkerstreet", "address.lineTwo" -> "", "address.lineThree" -> "", "postcode" -> "SE 6EH")
+        Map("address.lineOne" -> "lineOne", "address.lineTwo" -> "", "address.lineThree" -> "", "postcode" -> "SE 6EH")
       ).fold(
         formWithErrors => formWithErrors.errors.head.message must equalTo("error.postcode"),
         theirContactDetails => "This mapping should not happen." must equalTo("Valid")
@@ -41,7 +42,7 @@ class G2TheirContactDetailsFormSpec extends Specification {
 
     "reject an invalid phone number" in {
       CareYouProvide.theirContactDetailsForm.bind(
-        Map("address.lineOne" -> "Walkerstreet", "address.lineTwo" -> "", "address.lineThree" -> "", "phoneNumber" -> "abcdef")
+        Map("address.lineOne" -> "lineOne", "address.lineTwo" -> "", "address.lineThree" -> "", "phoneNumber" -> "abcdef")
       ).fold(
         formWithErrors => formWithErrors.errors.head.message must equalTo("error.invalid"),
         theirContactDetails => "This mapping should not happen." must equalTo("Valid")
