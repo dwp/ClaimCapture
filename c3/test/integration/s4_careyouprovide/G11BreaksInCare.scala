92547ae749e03e8e25b5403a7e4bdbc6657c354b
David Ainslie <dainslie@gmail.com>
2013-06-23 22:13:56 +0100

    Deletion is done.

--- c3/test/integration/s4_careyouprovide/G11BreaksInCare.scala
+++ c3/test/integration/s4_careyouprovide/G11BreaksInCare.scala
@@ -2,6 +2,7 @@ package integration.s4_careYouProvide
 
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
+import java.util.concurrent.TimeUnit
 
 class G11BreaksInCare extends Specification with Tags {
    "Breaks in care" should {
@@ -35,7 +36,7 @@ class G11BreaksInCare extends Specification with Tags {
      """show 2 breaks in "break table" upon providing 2 break and choosing to "add" another break""" in new WithBrowser {
        browser.goTo("/careYouProvide/breaksInCare")
 
-       def addBreak = {
+       def break = {
          browser.click("#break_start_day option[value='1']")
          browser.click("#break_start_month option[value='1']")
          browser.fill("#break_start_year") `with` "2001"
@@ -52,10 +53,57 @@ class G11BreaksInCare extends Specification with Tags {
          browser.submit("button[value='next']")
        }
 
-       addBreak
-       addBreak
+       break
+       break
 
        browser.$("#trips table tbody tr").size() mustEqual 2
+     }
+
+     "show zero breaks after creating one and then deleting" in new WithBrowser {
+       browser.goTo("/careYouProvide/breaksInCare")
+
+       browser.click("#break_start_day option[value='1']")
+       browser.click("#break_start_month option[value='1']")
+       browser.fill("#break_start_year") `with` "2001"
+       browser.click("#break_whereYou_location option[value='Holiday']")
+       browser.click("#break_wherePerson_location option[value='Holiday']")
+       browser.click("#moreBreaks_yes")
+
+       browser.submit("button[value='next']")
+       browser.$("tbody tr").size() mustEqual 1
+
+       browser.click("input[value='Delete']")
+       browser.await().atMost(5, TimeUnit.SECONDS).until("tbody tr").hasSize(0)
+     }
+
+     "show two breaks after creating three and then deleting one" in new WithBrowser {
+       browser.goTo("/careYouProvide/breaksInCare")
+
+       def break = {
+         browser.click("#break_start_day option[value='1']")
+         browser.click("#break_start_month option[value='1']")
+         browser.fill("#break_start_year") `with` "2001"
+
+         browser.click("#break_end_day option[value='1']")
+         browser.click("#break_end_month option[value='1']")
+         browser.fill("#break_end_year") `with` "2001"
+
+         browser.click("#break_whereYou_location option[value='Holiday']")
+         browser.click("#break_wherePerson_location option[value='Holiday']")
+
+         browser.click("#moreBreaks_yes")
+
+         browser.submit("button[value='next']")
+       }
+
+       break
+       break
+       break
+       browser.$("tbody tr").size() mustEqual 3
+
+       browser.findFirst("tbody tr input[value='Delete']").click()
+       browser.await().atMost(5, TimeUnit.SECONDS).until("tbody tr").hasSize(2)
+
      }
    } section "integration"
  }
