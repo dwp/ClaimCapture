c945502bc7e71efd323789810c35e9ca7590f0df
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-06-25 16:23:58 +0100

    Added G7 More About The Care

--- c3/app/models/domain/CareYouProvide.scala
+++ c3/app/models/domain/CareYouProvide.scala
@@ -32,6 +32,12 @@ object RepresentativesForPerson {
   val id = s"${CareYouProvide.id}.g6"
 }
 
+case class MoreAboutTheCare(spent35HoursCaring: String,spent35HoursCaringBeforeClaim:String,careStartDate:Option[DayMonthYear],hasSomeonePaidYou: String) extends QuestionGroup(MoreAboutTheCare.id)
+
+object MoreAboutTheCare {
+  val id = s"${CareYouProvide.id}.g7"
+}
+
 case class PreviousCarerPersonalDetails(firstName: Option[String], middleName: Option[String], surname: Option[String],
                                 nationalInsuranceNumber: Option[NationalInsuranceNumber],
                                 dateOfBirth: Option[DayMonthYear]) extends QuestionGroup(PreviousCarerPersonalDetails.id)
