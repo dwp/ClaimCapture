8d6ee1d3ede61c4225cd1e85f9b31e719b6517df
Scott Walker <scott.walker@valtech.co.uk>
2013-06-25 17:12:41 +0100

    S4 G5 test be blank if they live at same address but did not enter one

--- c3/test/integration/s4_care_you_provide/G5PreviousCarerContactDetailsSpec.scala
+++ c3/test/integration/s4_care_you_provide/G5PreviousCarerContactDetailsSpec.scala
@@ -23,7 +23,6 @@ class G5PreviousCarerContactDetailsSpec extends Specification with Tags {
      "be prepopulated if they live at same address" in new WithBrowser {
         Helper.fillYourContactDetails(browser)
         Helper.fillTheirPersonalDetails(browser)
-        Helper.fillTheirContactDetails(browser)
         browser.goTo("/careYouProvide/previousCarerContactDetails")
         browser.find("#address_lineOne").getValue mustEqual("My Address")
         browser.find("#postcode").getValue mustEqual("SE1 6EH")
@@ -35,13 +34,15 @@ class G5PreviousCarerContactDetailsSpec extends Specification with Tags {
        browser.find("#address_lineOne").getValue mustEqual("")
        browser.find("#postcode").getValue mustEqual("")
      }
-/*
+
      "be blank if they live at same address but did not enter one" in new WithBrowser {
-       Helper.fillTheirPersonalDetails(browser)
+       Helper.fillYourContactDetails(browser)
+       Helper.fillTheirContactDetails(browser)
+       browser.goTo("/careYouProvide/previousCarerContactDetails")
        browser.find("#address_lineOne").getValue mustEqual("")
        browser.find("#postcode").getValue mustEqual("")
      }
-
+/*
      "navigate back to Their Personal Details" in new WithBrowser {
        browser.goTo("/careYouProvide/theirContactDetails")
        browser.click("#backButton")
