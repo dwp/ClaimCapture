b0bd380f07532d47dd4bf4c9df54752bc5de5818
Leon Derks <l.a.derks@gmail.com>
2013-06-25 15:24:12 +0100

    fixed emtpy li class and restructured controller tests

---
+++ c3/test/controllers/s4_care_you_provide/G6RepresentativesForThePersonSpec.scala
@@ -0,0 +1,37 @@
+package controllers.s4_care_you_provide
+
+import org.specs2.mutable.Specification
+import org.specs2.mock.Mockito
+import play.api.test.{FakeRequest, WithApplication}
+import models.view.Claiming
+import play.api.test.Helpers._
+
+class G6RepresentativesForThePersonSpec extends Specification with Mockito {
+
+  val representativesForThePersonInput = Seq("actForPerson" -> "no", "someoneElseActForPerson" -> "no")
+
+  "Representatives for the person" should {
+
+    """present "Representatives for the person you care for""" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+
+      val result = controllers.CareYouProvide.representativesForPerson(request)
+      status(result) mustEqual OK
+    }
+
+    "fail submit for no input" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+
+      val result = controllers.CareYouProvide.representativesForPersonSubmit(request)
+      status(result) mustEqual BAD_REQUEST
+    }
+
+    "success for minimal input without optional fields" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey).withFormUrlEncodedBody(representativesForThePersonInput:_*)
+
+      val result = controllers.CareYouProvide.representativesForPersonSubmit(request)
+      status(result) mustEqual SEE_OTHER
+    }
+  }
+
+}
