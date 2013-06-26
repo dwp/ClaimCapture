e8d4d0778509cf798c70009295f6e9d03e0a4313
Phil Barton <philbart@mac.com>
2013-06-26 17:06:08 +0100

    txn id realistic

--- c3/app/controllers/Submission.scala
+++ c3/app/controllers/Submission.scala
@@ -5,22 +5,36 @@ import models.view._
 import services.submission.{ClaimSubmission, ClaimSubmissionService}
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
-import play.api.Logger
+import play.api.{http, Logger}
 
 object Submission extends Controller with CachedClaim {
   def submit = claiming {
     implicit claim => implicit request =>
       Async {
-        val claimXml = ClaimSubmission(claim).buildDwpClaim
+        val claimXml = ClaimSubmission(claim, "TY6TV9G").buildDwpClaim
         Logger.debug(s"Claim submitting transactionId : ${claimXml \\ "DWPCAClaim" \ "@id" toString()}")
         ClaimSubmissionService.submitClaim(claimXml).map(
           response => {
-            println(response.status)
-            Ok(response.xml.toString())
+            // What we'll really do with the response is redirect to relevant page
+            response.status match {
+              case http.Status.ACCEPTED =>
+                Ok("ACCEPTED")
+              case http.Status.BAD_REQUEST =>
+                Ok("BAD_REQUEST")
+              case http.Status.REQUEST_TIMEOUT =>
+                Ok("REQUEST_TIMEOUT")
+              case http.Status.INTERNAL_SERVER_ERROR =>
+                Ok("INTERNAL_SERVER_ERROR")
+              case _ =>
+                Ok(s"Unexpected response ! ${response.status}")
+            }
           }
         ).recover {
-          case e: java.net.ConnectException => {
-            Ok(e.getMessage)
+          case e : java.net.ConnectException => {
+            ServiceUnavailable(e.getMessage)
+          }
+          case e : java.lang.Exception  => {
+            ServiceUnavailable(e.getMessage)
           }
         }
       }
