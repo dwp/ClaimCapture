e4d8bd9cd3b59cc6ef8e457547335d7b22ee9607
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-06-27 16:44:15 +0100

    Added carers repo. Added carers repo XML Validator dependency

--- c3/project/Build.scala
+++ c3/project/Build.scala
@@ -12,14 +12,17 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc()
+    "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc(),
+  "com.dwp.carers" % "carersXMLValidation" % "0.2"
   )
 
   var sO: Seq[Project.Setting[_]] = Seq(scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-Xlint"))
 
   var sV: Seq[Project.Setting[_]] = Seq(scalaVersion := "2.10.0")
 
-  var appSettings: Seq[Project.Setting[_]] = SassPlugin.sassSettings ++ sV ++ sO
+  var sR: Seq[Project.Setting[_]] = Seq(resolvers += "Carers repo" at "http://build.3cbeta.co.uk:8080/artifactory/repo/")
+
+  var appSettings: Seq[Project.Setting[_]] = SassPlugin.sassSettings ++ sV ++ sO ++ sR
 
   val main = play.Project(appName, appVersion, appDependencies).settings(appSettings: _*)
 }
