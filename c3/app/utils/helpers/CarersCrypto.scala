3cd7f87f41e092ad82e5c6b5c33d0bfee3d4df0c
Phil Barton <philbart@mac.com>
2013-06-28 13:05:35 +0100

    change crypto generation flags

--- c3/app/utils/helpers/CarersCrypto.scala
+++ c3/app/utils/helpers/CarersCrypto.scala
@@ -1,16 +1,27 @@
 package utils.helpers
 
-import play.api.{Logger, Play}
+import play.Configuration
+import java.util.UUID
 
 object CarersCrypto {
 
   def encryptAES(v: String) = {
-
-    Play.current.mode match{
-      case play.api.Mode.Dev => v
-      case play.api.Mode.Test => v
-      case _ => play.api.libs.Crypto.encryptAES(v)
-
+    val encrypt = Configuration.root().getBoolean("encryptFields", false)
+    val staticSecret = Configuration.root().getBoolean("staticSecret", false)
+    if (encrypt) {
+      if (staticSecret) {
+        play.api.libs.Crypto.encryptAES(v, "1234567890123456")
+      } else {
+        play.api.libs.Crypto.encryptAES(v, secretKey.substring(0,16))
+      }
+    } else {
+      v
     }
   }
+
+  def generateKey: String = {
+    UUID.randomUUID().toString
+  }
+
+  val secretKey : String = generateKey
 }
