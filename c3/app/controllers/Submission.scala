baf48bc00a721a306b3b60fae8e47b0c85492493
Phil Barton <philbart@mac.com>
2013-06-28 08:51:04 +0100

    try to format xml

--- c3/app/controllers/Submission.scala
+++ c3/app/controllers/Submission.scala
@@ -18,7 +18,7 @@ object Submission extends Controller with CachedClaim {
             // What we'll really do with the response is redirect to relevant page
             response.status match {
               case http.Status.OK =>
-                Ok(s"OK : ${response.status} : ${response.xml}")
+                Ok(response.xml).withHeaders("Content-Type" -> "application/xhtml+xml")
               case http.Status.BAD_REQUEST =>
                 Ok(s"BAD_REQUEST : ${response.status} : ${response.toString}")
               case http.Status.REQUEST_TIMEOUT =>
