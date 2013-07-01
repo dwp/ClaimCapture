6350f1bc0b44745c85fd5d116302fa1a4b2f2fc9
Chris Dennett <chris@chris-dennetts-macbook-pro.local>
2013-07-01 14:31:13 +0100

    Jquery fix for scroll on error messages

--- c3/public/javascripts/custom.js
+++ c3/public/javascripts/custom.js
@@ -8,13 +8,11 @@ $('.date').each(function() {
 // smooth scroll    
 $('a[href^="#"]').bind('click.smoothscroll',function (e) {
     e.preventDefault();
- 
     var target = this.hash,
         $target = $(target);
- 
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 1200, 'swing', function () {
+    $('html, body').animate({
+        scrollTop: $(target).position().top - 70
+    }, 1000, 'swing', function () {
         window.location.hash = target;
     });
 });    
