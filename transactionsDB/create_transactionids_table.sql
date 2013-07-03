d6c466619c27cef4be74c9aa218162ddd3139397
jmigueis <jorge.migueis@valtech.co.uk>
2013-07-03 13:54:45 +0100

    Scripts for transaction database (PostgreSQL).

---
+++ transactionsDB/create_transactionids_table.sql
@@ -0,0 +1,2 @@
+DROP TABLE transactionids;
+CREATE TABLE transactionids (transaction_id CHARACTER(7) NOT NULL, createdon TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT now() NOT NULL, PRIMARY KEY (transaction_id));
