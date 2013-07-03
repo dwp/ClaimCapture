ff37955987e998015f6acc9fe428652f9b673e96
jmigueis <jorge.migueis@valtech.co.uk>
2013-07-03 17:05:52 +0100

    Read transaction id from datatabase. First version.

---
+++ c3/app/utils/helpers/TransactionId.scala
@@ -0,0 +1,23 @@
+package utils.helpers
+
+import play.api.db.DB
+import play.api.Play.current
+
+/**
+ * Connects to PostgreSQL to get a new unique transaction id.
+ * @author Jorge Migueis
+ *         Date: 03/07/2013
+ */
+object TransactionId {
+
+  def getUniqueTransactionIt(): String = {
+    DB.withConnection { connection =>
+      val statement = connection.prepareCall("select get_new_transaction_id();")
+      statement.execute()
+      val result = statement.getResultSet()
+      result.next
+      result.getString("get_new_transaction_id")
+    }
+  }
+
+}
