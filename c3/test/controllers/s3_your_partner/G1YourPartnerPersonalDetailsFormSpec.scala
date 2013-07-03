6998d2b473a9aec9cb766708893175bb3dfe95fa
Scott Walker <scott.walker@valtech.co.uk>
2013-07-03 09:52:24 +0100

    G1YourPartnerPersonalDetailsFormSpec test form number of mandatory fields

--- c3/test/controllers/s3_your_partner/G1YourPartnerPersonalDetailsFormSpec.scala
+++ c3/test/controllers/s3_your_partner/G1YourPartnerPersonalDetailsFormSpec.scala
@@ -57,6 +57,43 @@ class G1YourPartnerPersonalDetailsFormSpec extends Specification {
         }
       )
     }
+    /*
+    "reject too long firstName, middleName or surname" in {
+      G1YourPartnerPersonalDetails.form.bind(
+        Map("title" -> "Mr",
+          "firstName" -> "CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS",
+          "middleName" -> "CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS",
+          "surname" -> "CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS,CHARACTERS",
+          "dateOfBirth.day" -> "1",
+          "dateOfBirth.month" -> "1",
+          "dateOfBirth.year" -> "1980",
+          "liveAtSameAddress" -> "yes"
+        )
+      ).fold(
+        formWithErrors => {
+          formWithErrors.errors(0).message must equalTo("error.maxLength")
+          formWithErrors.errors(1).message must equalTo("error.maxLength")
+          formWithErrors.errors(2).message must equalTo("error.maxLength")
+        },
+        theirPersonalDetails => "This mapping should not happen." must equalTo("Valid")
+      )
+    }
+*/
+    "have 5 mandatory fields" in {
+      G1YourPartnerPersonalDetails.form.bind(
+        Map("middleName" -> "middle name is optional")
+      ).fold(
+        formWithErrors => {
+          formWithErrors.errors.length must equalTo(5)
+          formWithErrors.errors(0).message must equalTo("error.required")
+          formWithErrors.errors(1).message must equalTo("error.required")
+          formWithErrors.errors(2).message must equalTo("error.required")
+          formWithErrors.errors(3).message must equalTo("error.required")
+          formWithErrors.errors(4).message must equalTo("error.required")
+        },
+        theirPersonalDetails => "This mapping should not happen." must equalTo("Valid")
+      )
+    }
   }
 
 }
