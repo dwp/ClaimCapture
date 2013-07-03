78a2be3047b2050901db3fc7cdd567cb17bf295d
Scott Walker <scott.walker@valtech.co.uk>
2013-07-03 12:43:26 +0100

    S3G1 created integration spec

---
+++ c3/test/controllers/s3_your_partner/G1YourPartnerPersonalDetailsIntegrationSpec.scala
@@ -0,0 +1,39 @@
+package controllers.s3_your_partner
+
+import org.specs2.mutable.{Tags, Specification}
+import play.api.test.WithBrowser
+import controllers.FormHelper
+
+class G1YourPartnerPersonalDetailsIntegrationSpec extends Specification with Tags {
+
+  "Your Partner Personal Details" should {
+    "be presented" in new WithBrowser {
+      browser.goTo("/yourPartner/personalDetails")
+      browser.title mustEqual "Personal Details - Your Partner"
+    }
+/*
+    "contain errors on invalid submission" in new WithBrowser {
+      browser.goTo("/careYouProvide/theirPersonalDetails")
+      browser.title mustEqual "Their Personal Details - Care You Provide"
+      browser.submit("button[type='submit']")
+
+      browser.find("div[class=validation-summary] ol li").size mustEqual 5
+    }
+
+    "navigate to next page on valid submission" in new WithBrowser {
+      FormHelper.fillTheirPersonalDetails(browser)
+      browser.title mustEqual "Their Contact Details - Care You Provide"
+    }
+
+    "navigate back to About You - Completed" in new WithBrowser {
+      browser.goTo("/careYouProvide/theirPersonalDetails")
+      browser.click(".form-steps a")
+      browser.title mustEqual "Completion - About You"
+    }
+
+    "contain the completed forms" in new WithBrowser {
+      FormHelper.fillTheirPersonalDetails(browser)
+      browser.find("div[class=completed] ul li").size() mustEqual 1
+    }*/
+  } section "integration"
+}
