8cb95d894be3d51f2f8248f8069520bea92db7dd
Scott Walker <scott.walker@valtech.co.uk>
2013-07-04 15:54:39 +0100

    Refactored breadcrumbs for readablility. Fixed up test expected values to use strings (reflection looks hard to do)

--- c3/app/models/domain/Breadcrumbs.scala
+++ c3/app/models/domain/Breadcrumbs.scala
@@ -1,14 +1,21 @@
 package models.domain
 
-case class Breadcrumbs(activeSection: String)
-{
-  private val allSections: Seq[String] = Seq(CarersAllowance.id, AboutYou.id, YourPartner.id, CareYouProvide.id) 
-  
+case class Breadcrumbs(currentSectionId: String) {
+  private case class Crumb(val id: String, val name: String)
+
+  private val allSections: Seq[Crumb] = Seq(
+    Crumb(CarersAllowance.id, "CarersAllowance"),
+    Crumb(AboutYou.id, "AboutYou"),
+    Crumb(YourPartner.id, "YourPartner"),
+    Crumb(CareYouProvide.id, "CareYouProvide"))
+
   val completedSections: Seq[String] = {
-    allSections.takeWhile(_ < activeSection)
+    allSections.takeWhile(_.id < currentSectionId).map(_.name)
   }
 
   val futureSections: Seq[String] = {
-    allSections.dropWhile(_ <= activeSection)
+    allSections.dropWhile(_.id <= currentSectionId).map(_.name)
   }
+  
+  val activeSection = allSections.find(_.id ==  currentSectionId).get.name
 }
--- c3/test/models/domain/BreadcrumbsSpec.scala
+++ c3/test/models/domain/BreadcrumbsSpec.scala
@@ -6,26 +6,26 @@ import org.specs2.execute.PendingUntilFixed
 class BreadcrumbsSpec extends Specification with PendingUntilFixed {
   "Breadcrumbs" should {
     "not display completed sections when in the first section" in {
-      val breadcrumbs = Breadcrumbs(activeSection = CarersAllowance.id)
+      val breadcrumbs = Breadcrumbs(currentSectionId = CarersAllowance.id)
 
       breadcrumbs.completedSections mustEqual (Nil)
-      breadcrumbs.activeSection mustEqual CarersAllowance.id
-      breadcrumbs.futureSections mustEqual (Seq(AboutYou.id, YourPartner.id, CareYouProvide.id))
+      breadcrumbs.activeSection mustEqual "CarersAllowance"
+      breadcrumbs.futureSections mustEqual (Seq("AboutYou", "YourPartner", "CareYouProvide"))
     }
         
     "display completed and future sections when in the middle section" in {
-      val breadcrumbs = Breadcrumbs(activeSection = AboutYou.id)
+      val breadcrumbs = Breadcrumbs(currentSectionId = AboutYou.id)
 
-      breadcrumbs.completedSections mustEqual (Seq(CarersAllowance.id))
-      breadcrumbs.activeSection mustEqual AboutYou.id
-      breadcrumbs.futureSections mustEqual (Seq(YourPartner.id, CareYouProvide.id))
+      breadcrumbs.completedSections mustEqual (Seq("CarersAllowance"))
+      breadcrumbs.activeSection mustEqual "AboutYou"
+      breadcrumbs.futureSections mustEqual (Seq("YourPartner", "CareYouProvide"))
     }
     
     "not display future sections when in the last section" in {
-      val breadcrumbs = Breadcrumbs(activeSection = CareYouProvide.id)
+      val breadcrumbs = Breadcrumbs(currentSectionId = CareYouProvide.id)
 
-      breadcrumbs.completedSections mustEqual (Seq(CarersAllowance.id, AboutYou.id, YourPartner.id))
-      breadcrumbs.activeSection mustEqual CareYouProvide.id
+      breadcrumbs.completedSections mustEqual (Seq("CarersAllowance", "AboutYou", "YourPartner"))
+      breadcrumbs.activeSection mustEqual "CareYouProvide"
       breadcrumbs.futureSections mustEqual (Nil)
     }
   }
