6cbc30240c3110f2d939e8de90e8f0c772048762
Scott Walker <scott.walker@valtech.co.uk>
2013-07-05 12:25:55 +0100

    Pending tests for dynamic text going from S2 to either S3 or S4

--- c3/test/controllers/s2_about_you/G8CompletedIntegrationSpec.scala
+++ c3/test/controllers/s2_about_you/G8CompletedIntegrationSpec.scala
@@ -12,6 +12,30 @@ class G8CompletedIntegrationSpec extends Specification with Tags {
       browser.goTo("/aboutyou/completed")
       browser.title mustEqual "Completion - About You"
     }
+    
+    """show the text "Continue to Partner / Spouse" on the submit button when next section is "Your Partner"""" in new WithBrowser {
+      FormHelper.fillYourDetails(browser)
+      FormHelper.fillYourContactDetails(browser)
+      FormHelper.fillClaimDate(browser)
+      FormHelper.fillMoreAboutYou(browser)
+      FormHelper.fillEmployment(browser)
+      FormHelper.fillPropertyAndRent(browser)
+      
+      pending("TODO test submit button text")
+      browser.find("#submit").getValue mustEqual "Continue to Partner / Spouse"
+    }
+    
+    """show the text "Continue to Care You Provide" on the submit button when next section is "Care You Provide"""" in new WithBrowser {
+      FormHelper.fillYourDetails(browser)
+      FormHelper.fillYourContactDetails(browser)
+      FormHelper.fillClaimDate(browser)
+      FormHelper.fillMoreAboutYouNotHadPartnerSinceClaimDate(browser)
+      FormHelper.fillEmployment(browser)
+      FormHelper.fillPropertyAndRent(browser)
+      
+      pending("TODO test submit button text")
+      browser.find("#submit").getValue mustEqual "Continue to Care You Provide"
+    }
 
     """be submitted to "Personal Details - Your Partner" page when they have had a partner/spouse at any time since the claim date""" in new WithBrowser {
       FormHelper.fillYourDetails(browser)
@@ -21,8 +45,8 @@ class G8CompletedIntegrationSpec extends Specification with Tags {
       FormHelper.fillEmployment(browser)
       FormHelper.fillPropertyAndRent(browser)
       
-      browser.goTo("/aboutyou/completed")
       browser.submit("button[type='submit']")
+      
       browser.title mustEqual "Personal Details - Your Partner"  
     }
 
@@ -34,8 +58,8 @@ class G8CompletedIntegrationSpec extends Specification with Tags {
       FormHelper.fillEmployment(browser)
       FormHelper.fillPropertyAndRent(browser)
       
-      browser.goTo("/aboutyou/completed")
       browser.submit("button[type='submit']")
+      
       browser.title mustEqual "Their Personal Details - Care You Provide"
     }
     
