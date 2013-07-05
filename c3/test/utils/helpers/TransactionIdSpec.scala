97ec4a5eb5363fbd216ccf21ac7a2d1575d5b28a
jmigueis <jorge.migueis@valtech.co.uk>
2013-07-05 11:11:20 +0100

    added test transaction id with tag database. Updated create_users.sql for first time installation.

---
+++ c3/test/utils/helpers/TransactionIdSpec.scala
@@ -0,0 +1,24 @@
+package utils.helpers
+
+import org.specs2.mutable.{Tags,Specification}
+import play.api.test.{FakeApplication, WithApplication}
+
+/**
+ * To change this template use Preferences | File and Code Templates.
+ * @author Jorge Migueis
+ *         Date: 03/07/2013
+ */
+class TransactionIdSpec extends Specification with Tags {
+
+  "The transaction id" should {
+
+    "Should be 7 characters long" in new WithApplication(FakeApplication(additionalConfiguration=Map("db.carers.driver" ->"org.postgresql.Driver",
+      "db.carers.url" -> """jdbc:postgresql://localhost:5432/carerstransactions_db""",
+       "db.carers.user" -> "carers_c3",
+      "db.carers.password" -> "claimant23"))) {
+      TransactionId.getUniqueTransactionIt() must have size(7)
+    }
+
+  } section "database"
+
+}
