8ec5b8267890527a838670a77c9c7a66ec0c8f11
Scott Walker <scott.walker@valtech.co.uk>
2013-07-08 15:37:26 +0100

    Started work on customised 404 error page

--- c3/app/Global.scala
+++ c3/app/Global.scala
@@ -1,6 +1,10 @@
 import com.typesafe.config.ConfigFactory
 import java.io.File
 import play.api.{GlobalSettings, Mode, Configuration}
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+
 
 /**
  * play -Dconfig.file=conf/application.test.conf run
@@ -10,5 +14,10 @@ object Global extends GlobalSettings {
     val applicationConf = System.getProperty("config.file", s"application.${mode.toString.toLowerCase}.conf")
     val environmentOverridingConfiguration = configuration ++ Configuration(ConfigFactory.load(applicationConf))
     super.onLoadConfig(environmentOverridingConfiguration, path, classloader, mode)
+  }
+  
+  // 404 - page not found error     http://alvinalexander.com/scala/handling-scala-play-framework-2-404-500-errors
+  override def onHandlerNotFound(request: RequestHeader): Result = {
+    NotFound(views.html.errors.onHandlerNotFound(request))
   }
 }
