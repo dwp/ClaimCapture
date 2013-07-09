200de23a4bab29fb8657f421323f2beb2c8cda3e
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-07-09 18:20:08 +0100

    Removed one printf

---
+++ c3/app/controllers/s7_consent_and_declaration/G4AdditionalInfo.scala
@@ -0,0 +1,44 @@
+package controllers.s7_consent_and_declaration
+
+import play.api.mvc.Controller
+import controllers.Routing
+import models.view.CachedClaim
+import models.domain._
+import play.api.data.Form
+import play.api.data.Forms._
+import scala.Some
+import utils.helpers.CarersForm._
+import models.domain.Claim
+import scala.Some
+
+object G4AdditionalInfo extends Controller with Routing with CachedClaim{
+
+  override val route = AdditionalInfo.id -> controllers.s7_consent_and_declaration.routes.G4AdditionalInfo.present
+
+  val form = Form(
+    mapping(
+      "anythingElse" -> optional(text),
+      "welshCommunication" -> nonEmptyText
+    )(AdditionalInfo.apply)(AdditionalInfo.unapply))
+
+  def completedQuestionGroups(implicit claim: Claim) = claim.completedQuestionGroups(AdditionalInfo)
+
+  def present = claiming {
+    implicit claim => implicit request =>
+
+      val currentForm: Form[AdditionalInfo] = claim.questionGroup(AdditionalInfo) match {
+        case Some(t: AdditionalInfo) => form.fill(t)
+        case _ => form
+      }
+
+      Ok(views.html.s7_consent_and_declaration.g4_additionalInfo(currentForm,completedQuestionGroups))
+  }
+
+  def submit = claiming { implicit claim => implicit request =>
+    form.bindEncrypted.fold(
+      formWithErrors => BadRequest(views.html.s7_consent_and_declaration.g4_additionalInfo(formWithErrors,completedQuestionGroups)),
+      additionalInfo => claim.update(additionalInfo) -> Redirect(routes.G1Consent.present()))
+  }
+
+
+}
