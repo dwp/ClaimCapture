619166a968e6f25d3d6fab57b4b2462695db7876
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-07-10 10:49:48 +0100

    Template for easying the retrieval of the label

--- c3/project/Build.scala
+++ c3/project/Build.scala
@@ -24,6 +24,7 @@ object ApplicationBuild extends Build {
 
   var sTest: Seq[Project.Setting[_]] = Seq()
 
+  print()
   if (System.getProperty("include") != null ){
     sTest = Seq(testOptions in Test += Tests.Argument("include", System.getProperty("include")))
   }
