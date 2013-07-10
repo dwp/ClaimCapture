c8729fbbc229dc46f0e2a1d9bd4d303acc2257f2
Scott Walker <scott.walker@valtech.co.uk>
2013-07-10 15:48:58 +0100

    According to the dwp-ca-claim-v1_10.xsd YourParner has a minOccurs=0 so we have to handle the entire section being optional if the user does not have a partner

--- c3/app/services/submission/YourPartnerSubmission.scala
+++ c3/app/services/submission/YourPartnerSubmission.scala
@@ -9,38 +9,47 @@ object YourPartnerSubmission {
   private def questionGroup[T](claim: Claim, questionGroup: QuestionGroup) = {
     claim.questionGroup(questionGroup).asInstanceOf[Option[T]]
   }
-  
-  def buildYourPartner(claim: Claim) = {
-    // TODO [SKW] Check which of the below are optional.
-    val yourPartnerPersonalDetails = getQuestionGroup[YourPartnerPersonalDetails](claim, YourPartnerPersonalDetails)
-    val yourPartnerContactDetails = getQuestionGroup[YourPartnerContactDetails](claim, YourPartnerContactDetails)
-    val moreAboutYourPartner = getQuestionGroup[MoreAboutYourPartner](claim, MoreAboutYourPartner)
-    val personYouCareFor = questionGroup[PersonYouCareFor](claim, PersonYouCareFor) // New to schema
-    YourPartner(yourPartnerPersonalDetails, yourPartnerContactDetails, moreAboutYourPartner, personYouCareFor)
+
+  def buildYourPartner(claim: Claim): Option[YourPartner] = {
+    claim.isSectionVisible(models.domain.YourPartner.id) match {
+      case true => {
+        val yourPartnerPersonalDetails = getQuestionGroup[YourPartnerPersonalDetails](claim, YourPartnerPersonalDetails)
+        val yourPartnerContactDetails = getQuestionGroup[YourPartnerContactDetails](claim, YourPartnerContactDetails)
+        val moreAboutYourPartner = getQuestionGroup[MoreAboutYourPartner](claim, MoreAboutYourPartner)
+        val personYouCareFor = questionGroup[PersonYouCareFor](claim, PersonYouCareFor) // New to schema
+        Some(YourPartner(yourPartnerPersonalDetails, yourPartnerContactDetails, moreAboutYourPartner, personYouCareFor))
+      }
+      case false => None
+    }
   }
 
-  def buildClaimant(yourPartner: YourPartner) = {
-      <Partner> 
-        <NationalityPartner>{yourPartner.yourPartnerPersonalDetails.nationality.getOrElse("")}</NationalityPartner>
-        <Surname>{yourPartner.yourPartnerPersonalDetails.surname}</Surname>
-        <OtherNames>{s"${yourPartner.yourPartnerPersonalDetails.firstName} ${yourPartner.yourPartnerPersonalDetails.middleName.getOrElse("")}"}</OtherNames>
-        <OtherSurnames/>
-        <Title>{yourPartner.yourPartnerPersonalDetails.title}</Title>
-        <DateOfBirth>{yourPartner.yourPartnerPersonalDetails.dateOfBirth.toXmlString}</DateOfBirth>
-        <NationalInsuranceNumber>{if(yourPartner.yourPartnerPersonalDetails.nationalInsuranceNumber.isDefined) yourPartner.yourPartnerPersonalDetails.nationalInsuranceNumber.get.toXmlString else ""}</NationalInsuranceNumber>
-        <Address>
-          <gds:Line>{if(yourPartner.yourPartnerContactDetails.address.isDefined) yourPartner.yourPartnerContactDetails.address.get.lineOne.getOrElse("") else ""}</gds:Line>
-          <gds:Line>{if(yourPartner.yourPartnerContactDetails.address.isDefined) yourPartner.yourPartnerContactDetails.address.get.lineTwo.getOrElse("") else ""}</gds:Line>
-          <gds:Line>{if(yourPartner.yourPartnerContactDetails.address.isDefined) yourPartner.yourPartnerContactDetails.address.get.lineThree.getOrElse("") else ""}</gds:Line>
-          <gds:PostCode>{if(yourPartner.yourPartnerContactDetails.address.isDefined)yourPartner.yourPartnerContactDetails.postcode.getOrElse("") else ""}</gds:PostCode>
-        </Address>
-        <ConfirmAddress>yes</ConfirmAddress> <!-- Always default to yes -->
-        <RelationshipStatus>
-          <JoinedHouseholdAfterDateOfClaim>{if(yourPartner.moreAboutYourPartner.dateStartedLivingTogether.isDefined) "yes" else "no"}</JoinedHouseholdAfterDateOfClaim>
-          <JoinedHouseholdDate>{if(yourPartner.moreAboutYourPartner.dateStartedLivingTogether.isDefined) yourPartner.moreAboutYourPartner.dateStartedLivingTogether.get.toXmlString else ""}</JoinedHouseholdDate>
-          <SeparatedFromPartner>{yourPartner.moreAboutYourPartner.separated.answer}</SeparatedFromPartner>
-          <SeparationDate>{if(yourPartner.moreAboutYourPartner.separated.answer == "yes") yourPartner.moreAboutYourPartner.separated.date.getOrElse("") else ""}</SeparationDate>
-        </RelationshipStatus>
-      </Partner>
+  def buildClaimant(yourPartner: Option[YourPartner]) = {
+    yourPartner match {
+      case Some(y) => {
+        <Partner>
+          <NationalityPartner>{ y.yourPartnerPersonalDetails.nationality.getOrElse("") }</NationalityPartner>
+          <Surname>{ y.yourPartnerPersonalDetails.surname }</Surname>
+          <OtherNames>{ s"${y.yourPartnerPersonalDetails.firstName} ${y.yourPartnerPersonalDetails.middleName.getOrElse("")}" }</OtherNames>
+          <OtherSurnames/>
+          <Title>{ y.yourPartnerPersonalDetails.title }</Title>
+          <DateOfBirth>{ y.yourPartnerPersonalDetails.dateOfBirth.toXmlString }</DateOfBirth>
+          <NationalInsuranceNumber>{ if (y.yourPartnerPersonalDetails.nationalInsuranceNumber.isDefined) y.yourPartnerPersonalDetails.nationalInsuranceNumber.get.toXmlString else "" }</NationalInsuranceNumber>
+          <Address>
+            <gds:Line>{ if (y.yourPartnerContactDetails.address.isDefined) y.yourPartnerContactDetails.address.get.lineOne.getOrElse("") else "" }</gds:Line>
+            <gds:Line>{ if (y.yourPartnerContactDetails.address.isDefined) y.yourPartnerContactDetails.address.get.lineTwo.getOrElse("") else "" }</gds:Line>
+            <gds:Line>{ if (y.yourPartnerContactDetails.address.isDefined) y.yourPartnerContactDetails.address.get.lineThree.getOrElse("") else "" }</gds:Line>
+            <gds:PostCode>{ if (y.yourPartnerContactDetails.address.isDefined) y.yourPartnerContactDetails.postcode.getOrElse("") else "" }</gds:PostCode>
+          </Address>
+          <ConfirmAddress>yes</ConfirmAddress><!-- Always default to yes -->
+          <RelationshipStatus>
+            <JoinedHouseholdAfterDateOfClaim>{ if (y.moreAboutYourPartner.dateStartedLivingTogether.isDefined) "yes" else "no" }</JoinedHouseholdAfterDateOfClaim>
+            <JoinedHouseholdDate>{ if (y.moreAboutYourPartner.dateStartedLivingTogether.isDefined) y.moreAboutYourPartner.dateStartedLivingTogether.get.toXmlString else "" }</JoinedHouseholdDate>
+            <SeparatedFromPartner>{ y.moreAboutYourPartner.separated.answer }</SeparatedFromPartner>
+            <SeparationDate>{ if (y.moreAboutYourPartner.separated.answer == "yes") y.moreAboutYourPartner.separated.date.getOrElse("") else "" }</SeparationDate>
+          </RelationshipStatus>
+        </Partner>
+      }
+      case None => {}
+    }
   }
 }
