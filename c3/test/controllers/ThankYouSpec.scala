adac03db952050fe8ca18c5071d852843765d4ea
Scott Walker <scott.walker@valtech.co.uk>
2013-07-10 16:26:01 +0100

    Controller and Integration tests for Thank You page. They're very basic as we don't have an easy way to get to it without having the submission page working

---
+++ c3/test/controllers/ThankYouSpec.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import org.specs2.mutable.Tags
+
+import models.domain.Claiming
+import play.api.test.FakeRequest
+import play.api.test.Helpers.OK
+import play.api.test.Helpers.status
+import play.api.test.WithApplication
+
+class ThankYouSpec extends Specification with Mockito with Tags {
+  "Thank You - Controller" should {
+    "present 'Thank You' page" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+
+      val result = controllers.ThankYou.present(request)
+      status(result) mustEqual OK
+    }
+  } section "unit"
+}
