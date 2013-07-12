a3c164d75eac6eb49472d9fc620f617081c4de13
David Ainslie <dainslie@gmail.com>
2013-07-12 16:02:18 +0100

    Tidy up.

--- c3/test/controllers/s5_time_spent_abroad/TimeSpentAbroadIntegrationSpec.scala
+++ c3/test/controllers/s5_time_spent_abroad/TimeSpentAbroadIntegrationSpec.scala
@@ -3,11 +3,10 @@ package controllers.s5_time_spent_abroad
 import org.specs2.mutable.{Specification, Tags}
 import play.api.test.WithBrowser
 import controllers.BrowserMatchers
-import models.domain.Claiming
 
 class TimeSpentAbroadIntegrationSpec extends Specification with Tags {
   "Time spent abroad" should {
-    """present "completion" and proceed to "pay details".""" in new WithBrowser with BrowserMatchers with Claiming {
+    """present "completion" and proceed to "pay details".""" in new WithBrowser with BrowserMatchers {
       browser.goTo("/timeSpentAbroad/normalResidenceAndCurrentLocation")
       titleMustEqual("Normal Residence and Current Location - Time Spent Abroad")
 
