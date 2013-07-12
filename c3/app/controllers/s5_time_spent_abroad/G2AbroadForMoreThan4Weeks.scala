039ab37b3ae8dccbf6c3a0113c45d1b07db4e975
David Ainslie <dainslie@gmail.com>
2013-07-12 09:31:55 +0100

    Navigation.

--- c3/app/controllers/s5_time_spent_abroad/G2AbroadForMoreThan4Weeks.scala
+++ c3/app/controllers/s5_time_spent_abroad/G2AbroadForMoreThan4Weeks.scala
@@ -7,7 +7,7 @@ import controllers.Routing
 import play.api.data.Form
 import play.api.data.Forms._
 import controllers.Mappings._
-import models.domain.{AbroadForMoreThan4Weeks, Claim, Trips}
+import models.domain.{AbroadForMoreThan4Weeks, Claim}
 import models.yesNo.YesNo
 import TimeSpentAbroad.trips
 
@@ -19,19 +19,21 @@ object G2AbroadForMoreThan4Weeks extends Controller with Routing with CachedClai
       "anyTrips" -> nonEmptyText.verifying(validYesNo)
     )(YesNo.apply)(YesNo.unapply))
 
-  def completedQuestionGroups(implicit claim: Claim) = claim.completedQuestionGroups(Trips)
+  def completedQuestionGroups(implicit claim: Claim) = claim.completedQuestionGroups(AbroadForMoreThan4Weeks)
 
   def present = claiming { implicit claim => implicit request =>
     Ok(views.html.s5_time_spent_abroad.g2_abroad_for_more_than_4_weeks(form, trips, completedQuestionGroups))
   }
 
   def submit = claiming { implicit claim => implicit request =>
+    def next(abroadForMoreThan4Weeks: YesNo) = abroadForMoreThan4Weeks.answer match {
+      case `yes` if trips.fourWeeksTrips.size < 10 => Redirect(routes.G4Trip.fourWeeks())
+      case `yes` => Redirect(routes.G2AbroadForMoreThan4Weeks.present())
+      case _ => Redirect(routes.G3AbroadForMoreThan52Weeks.present())
+    }
+
     form.bindEncrypted.fold(
       formWithErrors => BadRequest(views.html.s5_time_spent_abroad.g2_abroad_for_more_than_4_weeks(formWithErrors, trips, completedQuestionGroups)),
-      abroadForMoreThan4Weeks => abroadForMoreThan4Weeks.answer match {
-        case `yes` if trips.fourWeeksTrips.size < 10 => claim.update(trips) -> Redirect(routes.G4Trip.fourWeeks())
-        case `yes` => claim.update(trips) -> Redirect(routes.G2AbroadForMoreThan4Weeks.present())
-        case _ => claim.update(trips) -> Redirect(routes.G3AbroadForMoreThan52Weeks.present())
-      })
+      abroadForMoreThan4Weeks => claim.update(AbroadForMoreThan4Weeks).update(trips) -> next(abroadForMoreThan4Weeks))
   }
 }
