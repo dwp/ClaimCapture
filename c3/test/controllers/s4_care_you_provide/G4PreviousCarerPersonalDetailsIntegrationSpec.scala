295bdfdd4941fab8a7ed201ba54c73fcdbd09a5e
David Ainslie <dainslie@gmail.com>
2013-07-15 14:49:43 +0100

    Resolved some more potential "timing" issues in specs.

--- c3/test/controllers/s4_care_you_provide/G4PreviousCarerPersonalDetailsIntegrationSpec.scala
+++ c3/test/controllers/s4_care_you_provide/G4PreviousCarerPersonalDetailsIntegrationSpec.scala
@@ -2,32 +2,30 @@ package controllers.s4_care_you_provide
 
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
-import java.util.concurrent.TimeUnit
-import controllers.Formulate
+import controllers.{BrowserMatchers, Formulate}
 
 class G4PreviousCarerPersonalDetailsIntegrationSpec extends Specification with Tags {
-
   "Previous Carer Personal Details" should {
-    "navigate to Previous Carer Details, if anyone else claimed allowance for this person before" in new WithBrowser {
+    "navigate to Previous Carer Details, if anyone else claimed allowance for this person before" in new WithBrowser with BrowserMatchers {
       Formulate.moreAboutThePersonWithClaimedAllowanceBefore(browser)
-      browser.title mustEqual "Details Of The Person Who Claimed Before - Care You Provide"
+      titleMustEqual("Details Of The Person Who Claimed Before - Care You Provide")
     }
 
-    "navigate to Representatives For The Person, if nobody claimed allowance for this person before" in new WithBrowser {
+    "navigate to Representatives For The Person, if nobody claimed allowance for this person before" in new WithBrowser with BrowserMatchers {
       browser.goTo("/careYouProvide/previousCarerPersonalDetails")
-      browser.title mustEqual "Representatives For The Person - Care You Provide"
+      titleMustEqual("Representatives For The Person - Care You Provide")
     }
 
-    "navigate to Previous Carer Contact Details on submission of empty form" in new WithBrowser {
+    "navigate to Previous Carer Contact Details on submission of empty form" in new WithBrowser with BrowserMatchers {
       Formulate.moreAboutThePersonWithClaimedAllowanceBefore(browser)
       browser.submit("button[type='submit']")
-      browser.title mustEqual "Contact Details Of The Person Who Claimed Before - Care You Provide"
+      titleMustEqual("Contact Details Of The Person Who Claimed Before - Care You Provide")
     }
 
-    "navigate to Previous Carer Contact Details on submission of completed form" in new WithBrowser {
+    "navigate to Previous Carer Contact Details on submission of completed form" in new WithBrowser with BrowserMatchers {
       Formulate.moreAboutThePersonWithClaimedAllowanceBefore(browser)
       Formulate.previousCarerPersonalDetails(browser)
-      browser.title mustEqual "Contact Details Of The Person Who Claimed Before - Care You Provide"
+      titleMustEqual("Contact Details Of The Person Who Claimed Before - Care You Provide")
     }
 
     "contain errors on invalid submission" in new WithBrowser {
@@ -37,10 +35,10 @@ class G4PreviousCarerPersonalDetailsIntegrationSpec extends Specification with T
       browser.find("div[class=validation-summary] ol li").size mustEqual 1
     }
 
-    "navigate back to More About The Person You Care For" in new WithBrowser {
+    "navigate back to More About The Person You Care For" in new WithBrowser with BrowserMatchers {
       Formulate.moreAboutThePersonWithClaimedAllowanceBefore(browser)
       browser.click("#backButton")
-      browser.title mustEqual "More About The Person You Care For - Care You Provide"
+      titleMustEqual("More About The Person You Care For - Care You Provide")
     }
 
     "contain the completed forms" in new WithBrowser {
@@ -48,17 +46,19 @@ class G4PreviousCarerPersonalDetailsIntegrationSpec extends Specification with T
       browser.find("div[class=completed] ul li").size() mustEqual 1
     }
 
-    "navigating forward and back presents the same completed question list" in new WithBrowser {
+    "navigating forward and back presents the same completed question list" in new WithBrowser with BrowserMatchers {
       Formulate.theirPersonalDetails(browser)
       Formulate.theirContactDetails(browser)
       Formulate.moreAboutThePersonWithClaimedAllowanceBefore(browser)
+      titleMustEqual("Details Of The Person Who Claimed Before - Care You Provide")
       browser.find("div[class=completed] ul li").size mustEqual 3
-      Formulate.previousCarerPersonalDetails(browser)
-      browser.title mustEqual "Contact Details Of The Person Who Claimed Before - Care You Provide" // DELETE
-      browser.find("div[class=completed] ul li").size mustEqual 4
-      browser.click("#backButton")
 
-      TimeUnit.SECONDS.sleep(2)
+      Formulate.previousCarerPersonalDetails(browser)
+      titleMustEqual("Contact Details Of The Person Who Claimed Before - Care You Provide")
+      browser.find("div[class=completed] ul li").size mustEqual 4
+
+      browser.click("#backButton")
+      titleMustEqual("Details Of The Person Who Claimed Before - Care You Provide")
       browser.find("div[class=completed] ul li").size mustEqual 3
     }
   } section "integration"
