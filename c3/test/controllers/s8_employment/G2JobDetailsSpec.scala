9b3d97d97f297682915b11952934034de2eb54db
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-07-16 17:42:00 +0100

    Added first 2 question groups of Employment

---
+++ c3/test/controllers/s8_employment/G2JobDetailsSpec.scala
@@ -0,0 +1,29 @@
+package controllers.s8_employment
+
+import org.specs2.mutable.{Tags, Specification}
+import play.api.test.{FakeRequest, WithApplication}
+import play.api.test.Helpers._
+import models.domain.Claiming
+
+class G2JobDetailsSpec extends Specification with Tags {
+  "Details about your job" should {
+    "present" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+
+      val result = G2JobDetails.present(request)
+      status(result) mustEqual OK
+    }
+
+    "miss all mandatory data" in new WithApplication with Claiming {
+
+      val request = FakeRequest().withSession("connected" -> claimKey)
+
+      val result = G2JobDetails.submit(request)
+      status(result) mustEqual BAD_REQUEST
+    }
+
+
+
+
+  } section "unit"
+}
