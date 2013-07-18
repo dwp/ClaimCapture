328112ba3e450ea2514b9400abd3b9e9a7f147c4
Phil Barton <philbart@mac.com>
2013-07-18 15:44:00 +0100

    add logback gelf jar

--- c3/project/Build.scala
+++ c3/project/Build.scala
@@ -13,7 +13,8 @@ object ApplicationBuild extends Build {
     jdbc,
     anorm,
     "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc(),
-    "com.dwp.carers" % "carersXMLValidation" % "0.4", "postgresql" % "postgresql" % "9.1-901.jdbc4"
+    "com.dwp.carers" % "carersXMLValidation" % "0.4", "postgresql" % "postgresql" % "9.1-901.jdbc4",
+    "me.moocar" % "logback-gelf" % "0.9.6p2"
   )
 
   var sO: Seq[Project.Setting[_]] = Seq(scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-Xlint"))
