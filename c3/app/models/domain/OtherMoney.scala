3feed112d113d9b64ec8a66643f747eb14523aa6
Leon Derks <l.a.derks@gmail.com>
2013-07-18 15:00:14 +0100

    Person Income 80% done

--- c3/app/models/domain/OtherMoney.scala
+++ c3/app/models/domain/OtherMoney.scala
@@ -14,3 +14,7 @@ object AboutOtherMoney extends QuestionGroup(s"${OtherMoney.id}.g1")
 case class MoneyPaidToSomeoneElseForYou(moneyAddedToBenefitSinceClaimDate: String) extends QuestionGroup(MoneyPaidToSomeoneElseForYou.id)
 
 case object MoneyPaidToSomeoneElseForYou extends QuestionGroup(s"${OtherMoney.id}.g2")
+
+case class PersonContactDetails(address: Option[MultiLineAddress], postcode: Option[String]) extends QuestionGroup(PersonContactDetails.id)
+
+case object PersonContactDetails extends QuestionGroup(s"${OtherMoney.id}.g4")
