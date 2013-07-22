09f2d0469ce3b903107fa3aa2234afddef433a5f
Scott Walker <scott.walker@valtech.co.uk>
2013-07-22 12:13:47 +0100

    S8G5 starting presentation

--- c3/app/controllers/s8_other_money/G5StatutorySickPay.scala
+++ c3/app/controllers/s8_other_money/G5StatutorySickPay.scala
@@ -33,14 +33,13 @@ object G5StatutorySickPay extends Controller with CachedClaim {
   
   def present = claiming { implicit claim =>
     implicit request =>
-      /*OtherMoney.whenVisible(claim)(() => {
+      OtherMoney.whenVisible(claim)(() => {
         val currentForm = claim.questionGroup(StatutorySickPay) match {
           case Some(t: StatutorySickPay) => form.fill(t)
           case _ => form
         }
         Ok(views.html.s8_other_money.g5_statutorySickPay(currentForm, completedQuestionGroups))
-      })*/
-      Ok(<title>Hello, world!</title>)
+      })
   }
 
   def submit = claiming { implicit claim =>
