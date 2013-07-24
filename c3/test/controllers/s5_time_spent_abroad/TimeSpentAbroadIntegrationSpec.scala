0f3e2784dbcff35af6a232cf7e3393fa6a9294f2
Scott Walker <scott.walker@valtech.co.uk>
2013-07-24 17:16:56 +0100

    Dynamic text on submit button for S5 compled

--- c3/test/controllers/s5_time_spent_abroad/TimeSpentAbroadIntegrationSpec.scala
+++ c3/test/controllers/s5_time_spent_abroad/TimeSpentAbroadIntegrationSpec.scala
@@ -3,33 +3,39 @@ package controllers.s5_time_spent_abroad
 import org.specs2.mutable.{Specification, Tags}
 import play.api.test.WithBrowser
 import controllers.BrowserMatchers
+import controllers.Formulate
 
 class TimeSpentAbroadIntegrationSpec extends Specification with Tags {
   "Time spent abroad" should {
     """present "completion" and proceed to 'Education'.""" in new WithBrowser with BrowserMatchers {
-      browser.goTo("/timeSpentAbroad/normalResidenceAndCurrentLocation")
-      titleMustEqual("Normal Residence and Current Location - Time Spent Abroad")
-
-      browser.click("#liveInUK_answer_yes")
-      browser.click("#inGBNow_yes")
-      browser.submit("button[value='next']")
-      titleMustEqual("Abroad for more than 4 weeks - Time Spent Abroad")
-
-      browser.click("#anyTrips_no")
-      browser.submit("button[value='next']")
-      titleMustEqual("Abroad for more than 52 weeks - Time Spent Abroad")
-
-      browser.click("#anyTrips_no")
-      browser.submit("button[value='next']")
-      titleMustEqual("Other EEA State or Switzerland - Time Spent Abroad")
-
-      browser.click("#benefitsFromOtherEEAStateOrSwitzerland_answer_no")
-      browser.click("#workingForOtherEEAStateOrSwitzerland_no")
-      browser.submit("button[value='next']")
+      Formulate.normalResidenceAndCurrentLocation(browser)
+      Formulate.abroadForMoreThan4Weeks(browser)
+      Formulate.abroadForMoreThan52Weeks(browser)
+      Formulate.otherEEAStateOrSwitzerland(browser)
+      
       titleMustEqual("Completion - Time Spent Abroad")
 
       browser.submit("button[value='next']")
       titleMustEqual("Your Course Details - Education")
+    }
+    
+    "navigate to" in {
+      "show the text 'Continue to Education' on the submit button when next section is 'Education'" in new WithBrowser with BrowserMatchers {
+        Formulate.normalResidenceAndCurrentLocation(browser)
+        Formulate.abroadForMoreThan4Weeks(browser)
+        Formulate.abroadForMoreThan52Weeks(browser)
+        Formulate.otherEEAStateOrSwitzerland(browser)
+      
+        browser.find("button[type='submit']").getText mustEqual "Continue to Education"
+      }
+      /*
+      "show the text 'Continue to Other Income' on the submit button when next section is 'Other Income'" in new WithBrowser {
+        Formulate.moreAboutYouNotBeenInEducationSinceClaimDate(browser)
+        Formulate.yourCourseDetails(browser)
+        Formulate.addressOfSchoolCollegeOrUniversity(browser)
+        
+        browser.find("button[type='submit']").getText mustEqual "Continue to Other Income"
+      }.pendingUntilFixed("Need a previous section to call hideSection on Employment")*/
     }
   }
 }
