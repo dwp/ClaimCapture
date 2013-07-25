319666785653894f293581f1da6114f3545ef8b2
Phil Barton <philbart@mac.com>
2013-07-25 16:47:02 +0100

    MDC config

--- c3/app/Global.scala
+++ c3/app/Global.scala
@@ -1,6 +1,9 @@
 import com.typesafe.config.ConfigFactory
 import java.io.File
+import java.net.InetAddress
+import org.slf4j.MDC
 import play.api._
+import play.api.Configuration
 import play.api.mvc._
 import play.api.mvc.Results._
 
@@ -17,6 +20,10 @@ import play.api.mvc.Results._
  */
 object Global extends GlobalSettings {
   override def onLoadConfig(configuration: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
+    MDC.put("httpPort", System.getProperty("http.port"))
+    MDC.put("hostName", InetAddress.getLocalHost().getHostName())
+    MDC.put("envName", System.getProperty("env.name"))
+
     val applicationConf = System.getProperty("config.file", s"application.${mode.toString.toLowerCase}.conf")
     val environmentOverridingConfiguration = configuration ++ Configuration(ConfigFactory.load(applicationConf))
     super.onLoadConfig(environmentOverridingConfiguration, path, classloader, mode)
