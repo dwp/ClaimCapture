6cc697c90a4525454fb031ecb3d8815f939fe503
Phil Barton <philbart@mac.com>
2013-07-29 10:58:30 +0100

    Add referer logic

--- c3/app/Global.scala
+++ c3/app/Global.scala
@@ -9,8 +9,8 @@ import play.api.mvc.Results._
 
 /**
  * Application configuration is in a hierarchy of files:
- *                            application.conf
- *                      /             |            \
+ * application.conf
+ * /             |            \
  * application.prod.conf    application.dev.conf    application.test.conf <- these can override and add to application.conf
  * play test  <- test mode picks up application.test.conf
  * play run   <- dev mode picks up application.dev.conf
@@ -18,19 +18,41 @@ import play.api.mvc.Results._
  * To override and stipulate a particular "conf" e.g.
  * play -Dconfig.file=conf/application.test.conf run
  */
-object Global extends GlobalSettings {
-  override def onLoadConfig(configuration: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
+object Global extends WithFilters(RefererCheck) {
+
+  override def onStart(app: Application) {
     MDC.put("httpPort", Option(System.getProperty("http.port")).getOrElse("Value not set"))
     MDC.put("hostName", Option(InetAddress.getLocalHost.getHostName).getOrElse("Value not set"))
     MDC.put("envName", Option(System.getProperty("env.name")).getOrElse("Value not set"))
+    super.onStart(app)
+  }
 
+  override def onLoadConfig(configuration: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
     val applicationConf = System.getProperty("config.file", s"application.${mode.toString.toLowerCase}.conf")
     val environmentOverridingConfiguration = configuration ++ Configuration(ConfigFactory.load(applicationConf))
     super.onLoadConfig(environmentOverridingConfiguration, path, classloader, mode)
   }
-  
+
   // 404 - page not found error     http://alvinalexander.com/scala/handling-scala-play-framework-2-404-500-errors
   override def onHandlerNotFound(request: RequestHeader): Result = {
     NotFound(views.html.errors.onHandlerNotFound(request))
   }
 }
+
+object RefererCheck extends Filter {
+  override def apply(next: RequestHeader => Result)(request: RequestHeader): Result = {
+    val expectedReferer = Option(play.Configuration.root().getString("referer")).get
+    val host = request.headers.get("Host").getOrElse("No Host in header")
+    val httpReferer = request.headers.get("Referer").getOrElse("No Referer in header")
+
+    if (httpReferer.contains(host) || httpReferer.startsWith(expectedReferer)) {
+      next(request)
+    } else {
+      Logger.debug(s"HTTP Referer : $httpReferer")
+      Logger.debug(s"Conf Referer : $expectedReferer")
+      Logger.debug(s"HTTP Host : $host")
+      Redirect(expectedReferer)
+    }
+  }
+}
+
