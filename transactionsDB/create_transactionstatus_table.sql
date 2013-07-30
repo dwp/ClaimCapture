442cdf7b39e1b86740e63ff8e02f56b366b0511f
Jorge Migueis <jorge.migueis@valtech.co.uk>
2013-07-30 17:03:46 +0100

    DB - created table to handle reconciliation report.

---
+++ transactionsDB/create_transactionstatus_table.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS "public"."transactionstatus" CASCADE;
+CREATE TABLE transactionstatus
+    ( transaction_id CHARACTER(7) NOT NULL, createdon TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT now() NOT NULL, error_status CHARACTER VARYING(10), PRIMARY KEY(transaction_id), CONSTRAINT transaction_fk FOREIGN KEY(transaction_id) REFERENCES transactionids(transaction_id)
+    ) ;
