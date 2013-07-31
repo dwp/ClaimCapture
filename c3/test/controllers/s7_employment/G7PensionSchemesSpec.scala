25ff6a4f5fbb50e69b1824402c54eff0623c5057
David Ainslie <dainslie@gmail.com>
2013-07-31 17:45:23 +0100

    New/Missing spec.

---
+++ c3/test/controllers/s7_employment/G7PensionSchemesSpec.scala
@@ -0,0 +1,59 @@
+package controllers.s7_employment
+
+import org.specs2.mutable.{Tags, Specification}
+import play.api.test.{FakeRequest, WithApplication}
+import play.api.test.Helpers._
+import models.domain._
+import play.api.cache.Cache
+import models.domain.Claim
+
+class G7PensionSchemesSpec extends Specification with Tags {
+  val jobID = "Dummy job ID"
+
+  "Pension schemes" should {
+    "present" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+      val result = G7PensionSchemes.present(jobID)(request)
+      status(result) mustEqual OK
+    }
+
+    """require all mandatory data.""" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+        .withFormUrlEncodedBody("jobID" -> "1")
+
+      val result = G7PensionSchemes.submit(request)
+      status(result) mustEqual BAD_REQUEST
+    }
+
+    """accept all mandatory data.""" in new WithApplication with Claiming {
+      val request = FakeRequest().withSession("connected" -> claimKey)
+        .withFormUrlEncodedBody("jobID" -> "1", "payOccupationalPensionScheme" -> "blah", "payPersonalPensionScheme" -> "blah")
+
+      val result = G7PensionSchemes.submit(request)
+      status(result) mustEqual SEE_OTHER
+    }
+
+    """be added to a (current) job""" in new WithApplication with Claiming {
+      G2JobDetails.submit(FakeRequest().withSession("connected" -> claimKey)
+        withFormUrlEncodedBody(
+        "jobID" -> "1",
+        "employerName" -> "Toys r not us",
+        "finishedThisJob" -> "yes"))
+
+      val result = G7PensionSchemes.submit(FakeRequest().withSession("connected" -> claimKey)
+        .withFormUrlEncodedBody("jobID" -> "1", "payOccupationalPensionScheme" -> "blah", "payPersonalPensionScheme" -> "blah"))
+
+      status(result) mustEqual SEE_OTHER
+
+      val claim = Cache.getAs[Claim](claimKey).get
+
+      claim.questionGroup(Jobs) must beLike {
+        case Some(js: Jobs) => {
+          js.size shouldEqual 1
+
+          js.find(_.jobID == "1") must beLike { case Some(j: Job) => j.questionGroups.size shouldEqual 2 }
+        }
+      }
+    }
+  } section "unit"
+}
