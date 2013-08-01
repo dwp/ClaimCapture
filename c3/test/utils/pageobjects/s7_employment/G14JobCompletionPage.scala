9ca6d49dca7e48c97f574fb586b0274b622baeda
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-08-01 10:43:00 +0100

    Page Objects for Employment

--- c3/test/utils/pageobjects/s7_employment/G14JobCompletionPage.scala
+++ c3/test/utils/pageobjects/s7_employment/G14JobCompletionPage.scala
@@ -6,15 +6,15 @@ import utils.pageobjects.{ClaimScenario, PageContext, Page}
 final class G14JobCompletionPage(browser: TestBrowser, previousPage: Option[Page] = None, iteration: Int) extends Page(browser, G14JobCompletionPage.url, G14JobCompletionPage.title, previousPage) {
   override val url = super.getUrl.replace(":jobID",iteration.toString)
                                           def fillPageWith(theClaim: ClaimScenario) {
-    fillYesNo("#beenEmployed", theClaim.EmploymentBeenEmployed)
+
   }
 
   override def updateIterationNumber: Int = iteration + 1
 }
 
 object G14JobCompletionPage {
-  val title = "Your employment history - Employment"
-  val url  = "/employment/beenEmployed"
+  val title = "Job Completion - Employment"
+  val url  = "/employment/jobCompletion/:jobID"
   def buildPageWith(browser: TestBrowser, previousPage: Option[Page] = None, iteration: Int) = new G14JobCompletionPage(browser,previousPage,iteration)
 }
 
