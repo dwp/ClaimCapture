07b9060a78003e268888e80ebbe55ff6fd0f4203
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-08-09 12:46:50 +0100

    Fixed test

--- c3/test/controllers/s7_employment/G9NecessaryExpensesIntegrationSpec.scala
+++ c3/test/controllers/s7_employment/G9NecessaryExpensesIntegrationSpec.scala
@@ -7,37 +7,61 @@ import utils.pageobjects.s7_employment._
 import utils.pageobjects.ClaimScenario
 
 class G9NecessaryExpensesIntegrationSpec extends Specification with Tags {
-  "Necessary Expenses" should {
+  "Necessary expenses" should {
     "be presented" in new WithBrowser with G8AboutExpensesPageContext {
-      val claim = ClaimScenarioFactory s7Employment()
       page goToThePage()
-      page fillPageWith claim
-      page submitPage()
     }
 
     "contain 1 completed form" in new WithBrowser with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
       page goToThePage()
       page fillPageWith claim
-      val p = page submitPage()
-      p fillPageWith claim
-      p submitPage() match {
-        case p: G9NecessaryExpensesPage => p numberSectionsCompleted()  mustEqual 2
+      page submitPage() match {
+        case p: G9NecessaryExpensesPage => p numberSectionsCompleted()  mustEqual 1
         case _ => ko("Next Page is not of the right type.")
       }
     }
 
-    "be able to navigate back to a completed form" in new WithBrowser  with G8AboutExpensesPageContext {
+    "go to job completion directly" in new WithBrowser with G8AboutExpensesPageContext {
+      val claim = new ClaimScenario
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1= "no"
+      claim.EmploymentDoYouPayAnyoneLookAfterYourChild_1= "no"
+      claim.EmploymentDoYouPayAnyonetoLookAfterPersonYouCareFor_1= "no"
+
+      page goToThePage()
+      page fillPageWith claim
+      page submitPage() must beAnInstanceOf[G14JobCompletionPage]
+    }
+
+    "go to childcare" in new WithBrowser with G8AboutExpensesPageContext {
+      val claim = new ClaimScenario
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1= "no"
+      claim.EmploymentDoYouPayAnyoneLookAfterYourChild_1= "yes"
+      claim.EmploymentDoYouPayAnyonetoLookAfterPersonYouCareFor_1= "yes"
+
+      page goToThePage()
+      page fillPageWith claim
+      page submitPage() must beAnInstanceOf[G10ChildcareExpensesPage]
+    }
+
+    "go to care" in new WithBrowser with G8AboutExpensesPageContext {
+      val claim = new ClaimScenario
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1= "no"
+      claim.EmploymentDoYouPayAnyoneLookAfterYourChild_1= "no"
+      claim.EmploymentDoYouPayAnyonetoLookAfterPersonYouCareFor_1= "yes"
+
+      page goToThePage()
+      page fillPageWith claim
+      page submitPage() must beAnInstanceOf[G12PersonYouCareForExpensesPage]
+    }
+
+    "be able to navigate back to a completed form" in new WithBrowser  with G7PensionSchemesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
       page goToThePage()
       page fillPageWith claim
-      val p = page submitPage()
-      p fillPageWith claim
-      p submitPage() match {
-        case p: G9NecessaryExpensesPage => p numberSectionsCompleted()  mustEqual 2
-        case _ => ko("Next Page is not of the right type.")
-      }
-      p goBack() must beAnInstanceOf[G8AboutExpensesPage]
+      val submitted = page submitPage()
+      val backPage = submitted goBack ()
+      backPage must beAnInstanceOf[G7PensionSchemesPage]
     }
   } section("integration",models.domain.Employed.id)
 }
