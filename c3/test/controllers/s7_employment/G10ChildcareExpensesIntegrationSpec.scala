a2c47054df50428c5f04e07d32c607dc1d56a1ea
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-08-09 14:01:30 +0100

    Employment tests

--- c3/test/controllers/s7_employment/G10ChildcareExpensesIntegrationSpec.scala
+++ c3/test/controllers/s7_employment/G10ChildcareExpensesIntegrationSpec.scala
@@ -4,12 +4,12 @@ import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
 import controllers.ClaimScenarioFactory
 import utils.pageobjects.s7_employment._
-import utils.pageobjects.ClaimScenario
 
-class G9NecessaryExpensesIntegrationSpec extends Specification with Tags {
-  "Necessary Expenses" should {
+class G10ChildcareExpensesIntegrationSpec extends Specification with Tags {
+  "Childcare Expenses" should {
     "be presented" in new WithBrowser with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1="no"
       page goToThePage()
       page fillPageWith claim
       page submitPage()
@@ -17,18 +17,20 @@ class G9NecessaryExpensesIntegrationSpec extends Specification with Tags {
 
     "contain 1 completed form" in new WithBrowser with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1="no"
       page goToThePage()
       page fillPageWith claim
       val p = page submitPage()
       p fillPageWith claim
       p submitPage() match {
-        case p: G9NecessaryExpensesPage => p numberSectionsCompleted()  mustEqual 2
+        case p: G11ChildcareProviderPage => p numberSectionsCompleted()  mustEqual 2
         case _ => ko("Next Page is not of the right type.")
       }
     }
 
     "be able to navigate back to a completed form" in new WithBrowser  with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1="no"
       page goToThePage()
       page fillPageWith claim
       val p = page submitPage()
--- c3/test/controllers/s7_employment/G12PersonYouCareForExpensesIntegrationSpec.scala
+++ c3/test/controllers/s7_employment/G12PersonYouCareForExpensesIntegrationSpec.scala
@@ -4,12 +4,13 @@ import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
 import controllers.ClaimScenarioFactory
 import utils.pageobjects.s7_employment._
-import utils.pageobjects.ClaimScenario
 
-class G9NecessaryExpensesIntegrationSpec extends Specification with Tags {
-  "Necessary Expenses" should {
+class G12PersonYouCareForExpensesIntegrationSpec extends Specification with Tags {
+  "Person you care for expenses" should {
     "be presented" in new WithBrowser with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1="no"
+      claim.EmploymentDoYouPayAnyoneLookAfterYourChild_1="no"
       page goToThePage()
       page fillPageWith claim
       page submitPage()
@@ -17,21 +18,29 @@ class G9NecessaryExpensesIntegrationSpec extends Specification with Tags {
 
     "contain 1 completed form" in new WithBrowser with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1="no"
+      claim.EmploymentDoYouPayAnyoneLookAfterYourChild_1="no"
       page goToThePage()
       page fillPageWith claim
       val p = page submitPage()
       p fillPageWith claim
       p submitPage() match {
-        case p: G9NecessaryExpensesPage => p numberSectionsCompleted()  mustEqual 2
+        case p: G13CareProviderPage => p numberSectionsCompleted()  mustEqual 2
         case _ => ko("Next Page is not of the right type.")
       }
     }
 
     "be able to navigate back to a completed form" in new WithBrowser  with G8AboutExpensesPageContext {
       val claim = ClaimScenarioFactory s7Employment()
+      claim.EmploymentDoYouPayforAnythingNecessaryToDoYourJob_1="no"
+      claim.EmploymentDoYouPayAnyoneLookAfterYourChild_1="no"
       page goToThePage()
       page fillPageWith claim
       val p = page submitPage()
+      p match {
+        case p: G12PersonYouCareForExpensesPage => p numberSectionsCompleted()  mustEqual 1
+        case _ => ko("Next Page is not of the right type.")
+      }
       p goBack() must beAnInstanceOf[G8AboutExpensesPage]
     }
   } section("integration",models.domain.Employed.id)
