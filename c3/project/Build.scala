4e4000dc2dbb7bc4b2916a25379d50e22b1b08bf
Phil Barton <philbart@mac.com>
2013-08-12 17:01:33 +0100

    XML validate update

--- c3/project/Build.scala
+++ c3/project/Build.scala
@@ -12,7 +12,7 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc(),
-    "com.dwp.carers" % "carersXMLValidation" % "0.9",
+    "com.dwp.carers" % "carersXMLValidation" % "0.10",
     "postgresql" % "postgresql" % "9.1-901.jdbc4",
     "me.moocar" % "logback-gelf" % "0.9.6p2",
     "com.google.inject" % "guice" % "3.0",
