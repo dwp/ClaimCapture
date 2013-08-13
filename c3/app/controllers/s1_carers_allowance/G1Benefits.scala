cf08fae84be890b3808fc8e731bf9c541bb1d7a0
Scott Walker <scott.walker@valtech.co.uk>
2013-08-13 12:04:30 +0100

    S1 fix wrong error message when neither yes or no ticked an press next

--- c3/app/controllers/s1_carers_allowance/G1Benefits.scala
+++ c3/app/controllers/s1_carers_allowance/G1Benefits.scala
@@ -9,20 +9,26 @@ import models.domain.Benefits
 import language.reflectiveCalls
 import controllers.Mappings._
 import CarersAllowance._
+import play.api.data.FormError
 
 object G1Benefits extends Controller with CachedClaim {
   val form = Form(
     mapping(
-      "answer" -> nonEmptyText.verifying(validYesNo)
-    )(Benefits.apply)(Benefits.unapply))
+      "answer" -> nonEmptyText.verifying(validYesNo))(Benefits.apply)(Benefits.unapply))
 
-  def present = newClaim { implicit claim => implicit request =>
-    Ok(views.html.s1_carers_allowance.g1_benefits(form.fill(Benefits), completedQuestionGroups(Benefits)))
+  def present = newClaim { implicit claim =>
+    implicit request =>
+      Ok(views.html.s1_carers_allowance.g1_benefits(form.fill(Benefits), completedQuestionGroups(Benefits)))
   }
 
-  def submit = claiming { implicit claim => implicit request =>
-    form.bindEncrypted.fold(
-      formWithErrors => BadRequest(views.html.s1_carers_allowance.g1_benefits(formWithErrors, completedQuestionGroups(Benefits))),
-      f => claim.update(f) -> Redirect(routes.G2Hours.present()))
+  def submit = claiming { implicit claim =>
+    implicit request =>
+      form.bindEncrypted.fold(
+        formWithErrors => {
+          val formWithErrorsUpdate = formWithErrors
+            .replaceError("answer", FormError("benefits.answer", "error.required"))
+          BadRequest(views.html.s1_carers_allowance.g1_benefits(formWithErrorsUpdate, completedQuestionGroups(Benefits)))
+        },
+        f => claim.update(f) -> Redirect(routes.G2Hours.present()))
   }
 }
--- c3/app/controllers/s1_carers_allowance/G4LivesInGB.scala
+++ c3/app/controllers/s1_carers_allowance/G4LivesInGB.scala
@@ -8,20 +8,26 @@ import utils.helpers.CarersForm._
 import models.domain.LivesInGB
 import language.reflectiveCalls
 import controllers.Mappings._
+import play.api.data.FormError
 
 object G4LivesInGB extends Controller with CarersAllowanceRouting with CachedClaim {
   val form = Form(
     mapping(
-      "answer" -> nonEmptyText.verifying(validYesNo)
-    )(LivesInGB.apply)(LivesInGB.unapply))
+      "answer" -> nonEmptyText.verifying(validYesNo))(LivesInGB.apply)(LivesInGB.unapply))
 
-  def present = claiming { implicit claim => implicit request =>
-    Ok(views.html.s1_carers_allowance.g4_livesInGB(form.fill(LivesInGB), completedQuestionGroups(LivesInGB)))
+  def present = claiming { implicit claim =>
+    implicit request =>
+      Ok(views.html.s1_carers_allowance.g4_livesInGB(form.fill(LivesInGB), completedQuestionGroups(LivesInGB)))
   }
 
-  def submit = claiming { implicit claim => implicit request =>
-    form.bindEncrypted.fold(
-      formWithErrors => BadRequest(views.html.s1_carers_allowance.g4_livesInGB(formWithErrors, completedQuestionGroups(LivesInGB))),
-      f => claim.update(f) -> Redirect(routes.CarersAllowance.approve()))
+  def submit = claiming { implicit claim =>
+    implicit request =>
+      form.bindEncrypted.fold(
+        formWithErrors => {
+          val formWithErrorsUpdate = formWithErrors
+            .replaceError("answer", FormError("livesInGB.answer", "error.required"))
+          BadRequest(views.html.s1_carers_allowance.g4_livesInGB(formWithErrorsUpdate, completedQuestionGroups(LivesInGB)))
+        },
+        f => claim.update(f) -> Redirect(routes.CarersAllowance.approve()))
   }
 }
