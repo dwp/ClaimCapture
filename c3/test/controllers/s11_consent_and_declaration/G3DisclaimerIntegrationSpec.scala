c3e1184d4729c434c1a4a08fbfbbc5e1811b04aa
Scott Walker <scott.walker@valtech.co.uk>
2013-08-13 14:53:28 +0100

    DE48 S11G2

--- c3/test/controllers/s11_consent_and_declaration/G3DisclaimerIntegrationSpec.scala
+++ c3/test/controllers/s11_consent_and_declaration/G3DisclaimerIntegrationSpec.scala
@@ -3,36 +3,37 @@ package controllers.s11_consent_and_declaration
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
 import controllers.Formulate
+import controllers.BrowserMatchers
 
 class G3DisclaimerIntegrationSpec extends Specification with Tags {
   "Disclaimer" should {
-    "be presented" in new WithBrowser {
+    "be presented" in new WithBrowser with BrowserMatchers {
       browser.goTo("/consent-and-declaration/disclaimer")
-      browser.title mustEqual "Disclaimer - Consent and Declaration"
+      titleMustEqual("Disclaimer - Consent and Declaration")
     }
 
-    "contain errors on invalid submission" in new WithBrowser {
+    "contain errors on invalid submission" in new WithBrowser with BrowserMatchers {
       browser.goTo("/consent-and-declaration/disclaimer")
-      browser.title mustEqual "Disclaimer - Consent and Declaration"
+      titleMustEqual("Disclaimer - Consent and Declaration")
       browser.submit("button[type='submit']")
 
-      browser.find("div[class=validation-summary] ol li").size mustEqual 1
+      findMustEqualSize("div[class=validation-summary] ol li", 1)
     }
 
-    "navigate to next page on valid submission" in new WithBrowser {
+    "navigate to next page on valid submission" in new WithBrowser with BrowserMatchers {
       Formulate.disclaimer(browser)
-      browser.title mustEqual "Declaration - Consent and Declaration"
+      titleMustEqual("Declaration - Consent and Declaration")
     }
 
-    "navigate back to Consent" in new WithBrowser {
+    "navigate back to Consent" in new WithBrowser with BrowserMatchers {
       Formulate.consent(browser)
       browser.click(".form-steps a")
-      browser.title mustEqual "Consent - Consent and Declaration"
+      titleMustEqual("Consent - Consent and Declaration")
     }
 
-    "contain the completed forms" in new WithBrowser {
+    "contain the completed forms" in new WithBrowser with BrowserMatchers {
       Formulate.disclaimer(browser)
-      browser.find("div[class=completed] ul li").size() mustEqual 1
+      findMustEqualSize("div[class=completed] ul li", 1)
     }
   } section("integration", models.domain.ConsentAndDeclaration.id)
 }
