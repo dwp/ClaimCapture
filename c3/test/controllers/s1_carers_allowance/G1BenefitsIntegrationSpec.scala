5f10d2ecc97ba5b7a4ad6697ac7187e58d433f3d
Leon Derks <l.a.derks@gmail.com>
2013-08-15 15:09:07 +0100

    fix for G1BenefitsIntegrationSpec.scala

--- c3/test/controllers/s1_carers_allowance/G1BenefitsIntegrationSpec.scala
+++ c3/test/controllers/s1_carers_allowance/G1BenefitsIntegrationSpec.scala
@@ -15,7 +15,6 @@ class G1BenefitsIntegrationSpec extends Specification with Tags {
     "contain errors on invalid submission" in {
       "missing mandatory field" in new WithBrowser with G1BenefitsPageContext {
         val claim = new ClaimScenario
-        claim.CanYouGetCarersAllowanceDoesthePersonYouCareforGetOneofTheseBenefits = ""
         page goToThePage()
         page fillPageWith claim
         val pageWithErrors = page.submitPage()
