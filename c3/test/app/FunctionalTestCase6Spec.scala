c5023ce6e322212ceb1465cb02000f7dd84a7ba5
Jorge Migueis <jorge.migueis@valtech.co.uk>
2013-08-22 14:42:19 +0100

    End to End tests - Split tests to avoid random errors and be able to run them individually.

---
+++ c3/test/app/FunctionalTestCase6Spec.scala
@@ -0,0 +1,32 @@
+package app
+
+import play.api.test.WithBrowser
+import utils.pageobjects.s1_carers_allowance.G1BenefitsPageContext
+import utils.pageobjects.{XmlPage, ClaimScenario, Page}
+
+/**
+ * End-to-End functional tests using input files created by Steve Moody.
+ * @author Jorge Migueis
+ *         Date: 02/08/2013
+ */
+class FunctionalTestCase6Spec extends FunctionalTestCommon {
+  isolated
+
+  "The application " should {
+
+    "Successfully run absolute Test Case 6 " in new WithBrowser with G1BenefitsPageContext {
+
+      val claim = ClaimScenario.buildClaimFromFile("/functional_scenarios/ClaimScenario_TestCase6.csv")
+      page goToThePage(waitForPage = true, waitDuration = 500)
+      val lastPage = page runClaimWith(claim, XmlPage.title, waitForPage = true, waitDuration = 500, trace = false)
+
+      lastPage match {
+        case p: XmlPage => {
+          validateAndPrintErrors(p, claim) should beTrue
+        }
+        case p: Page => println(p.source())
+      }
+    }
+
+  } section "functional"
+}
