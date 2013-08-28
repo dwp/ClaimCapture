f3e7d81e8a166d851cc2c8f93c0285491562ac8e
David Ainslie <dainslie@gmail.com>
2013-08-28 14:05:27 +0100

    Updated "new claim"

--- c3/app/controllers/s1_carers_allowance/G1Benefits.scala
+++ c3/app/controllers/s1_carers_allowance/G1Benefits.scala
@@ -3,7 +3,7 @@ package controllers.s1_carers_allowance
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.mvc.Controller
-import models.view.CachedClaim
+import models.view.{Navigable, CachedClaim}
 import utils.helpers.CarersForm._
 import models.domain.Benefits
 import language.reflectiveCalls
@@ -11,14 +11,13 @@ import controllers.Mappings._
 import CarersAllowance._
 import play.api.data.FormError
 
-object G1Benefits extends Controller with CachedClaim {
-  val form = Form(
-    mapping(
-      "answer" -> nonEmptyText.verifying(validYesNo)
-    )(Benefits.apply)(Benefits.unapply))
+object G1Benefits extends Controller with CachedClaim with Navigable {
+  val form = Form(mapping(
+    "answer" -> nonEmptyText.verifying(validYesNo)
+  )(Benefits.apply)(Benefits.unapply))
 
   def present = newClaim { implicit claim => implicit request =>
-    Ok(views.html.s1_carers_allowance.g1_benefits(form.fill(Benefits), completedQuestionGroups(Benefits)))
+    track(Benefits) { implicit claim => Ok(views.html.s1_carers_allowance.g1_benefits(form.fill(Benefits), completedQuestionGroups(Benefits))) }
   }
 
   def submit = claiming { implicit claim => implicit request =>
