633bf02e13683f2ae5158be80ab0028c3d4a8c41
David Ainslie <dainslie@gmail.com>
2013-08-30 14:59:44 +0100

    Employment no longer uses "referrer".

--- c3/test/controllers/s7_employment/G14JobCompletionIntegrationSpec.scala
+++ c3/test/controllers/s7_employment/G14JobCompletionIntegrationSpec.scala
@@ -2,47 +2,49 @@ package controllers.s7_employment
 
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
-import controllers.BrowserMatchers
+import controllers.{WithBrowserHelper, BrowserMatchers}
 
 class G14JobCompletionIntegrationSpec extends Specification with Tags {
   val jobID = "dummyJobID"
 
   "Job completion - Integration" should {
-    "present" in new WithBrowser with BrowserMatchers {
-      browser.goTo(s"/employment/job-completion/$jobID")
+    "present" in new WithBrowser with WithBrowserHelper with BrowserMatchers {
+      goTo(s"/employment/job-completion/$jobID")
       titleMustEqual("Job Completion - Employment History")
     }
 
-    """progress back to start i.e. "employment history".""" in new WithBrowser with BrowserMatchers with EmployedSinceClaimDate {
-      beginClaim
+    """progress back to start i.e. "employment history".""" in new WithBrowser with WithBrowserHelper with BrowserMatchers with EmployedSinceClaimDate {
+      beginClaim()
 
-      browser.goTo(s"/employment/job-completion/$jobID")
+      goTo("/employment/been-employed")
+
+      goTo(s"/employment/job-completion/$jobID")
       titleMustEqual("Job Completion - Employment History")
 
-      browser.submit("button[type='submit']")
+      next
       titleMustEqual("Your employment history - Employment History")
     }
 
-    """go back to "Person you care for expenses".""" in new WithBrowser with BrowserMatchers {
+    """go back to "Person you care for expenses".""" in new WithBrowser with WithBrowserHelper with BrowserMatchers {
       /* Required data to get to "end" pages */
-      browser.goTo(s"/employment/about-expenses/$jobID")
-      browser.click("#payForAnythingNecessary_yes")
-      browser.click("#payAnyoneToLookAfterChildren_yes")
-      browser.click("#payAnyoneToLookAfterPerson_yes")
-      browser.submit("button[type='submit']")
+      goTo(s"/employment/about-expenses/$jobID")
+      click("#payForAnythingNecessary_yes")
+      click("#payAnyoneToLookAfterChildren_yes")
+      click("#payAnyoneToLookAfterPerson_yes")
+      next
       titleMustEqual("Necessary expenses to do your job - Employment History")
 
       /* The page we wish to go back to */
-      browser.goTo(s"/employment/person-you-care-for-expenses/$jobID")
+      goTo(s"/employment/person-you-care-for-expenses/$jobID")
       titleMustEqual("Expenses related to the person you care for, while you are at work - Employment History")
-      browser.fill("#whoDoYouPay") `with` "someone"
-      browser.click("#relationToPersonYouCare option[value='brother']")
-      browser.click("#relationToYou option[value='sister']")
+      fill("#whoDoYouPay") `with` "someone"
+      click("#relationToPersonYouCare option[value='brother']")
+      click("#relationToYou option[value='sister']")
 
-      browser.submit("button[type='submit']")
+      next
       titleMustEqual("Job Completion - Employment History")
 
-      browser.click("#backButton")
+      click("#backButton")
       titleMustEqual("Expenses related to the person you care for, while you are at work - Employment History")
     }
   } section("integration", models.domain.Employed.id)
