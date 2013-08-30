692ff909977864373968dcbcb1025f1c8f518dc2
David Ainslie <dainslie@gmail.com>
2013-08-30 16:29:00 +0100

    Trips no longer uses "referrer".

--- c3/test/controllers/s5_time_spent_abroad/G3AbroadForMoreThan52WeeksIntegrationSpec.scala
+++ c3/test/controllers/s5_time_spent_abroad/G3AbroadForMoreThan52WeeksIntegrationSpec.scala
@@ -3,6 +3,7 @@ package controllers.s5_time_spent_abroad
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
 import controllers.{WithBrowserHelper, BrowserMatchers}
+import controllers.{BrowserMatchers, WithBrowserHelper}
 
 class G3AbroadForMoreThan52WeeksIntegrationSpec extends Specification with Tags {
   "Abroad for more that 52 weeks" should {
@@ -39,49 +40,6 @@ class G3AbroadForMoreThan52WeeksIntegrationSpec extends Specification with Tags 
 
       back
       titleMustEqual("Abroad for more than 4 weeks - Time Spent Abroad")
-    }
-
-    """not record the "yes/no" answer upon starting to add a new 52 weeks trip but "cancel".""" in new WithBrowser with WithBrowserHelper with BrowserMatchers {
-      browser goTo "/time-spent-abroad/abroad-for-more-than-52-weeks"
-      titleMustEqual("Abroad for more than 52 weeks - Time Spent Abroad")
-
-      browser.click("#anyTrips_yes")
-      next
-      titleMustEqual("Trips - Time Spent Abroad")
-
-      back
-      titleMustEqual("Abroad for more than 52 weeks - Time Spent Abroad")
-      browser.findFirst("#anyTrips_yes").isSelected should beFalse
-      browser.findFirst("#anyTrips_no").isSelected should beFalse
-    }
-
-    """allow a new 52 weeks trip to be added but not record the "yes/no" answer""" in new WithBrowser with WithBrowserHelper with BrowserMatchers {
-      def trip() = {
-        browser click "#start_day option[value='1']"
-        browser click "#start_month option[value='1']"
-        browser fill "#start_year" `with` "2000"
-
-        browser click "#end_day option[value='1']"
-        browser click "#end_month option[value='1']"
-        browser fill "#end_year" `with` "2000"
-
-        browser fill "#where" `with` "Scotland"
-        browser fill "#why" `with` "For Holidays"
-      }
-
-      browser goTo "/time-spent-abroad/abroad-for-more-than-52-weeks"
-      titleMustEqual("Abroad for more than 52 weeks - Time Spent Abroad")
-
-      browser.click("#anyTrips_yes")
-      next
-      titleMustEqual("Trips - Time Spent Abroad")
-
-      trip()
-      next
-      titleMustEqual("Abroad for more than 52 weeks - Time Spent Abroad")
-
-      browser.findFirst("#anyTrips_yes").isSelected should beFalse
-      browser.findFirst("#anyTrips_no").isSelected should beFalse
     }
 
     """remember "no more 52 weeks trips" upon stating "52 weeks trips" and returning""" in new WithBrowser with WithBrowserHelper with BrowserMatchers {
