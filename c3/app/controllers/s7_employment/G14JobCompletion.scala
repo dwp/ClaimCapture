2e3e9bf46613c9a62cef08ab517e63386ef2580c
Leon Derks <l.a.derks@gmail.com>
2013-09-06 11:30:34 +0100

    DE205

--- c3/app/controllers/s7_employment/G14JobCompletion.scala
+++ c3/app/controllers/s7_employment/G14JobCompletion.scala
@@ -3,7 +3,7 @@ package controllers.s7_employment
 import language.reflectiveCalls
 import models.view.{Navigable, CachedClaim}
 import play.api.mvc.Controller
-import models.domain.JobCompletion
+import models.domain.{BeenEmployed, JobCompletion}
 import controllers.s7_employment.Employment._
 
 object G14JobCompletion extends Controller with CachedClaim with Navigable {
@@ -12,6 +12,6 @@ object G14JobCompletion extends Controller with CachedClaim with Navigable {
   }
 
   def submit = claimingInJob { jobID => implicit claim => implicit request =>
-    claim.update(jobs.completeJob(jobID)) -> Redirect(routes.G1BeenEmployed.present())
+    claim.update(BeenEmployed(beenEmployed="")).update(jobs.completeJob(jobID)) -> Redirect(routes.G1BeenEmployed.present())
   }
 }
