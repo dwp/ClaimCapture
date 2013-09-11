00c21118ed49eccd791a2111bf61b8bc4fbd2dd5
Scott Walker <scott.walker@valtech.co.uk>
2013-09-11 10:18:12 +0100

    Removed duplicates after merge

--- c3/modules/common/app/submission/Submitter.scala
+++
@@ -1,9 +0,0 @@
-package submission
-
-import models.domain.Claim
-import scala.concurrent.Future
-import play.api.mvc.{AnyContent, Request, PlainResult}
-
-trait Submitter {
-  def submit(claim: Claim, request : Request[AnyContent]): Future[PlainResult]
-}
