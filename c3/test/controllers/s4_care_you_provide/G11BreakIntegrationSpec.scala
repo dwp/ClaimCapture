50f64b46c7a626efb7b404773fb6295e51d1a5c7
David Ainslie <dainslie@gmail.com>
2013-09-17 17:45:12 +0100

    "Home" added to DP breaks. Cleaning up.

--- c3/test/controllers/s4_care_you_provide/G11BreakIntegrationSpec.scala
+++ c3/test/controllers/s4_care_you_provide/G11BreakIntegrationSpec.scala
@@ -7,9 +7,10 @@ import models.DayMonthYear
 import java.util.concurrent.TimeUnit
 import utils.pageobjects.s4_care_you_provide.{G11BreakPage, G10BreaksInCarePage}
 import app.Whereabouts._
+import java.util.Collections
+import java.util
 
 class G11BreakIntegrationSpec extends Specification with Tags {
-
   "Break" should {
     "be presented" in new WithBrowser with BreakFiller with WithBrowserHelper with BrowserMatchers {
       goTo("/care-you-provide/break")
@@ -21,33 +22,33 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       goTo("/care-you-provide/breaks-in-care")
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_no")
+      click("#answer_no")
       next
       titleMustEqual("Completion - About the care you provide")
     }
 
     """give 2 errors when missing 2 mandatory fields of data - missing "start year" and "medical" """ in new WithBrowser with BreakFiller with WithBrowserHelper with BrowserMatchers {
       goTo("/care-you-provide/breaks-in-care")
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
-      browser.click("#start_day option[value='1']")
-      browser.click("#start_month option[value='1']")
+      click("#start_day option[value='1']")
+      click("#start_month option[value='1']")
 
-      browser.click("#whereYou_location option[value='Hospital']")
-      browser.click("#wherePerson_location option[value='Hospital']")
+      click("#whereYou_location option[value='Hospital']")
+      click("#wherePerson_location option[value='Hospital']")
 
       next
       titleMustEqual("Break - About the care you provide")
-      browser.find("div[class=validation-summary] ol li").size shouldEqual 2
+      findAll("div[class=validation-summary] ol li").size shouldEqual 2
     }
 
     """show 2 breaks in "break table" upon providing 2 breaks""" in new WithBrowser with BreakFiller with WithBrowserHelper with BrowserMatchers {
       goTo("/care-you-provide/breaks-in-care")
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -55,7 +56,7 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -63,7 +64,7 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.$("#breaks table tbody tr").size() shouldEqual 2
+      $("#breaks table tbody tr").size() shouldEqual 2
     }
 
     "show zero breaks after creating one and then deleting" in new WithBrowser with BreakFiller with WithBrowserHelper with BrowserMatchers {
@@ -72,20 +73,20 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       goTo("/care-you-provide/breaks-in-care")
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
       break()
       next
       titleMustEqual(G10BreaksInCarePage.title)
-      browser.$("tbody tr").size mustEqual 1
+      $("tbody tr").size mustEqual 1
 
-      browser.click("input[value='Delete']")
-      browser.await().atMost(10, TimeUnit.SECONDS).until(".breaks-prompt").areDisplayed
-      browser.click("input[value='Yes']")
+      click("input[value='Delete']")
+      await().atMost(10, TimeUnit.SECONDS).until(".breaks-prompt").areDisplayed
+      click("input[value='Yes']")
 
-      browser.await().atMost(10, TimeUnit.SECONDS).until("tbody tr").hasSize(0)
+      await().atMost(10, TimeUnit.SECONDS).until("tbody tr").hasSize(0)
     }
 
     "show two breaks after creating three and then deleting one" in new WithBrowser with BreakFiller with WithBrowserHelper with BrowserMatchers {
@@ -93,7 +94,7 @@ class G11BreakIntegrationSpec extends Specification with Tags {
 
       goTo("/care-you-provide/breaks-in-care")
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -101,7 +102,7 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -109,7 +110,7 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -117,20 +118,20 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.$("tbody tr").size() mustEqual 3
+      $("tbody tr").size() mustEqual 3
 
-      browser.findFirst("tbody tr input[value='Delete']").click()
-      browser.await().atMost(30, TimeUnit.SECONDS).until(".breaks-prompt").areDisplayed
-      browser.click("input[value='Yes']")
+      findFirst("tbody tr input[value='Delete']").click()
+      await().atMost(30, TimeUnit.SECONDS).until(".breaks-prompt").areDisplayed
+      click("input[value='Yes']")
 
-      browser.await().atMost(30, TimeUnit.SECONDS).until("tbody tr").hasSize(2)
+      await().atMost(30, TimeUnit.SECONDS).until("tbody tr").hasSize(2)
     }
 
     "add two breaks and edit the second's start year" in new WithBrowser with BreakFiller with WithBrowserHelper with BrowserMatchers {
       goTo("/care-you-provide/breaks-in-care")
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -138,7 +139,7 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.click("#answer_yes")
+      click("#answer_yes")
       next
       titleMustEqual("Break - About the care you provide")
 
@@ -146,17 +147,26 @@ class G11BreakIntegrationSpec extends Specification with Tags {
       next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.findFirst("input[value='Change']").click()
+      findFirst("input[value='Change']").click()
 
       titleMustEqual(G11BreakPage.title)
-      browser.$("#start_year").getValue mustEqual 2001.toString
+      $("#start_year").getValue mustEqual 2001.toString
 
-      browser.fill("#start_year") `with` "1999"
-      browser.submit("button[type='submit']")
+      fill("#start_year") `with` "1999"
+      next
       titleMustEqual(G10BreaksInCarePage.title)
 
-      browser.$("tbody tr").size() mustEqual 2
-      browser.$("tbody").findFirst("tr").findFirst("td").getText must contain("1999")
+      $("tbody tr").size() mustEqual 2
+      $("tbody").findFirst("tr").findFirst("td").getText must contain("1999")
+    }
+
+    """show "all options" for "Where was the person you care for during the break?".""" in new WithBrowser with WithBrowserHelper with BrowserMatchers {
+      import scala.collection.JavaConverters._
+
+      goTo("/care-you-provide/break")
+      titleMustEqual("Break - About the care you provide")
+
+      text("#wherePerson_location option").asScala should containAllOf(List("Please select", "At Home", "Hospital", "Respite Care", "Care Home", "Nursing Home", "Other"))
     }
   } section("integration", models.domain.CareYouProvide.id)
 }
