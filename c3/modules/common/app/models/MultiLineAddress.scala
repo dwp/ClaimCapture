6df9d7a627dd1c7846208313ae0367337992379e
Scott Walker <scott.walker@valtech.co.uk>
2013-09-18 17:37:26 +0100

    removed dead code from the previous bot detection code

--- c3/modules/common/app/models/MultiLineAddress.scala
+++ c3/modules/common/app/models/MultiLineAddress.scala
@@ -1,9 +1,3 @@
 package models
 
-case class MultiLineAddress(lineOne: Option[String] = None, lineTwo: Option[String] = None, lineThree: Option[String] = None){
-  def numberOfCharactersInput: Int = {
-    {lineOne match {case Some(s) => s.length case _ => 0}} +
-    {lineTwo match {case Some(s) => s.length case _ => 0}} +
-    {lineThree match {case Some(s) => s.length case _ => 0}}
-  }
-}
+case class MultiLineAddress(lineOne: Option[String] = None, lineTwo: Option[String] = None, lineThree: Option[String] = None)
--- c3/modules/common/app/models/NationalInsuranceNumber.scala
+++ c3/modules/common/app/models/NationalInsuranceNumber.scala
@@ -2,12 +2,4 @@ package models
 
 case class NationalInsuranceNumber(ni1: Option[String], ni2: Option[String], ni3: Option[String], ni4: Option[String], ni5: Option[String]) {
   def stringify = if (ni1.isDefined && ni2.isDefined && ni3.isDefined && ni4.isDefined && ni5.isDefined) s"${ni1.get}${ni2.get}${ni3.get}${ni4.get}${ni5.get}".toUpperCase
-                  else ""
-  def numberOfCharactersInput: Int = {
-    {ni1 match {case Some(s) => s.length case _ => 0}} +
-    {ni2 match {case Some(s) => s.length case _ => 0}} +
-    {ni3 match {case Some(s) => s.length case _ => 0}} +
-    {ni4 match {case Some(s) => s.length case _ => 0}} +
-    {ni5 match {case Some(s) => s.length case _ => 0}}
-  }
 }
