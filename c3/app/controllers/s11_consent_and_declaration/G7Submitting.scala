2c7c9821df9b9c25daef44ea8f3f2ac3e74741f3
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-09-19 17:49:48 +0100

    Adding a Submitting page to wait for the submit to complete

--- c3/app/controllers/s11_consent_and_declaration/G7Submitting.scala
+++ c3/app/controllers/s11_consent_and_declaration/G7Submitting.scala
@@ -4,12 +4,12 @@ import play.api.mvc.Controller
 import models.view.CachedClaim
 import models.domain.Submit
 
-object G5Submit extends Controller with ConsentAndDeclarationRouting with CachedClaim {
+object G7Submitting extends Controller with ConsentAndDeclarationRouting with CachedClaim {
   def present = claiming { implicit claim => implicit request =>
-    Ok(views.html.s11_consent_and_declaration.g5_submit(completedQuestionGroups(Submit)))
+    Ok(views.html.s11_consent_and_declaration.g7_submitting())
   }
 
   def submit = claiming { implicit claim => implicit request =>
-    Redirect("/consent-and-declaration/submitting")
+    Redirect("/submit")
   }
 }
