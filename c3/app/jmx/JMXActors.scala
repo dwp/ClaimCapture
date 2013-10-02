e903270e93f4dc47b32e17ec6d0f83d937d9a0ab
David Ainslie <dainslie@gmail.com>
2013-10-02 14:21:08 +0100

    Missing an import

--- c3/app/jmx/JMXActors.scala
+++ c3/app/jmx/JMXActors.scala
@@ -3,6 +3,7 @@ package jmx
 import akka.actor.Props
 
 object JMXActors {
+  import play.api.Play.current
   import play.api.libs.concurrent.Akka
 
   val claimInspector = Akka.system.actorOf(Props[ClaimInspector], name = "claim-inspector")
