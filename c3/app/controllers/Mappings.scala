dd6ecb5a98fe2d38f0ae42d73d3ab0f5c767ffa7
Phil Barton <phil.barton@valtech.co.uk>
2013-10-17 11:44:01 +0100

    whereabouts 60 to 35 simpleTextline validatiion

--- c3/app/controllers/Mappings.scala
+++ c3/app/controllers/Mappings.scala
@@ -41,13 +41,13 @@ object Mappings {
     "to" -> dayMonthYear.verifying(validDate))(PeriodFromTo.apply)(PeriodFromTo.unapply)
 
   val address: Mapping[MultiLineAddress] = mapping(
-    "lineOne" -> optional(text(maxLength = sixty)),
-    "lineTwo" -> optional(text(maxLength = sixty)),
-    "lineThree" -> optional(text(maxLength = sixty)))(MultiLineAddress.apply)(MultiLineAddress.unapply)
+    "lineOne" -> optional(text(maxLength = sixty).verifying(simpleTextLine)),
+    "lineTwo" -> optional(text(maxLength = sixty).verifying(simpleTextLine)),
+    "lineThree" -> optional(text(maxLength = sixty).verifying(simpleTextLine)))(MultiLineAddress.apply)(MultiLineAddress.unapply)
 
   val whereabouts: Mapping[Whereabouts] = mapping(
-    "location" -> nonEmptyText,
-    "location.other" -> optional(text(maxLength = sixty)))(Whereabouts.apply)(Whereabouts.unapply)
+    "location" -> nonEmptyText(maxLength = 35),
+    "location.other" -> optional(text(maxLength = 35)))(Whereabouts.apply)(Whereabouts.unapply)
 
   val paymentFrequency: Mapping[PaymentFrequency] = mapping(
     "frequency" -> text(maxLength = sixty),
@@ -202,6 +202,15 @@ object Mappings {
       case true => Invalid(ValidationError("error.forbidden.characters"))
 
     }
-
   }
+
+  def simpleTextLine: Constraint[String] = Constraint[String]("constraint.addressLine") { simpleTextLine =>
+    val simpleTextLinePattern = """^[a-zA-Z0-9 ]*$""".r
+
+    simpleTextLinePattern.pattern.matcher(simpleTextLine).matches match {
+      case true => Valid
+      case false => Invalid(ValidationError("error.address.characters"))
+    }
+  }
+
 }
