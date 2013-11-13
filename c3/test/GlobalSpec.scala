d9978689229fe6c058fb5668b2947bd41ceb1519
Jorge Migueis <jorge.migueis@valtech.co.uk>
2013-11-13 12:43:49 +0000

    GlobalSpec tagged 'unit'.

--- c3/test/GlobalSpec.scala
+++ c3/test/GlobalSpec.scala
@@ -1,5 +1,5 @@
 import org.specs2.mock.Mockito
-import org.specs2.mutable.Specification
+import org.specs2.mutable.{Tags, Specification}
 import org.specs2.specification.BeforeExample
 import play.api.GlobalSettings
 import play.api.test.WithApplication
@@ -7,7 +7,7 @@ import com.google.inject.Guice
 import controllers.submission.{WebServiceSubmitter, XmlSubmitter, Submitter}
 import modules.ProdModule
 
-class GlobalSpec extends Specification with BeforeExample with Mockito {
+class GlobalSpec extends Specification with BeforeExample with Mockito  with Tags {
   "Application" should {
     "use 'production' module in 'prod' mode <- This is actual a badly implemented example as we use hard coding" in new WithApplication {
       ProdGlobal.injector.getInstance(classOf[Submitter]) should beAnInstanceOf[WebServiceSubmitter]
@@ -16,7 +16,7 @@ class GlobalSpec extends Specification with BeforeExample with Mockito {
     "use 'default' module when not in 'prod' mode" in new WithApplication {
       Global.injector.getInstance(classOf[Submitter]) should beAnInstanceOf[XmlSubmitter]
     }
-  }
+  } section "unit"
 
   protected def before: Any = System.setProperty("application.global", "ProdGlobal")
 }
