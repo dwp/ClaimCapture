4a4e40129b3446f31b496fac2838af94f70682d1
Jorge Migueis <jorge.migueis@valtech.co.uk>
2013-11-20 15:38:55 +0000

    Added Consents sectionto Change of Circumstances XML.

--- c3/test/xml/circumstances/OtherChangesSpec.scala
+++ c3/test/xml/circumstances/OtherChangesSpec.scala
@@ -1,16 +1,15 @@
-package xml
+package xml.circumstances
 
 import org.specs2.mutable.{Tags, Specification}
 import models.domain._
-import xml.circumstances.OtherChanges
 
-class AdditionalInfoSpec extends Specification with Tags {
+class OtherChangesSpec extends Specification with Tags {
   val otherInfo = "Some other info"
 
   "Additional Info" should {
     "generate xml" in {
-      val claim = Claim().update(CircumstancesOtherInfo(otherInfo))
-      val xml = OtherChanges.xml(claim)
+      val circs = Claim().update(CircumstancesOtherInfo(otherInfo))
+      val xml = OtherChanges.xml(circs)
       (xml \\ "OtherChanges" \ "QuestionLabel").text shouldEqual "c2.g1"
       (xml \\ "OtherChanges" \ "Answer").text shouldEqual otherInfo
     }
