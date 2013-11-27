1dc87e1a2ce958a62ac6c6812f89dcadeef5b8d1
Chris Rudd <chris.rudd@valtech.co.uk>
2013-11-27 16:42:35 +0000

    Reviewed Partner XML.

--- c3/app/xml/claim/Partner.scala
+++ c3/app/xml/claim/Partner.scala
@@ -12,25 +12,22 @@ object Partner extends XMLComponent {
 
   def xml(claim: Claim) = {
     val moreAboutYou = claim.questionGroup[MoreAboutYou].getOrElse(MoreAboutYou(hadPartnerSinceClaimDate = no))
-
     val yourPartnerPersonalDetails = claim.questionGroup[YourPartnerPersonalDetails].getOrElse(YourPartnerPersonalDetails())
-
     val personYouCareFor = claim.questionGroup[PersonYouCareFor].getOrElse(PersonYouCareFor())
-
-    val hadPartner = moreAboutYou.hadPartnerSinceClaimDate == yes
+    val hadPartner = moreAboutYou.hadPartnerSinceClaimDate.toLowerCase == yes
 
     if (hadPartner) {
       <Partner>
         <Surname>{yourPartnerPersonalDetails.surname}</Surname>
-        <OtherNames>{yourPartnerPersonalDetails.firstName} {yourPartnerPersonalDetails.middleName.orNull}</OtherNames>
+        <OtherNames>{yourPartnerPersonalDetails.firstName} {yourPartnerPersonalDetails.middleName.getOrElse("")}</OtherNames>
         {yourPartnerPersonalDetails.otherSurnames match {
-          case Some(n) => <OtherSurnames>{yourPartnerPersonalDetails.otherSurnames.orNull}</OtherSurnames>
+          case Some(n) => <OtherSurnames>{yourPartnerPersonalDetails.otherSurnames.get}</OtherSurnames>
           case None => NodeSeq.Empty
         }}
         <Title>{yourPartnerPersonalDetails.title}</Title>
         <DateOfBirth>{yourPartnerPersonalDetails.dateOfBirth.`dd-MM-yyyy`}</DateOfBirth>
         <NationalInsuranceNumber>{stringify(yourPartnerPersonalDetails.nationalInsuranceNumber)}</NationalInsuranceNumber>
-        <NationalityPartner>{yourPartnerPersonalDetails.nationality.orNull}</NationalityPartner>
+        <NationalityPartner>{yourPartnerPersonalDetails.nationality.get}</NationalityPartner>
         <RelationshipStatus>
           {question(<SeparatedFromPartner/>, "separated_fromPartner.label", yourPartnerPersonalDetails.separatedFromPartner, claim.dateOfClaim.fold("{NO CLAIM DATE}")(_.`dd/MM/yyyy`))}
         </RelationshipStatus>
