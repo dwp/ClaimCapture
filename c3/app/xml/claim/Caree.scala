69ee7d9d19797118d3e00a1ff1b078ba67a8ba8f
Chris Rudd <chris.rudd@valtech.co.uk>
2013-11-27 16:03:57 +0000

    Simplified XML.Caree

--- c3/app/xml/claim/Caree.scala
+++ c3/app/xml/claim/Caree.scala
@@ -16,7 +16,7 @@ object Caree extends XMLComponent {
 
     <Caree>
       <Surname>{theirPersonalDetails.surname}</Surname>
-      <OtherNames>{theirPersonalDetails.firstName} {theirPersonalDetails.middleName.orNull}</OtherNames>
+      <OtherNames>{theirPersonalDetails.firstName} {theirPersonalDetails.middleName.getOrElse("")}</OtherNames>
       <Title>{theirPersonalDetails.title}</Title>
       <DateOfBirth>{theirPersonalDetails.dateOfBirth.`dd-MM-yyyy`}</DateOfBirth>
       {theirPersonalDetails.nationalInsuranceNumber match {
@@ -25,15 +25,14 @@ object Caree extends XMLComponent {
       }}
       {postalAddressStructure(theirContactDetails.address, theirContactDetails.postcode)}
       {if(!theirContactDetails.phoneNumber.isEmpty){
-        <DayTimePhoneNumber>{theirContactDetails.phoneNumber.orNull}</DayTimePhoneNumber>
+        <DayTimePhoneNumber>{theirContactDetails.phoneNumber.get}</DayTimePhoneNumber>
       }}
       {question(<RelationToClaimant/>,"whatRelationIsToYou", moreAboutThePerson.relationship)}
       {question(<Cared35Hours/>,"hours.answer", moreAboutTheCare.spent35HoursCaring)}
       {breaksSinceClaim(claim)}
       {careBreak(claim)}
       {question(<Cared35HoursBefore/>,"beforeClaimCaring.answer", moreAboutTheCare.spent35HoursCaringBeforeClaim.answer)}
-      {dateStartedCaring(moreAboutTheCare)}
-      <!--{breaksBeforeClaim(claim)}-->
+      {question(<DateStartCaring/>,"beforeClaimCaring_date", moreAboutTheCare.spent35HoursCaringBeforeClaim.date)}
       {question(<LiveSameAddress/>,"liveAtSameAddressCareYouProvide", theirPersonalDetails.liveAtSameAddressCareYouProvide)}
       {question(<ArmedForcesIndependencePayment/>,"armedForcesPayment", moreAboutThePerson.armedForcesPayment)}
     </Caree>
@@ -50,15 +49,7 @@ object Caree extends XMLComponent {
     val hasSpent35HoursCaringBeforeClaimDate = moreAboutTheCare.spent35HoursCaringBeforeClaim.answer == yes
 
     if (hasSpent35HoursCaringBeforeClaimDate) {
-      <BreaksBeforeClaim>{if (breaksInCare.hasBreaks) yes else no}</BreaksBeforeClaim>
-    } else NodeSeq.Empty
-  }
-
-  def dateStartedCaring(moreAboutTheCare: MoreAboutTheCare) = {
-    val startedCaringBeforeClaimDate = moreAboutTheCare.spent35HoursCaringBeforeClaim.answer == yes
-
-    if (startedCaringBeforeClaimDate) {
-      {question(<DateStartCaring/>,"beforeClaimCaring_date", stringify(moreAboutTheCare.spent35HoursCaringBeforeClaim.date))}
+      <BreaksBeforeClaim>{stringify(breaksInCare.hasBreaks)}</BreaksBeforeClaim>
     } else NodeSeq.Empty
   }
 
@@ -75,7 +66,6 @@ object Caree extends XMLComponent {
         }}
         {question(<MedicalCare/>,"medicalDuringBreak", break.medicalDuringBreak)}
         {questionOther(<ReasonClaimant/>,"whereYou", break.whereYou.location, break.whereYou.other)}
-
         {questionOther(<ReasonCaree/>,"wherePerson", break.wherePerson.location, break.wherePerson.other)}
       </CareBreak>
     }
