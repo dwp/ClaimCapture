c72e284b3a3c2fa9efbac2487b1e2874e804e389
Ruben Diaz <ruben.diaz@valtech.co.uk>
2013-11-28 12:30:23 +0000

    Intelligent claim pack notes

--- c3/app/controllers/ThankYou.scala
+++ c3/app/controllers/ThankYou.scala
@@ -4,11 +4,11 @@ import play.api.mvc.{Action, Controller}
 import play.api.Logger
 
 object ThankYou extends Controller {
-  def claim(txnId:String) = Action { request =>
+  def claim(txnId:String) = Action { implicit request =>
     Logger.debug(txnId)
     Ok(views.html.common.thankYouClaim(txnId))
   }
-  def circs(txnId:String) = Action { request =>
+  def circs(txnId:String) = Action { implicit request =>
     Ok(views.html.common.thankYouCircs(txnId))
   }
 }
