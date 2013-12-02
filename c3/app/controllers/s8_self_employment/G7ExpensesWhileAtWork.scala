38907c0ae061356ecdc4b73c288ce403682a8575
Jorge Migueis <jorge.migueis@valtech.co.uk>
2013-12-02 10:45:25 +0000

    DE251 - fixed.

--- c3/app/controllers/s8_self_employment/G7ExpensesWhileAtWork.scala
+++ c3/app/controllers/s8_self_employment/G7ExpensesWhileAtWork.scala
@@ -58,7 +58,7 @@ object G7ExpensesWhileAtWork extends Controller with CachedClaim with Navigable 
         val formWithErrorsUpdate = formWithErrors
           .replaceError("howMuchYouPay", "error.required", FormError("howMuchYouPay", "error.required", Seq(didYouDoYouIfSelfEmployed.toLowerCase)))
           .replaceError("howMuchYouPay", "decimal.invalid", FormError("howMuchYouPay", "decimal.invalid", Seq(didYouDoYouIfSelfEmployed.toLowerCase)))
-          .replaceError("howOftenPayExpenses", "error.required", FormError("howOftenPayExpenses", "error.required", Seq(didYouDoYouIfSelfEmployed.toLowerCase)))
+          .replaceError("howOftenPayExpenses.frequency", "error.required", FormError("howOftenPayExpenses", "error.required", Seq(didYouDoYouIfSelfEmployed.toLowerCase)))
           .replaceError("", "relationToPartner.required", FormError("relationToPartner", "error.required"))
         BadRequest(views.html.s8_self_employment.g7_expensesWhileAtWork(formWithErrorsUpdate))
       },
