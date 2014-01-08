f88dd94217778062e249c4903439f334671a2c17
Phil Barton <phil.barton@valtech.co.uk>
2014-01-08 17:20:44 +0000

    US521 - Add time to health page and obscure url

--- c3/app/controllers/Health.scala
+++ c3/app/controllers/Health.scala
@@ -1,12 +1,15 @@
 package controllers
 
 import play.api.mvc.{Controller, Action}
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 trait HealthController {
   this: Controller =>
 
   def health = Action {
-    Ok(views.html.common.health())
+    val now = DateTimeFormat.forPattern("HH:mm:ss dd-MM-yy").print(DateTime.now())
+    Ok(views.html.common.health(now))
   }
 }
 
