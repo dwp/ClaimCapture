7fd9aed3e08d8a27ef71e1b916562376f86a2c66
Mark Hunter <mark@huntersix.co.uk>
2014-01-23 15:52:25 +0000

    US553 - remove date from evidence list

--- c3/app/xml/EvidenceList.scala
+++ c3/app/xml/EvidenceList.scala
@@ -122,13 +122,11 @@ object EvidenceList {
     val claimDate = claim.questionGroup[ClaimDate].getOrElse(ClaimDate())
     var textLines = NodeSeq.Empty
 
-    textLines ++= textLine("Have you been out of England, Scotland or Wales for more than 52 weeks in the last 3 years before your claim date " +
-      s" ${(claimDate.dateOfClaim - 3 years).`dd/MM/yyyy`}? = ", if (trips.fiftyTwoWeeksTrips.size > 0) Yes else No)
+    textLines ++= textLine("Have you been out of England, Scotland or Wales for more than 52 weeks in the last 3 years before your claim date? = ", if (trips.fiftyTwoWeeksTrips.size > 0) Yes else No)
 
     for ((fiftyTwoWeekTrip,index) <- trips.fiftyTwoWeeksTrips.zipWithIndex) {
       if (index > 0){
-        textLines ++= textLine("Have you been out of England, Scotland or Wales at any other time in the last 3 years before your claim date" +
-          s" ${(claimDate.dateOfClaim - 3 years).`dd/MM/yyyy`}? = ", if (trips.fiftyTwoWeeksTrips.size > 0) Yes else No)
+        textLines ++= textLine("Have you been out of England, Scotland or Wales at any other time in the last 3 years before your claim date? = ", if (trips.fiftyTwoWeeksTrips.size > 0) Yes else No)
       }
       textLines ++= textLine("Which country did you go to? = ", fiftyTwoWeekTrip.where)
 
