ebc61f8e96ef2cac91b4f001722fb6bfed785f5a
Nedda Kaltcheva <nedda.kaltcheva@gmail.com>
2014-01-30 15:25:57 +0000

    Fixed failing tests - cleanup after merge and US506 - Employment

--- c3/test/utils/pageobjects/s7_employment/G14JobCompletionPage.scala
+++ c3/test/utils/pageobjects/s7_employment/G14JobCompletionPage.scala
@@ -4,6 +4,10 @@ import play.api.test.WithBrowser
 import utils.pageobjects._
 
 final class G14JobCompletionPage(ctx:PageObjectsContext, iteration: Int) extends ClaimPage(ctx, G14JobCompletionPage.url.replace(":jobID", iteration.toString), G14JobCompletionPage.title, iteration) {
+  override def getNewIterationNumber: Int = {
+    import IterationManager._
+    ctx.iterationManager.increment(Employment)
+  }
 }
 
 object G14JobCompletionPage {
