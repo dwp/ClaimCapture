c0006ebedfca85dbd98f4ec6d7bf27d8efd59e0d
Mark Hunter <mark@huntersix.co.uk>
2014-02-03 10:38:29 +0000

    US535 - start adding change of circs self employment page

---
+++ c3/app/controllers/circs/s2c_self_employment/G1SelfEmployment.scala
@@ -0,0 +1,40 @@
+package controllers.circs.s2c_self_employment
+
+import play.api.mvc.Controller
+import models.view.{Navigable, CachedChangeOfCircs}
+import play.api.data.Form
+import play.api.data.Forms._
+import models.domain.{CircumstancesSelfEmployment, CircumstancesOtherInfo}
+import utils.helpers.CarersForm._
+import controllers.CarersForms._
+import controllers.Mappings._
+import models.yesNo.YesNoWithText
+
+object G1SelfEmployment extends Controller with CachedChangeOfCircs with Navigable {
+  val stillCaringMapping =
+    "stillCaring" -> mapping(
+      "answer" -> nonEmptyText.verifying(validYesNo),
+      "text" -> optional(carersNonEmptyText(maxLength = 35))
+    )(YesNoWithText.apply)(YesNoWithText.unapply)
+      .verifying("required", YesNoWithText.validateOnNo _)
+
+  val form = Form(mapping(
+    stillCaringMapping,
+    "startOfSelfEmployment" -> dayMonthYear.verifying(validDate),
+    "typeOfBusiness" -> carersNonEmptyText(maxLength = 60),
+    "totalOverWeeklyIncomeThreshold" -> nonEmptyText.verifying(validYesNo)
+  )(CircumstancesSelfEmployment.apply)(CircumstancesSelfEmployment.unapply))
+
+  def present = claiming { implicit circs => implicit request =>
+    track(CircumstancesSelfEmployment) {
+      implicit circs => Ok(views.html.circs.s2c_self_employment.g1_selfEmployment(form.fill(CircumstancesSelfEmployment)))
+    }
+  }
+
+  def submit = claiming { implicit circs => implicit request =>
+    form.bindEncrypted.fold(
+      formWithErrors => BadRequest(views.html.circs.s2c_self_employment.g1_selfEmployment(formWithErrors)),
+      f => circs.update(f) -> Redirect(controllers.circs.s3_consent_and_declaration.routes.G1Declaration.present())
+    )
+  }
+}
--- c3/app/models/domain/CircumstancesAdditionalInfo.scala
+++ c3/app/models/domain/CircumstancesAdditionalInfo.scala
@@ -1,5 +1,8 @@
 package models.domain
 
+import models.yesNo.YesNoWithText
+import models.DayMonthYear
+
 case object CircumstancesAdditionalInfo extends Section.Identifier {
   val id = "c2"
   //override val expectedMinTimeToCompleteInMillis: Long = 10000
@@ -8,5 +11,15 @@ case object CircumstancesAdditionalInfo extends Section.Identifier {
 case class CircumstancesOtherInfo(change: String = "") extends QuestionGroup(CircumstancesOtherInfo)
 
 object CircumstancesOtherInfo extends QuestionGroup.Identifier {
+  val id = s"${CircumstancesAdditionalInfo.id}.g2"
+}
+
+case class CircumstancesSelfEmployment(stillCaring: YesNoWithText = YesNoWithText("", None),
+                                       startOfSelfEmployment: DayMonthYear = DayMonthYear(),
+                                       typeOfBusiness: String = "",
+                                       totalOverWeeklyIncomeThreshold: String = "")
+  extends QuestionGroup(CircumstancesSelfEmployment)
+
+object CircumstancesSelfEmployment extends QuestionGroup.Identifier {
   val id = s"${CircumstancesAdditionalInfo.id}.g2"
 }
