2e69378e109ffa71bdb192d49524170afcb69114
Mark Hunter <mark@huntersix.co.uk>
2014-02-07 08:34:26 +0000

    US573 - apply bug fix

--- c3/app/controllers/circs/s2_report_changes/G1ReportChanges.scala
+++ c3/app/controllers/circs/s2_report_changes/G1ReportChanges.scala
@@ -26,23 +26,29 @@ object G1ReportChanges extends Controller with CachedChangeOfCircs with Navigabl
   def submit = claiming { implicit circs => implicit request =>
     form.bindEncrypted.fold(
       formWithErrors => BadRequest(views.html.circs.s2_report_changes.g1_reportChanges(formWithErrors)),
-      f => circs.update(f) -> {
-        if (f.reportChanges == StoppedCaring.name) {
-          val updatedCircs = circs.update(new CircumstancesSelfEmployment())
-          val updatedCircs2 = updatedCircs.update(new CircumstancesOtherInfo())
-          Redirect(controllers.circs.s2_report_changes.routes.G3PermanentlyStoppedCaring.present())
-        }
-        else if (f.reportChanges == app.ReportChange.SelfEmployment.name) {
-          val updatedCircs = circs.update(new CircumstancesStoppedCaring())
-          val updatedCircs2 = updatedCircs.update(new CircumstancesOtherInfo())
-          Redirect(controllers.circs.s2_report_changes.routes.G2SelfEmployment.present())
-        }
-        else {
-          val updatedCircs = circs.update(new CircumstancesStoppedCaring())
-          val updatedCircs2 = updatedCircs.update(new CircumstancesSelfEmployment())
-          Redirect(controllers.circs.s2_report_changes.routes.G4OtherChangeInfo.present())
-        }
+      form => {
+        val c = updateCircs(form, circs)
+        c
       }
     )
+  }
+
+  def updateCircs(f:ReportChanges, circs:Claim) = {
+    // for qs groups under this section, if it is not reportedChange - delete
+    if (f.reportChanges == StoppedCaring.name) {
+      var updatedCircs = circs.delete(CircumstancesSelfEmployment)
+      var updatedCircs2 = updatedCircs.delete(CircumstancesOtherInfo)
+      updatedCircs2.update(f) -> Redirect(controllers.circs.s2_report_changes.routes.G3PermanentlyStoppedCaring.present())
+    }
+    else if (f.reportChanges == app.ReportChange.SelfEmployment.name) {
+      var updatedCircs = circs.delete(CircumstancesStoppedCaring)
+      var updatedCircs2 = updatedCircs.delete(CircumstancesOtherInfo)
+      updatedCircs2.update(f) -> Redirect(controllers.circs.s2_report_changes.routes.G2SelfEmployment.present())
+    }
+    else {
+      var updatedCircs = circs.delete(CircumstancesStoppedCaring)
+      var updatedCircs2 = updatedCircs.delete(CircumstancesSelfEmployment)
+      updatedCircs2.update(f) -> Redirect(controllers.circs.s2_report_changes.routes.G4OtherChangeInfo.present())
+    }
   }
 }
