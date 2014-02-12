e6103e7cd5f52f43a4460ac4512a11b1960d9bcb
Nedda Kaltcheva <nedda.kaltcheva@gmail.com>
2014-02-12 14:06:31 +0000

    US505 - redesign of thank you page - added NI number for thank you page on claim submit

--- c3/app/controllers/ClaimEnding.scala
+++ c3/app/controllers/ClaimEnding.scala
@@ -5,16 +5,16 @@ import models.view.CachedClaim
 
 object ClaimEnding extends Controller with CachedClaim {
 
-  def timeout = ending {
+  def timeout = ending {implicit claim => implicit request  =>
     Ok(views.html.common.session_timeout(startPage))
   }
 
-  def error = ending {
+  def error = ending {implicit claim => implicit request  =>
     Ok(views.html.common.error(startPage))
   }
 
-  def thankyou = claiming { implicit claim => implicit request  =>
-    Ok(views.html.common.thankYouClaim(claim))
+  def thankyou = ending { implicit claim => implicit request  =>
+    Ok(views.html.common.thankYouClaim())
   }
 
 }
