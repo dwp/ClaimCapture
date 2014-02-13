e760012dc0102c3e7f317769a9ce95d14b5ac1a1
Mark Hunter <mark@huntersix.co.uk>
2014-02-13 09:36:15 +0000

    US533 - Add form, unit and integration tests

--- c3/test/utils/pageobjects/CircumstancesPageFactory.scala
+++ c3/test/utils/pageobjects/CircumstancesPageFactory.scala
@@ -3,7 +3,7 @@ package utils.pageobjects
 import play.api.test.TestBrowser
 import utils.pageobjects.circumstances.s1_about_you._
 import utils.pageobjects.circumstances.s3_consent_and_declaration.G1DeclarationPage
-import utils.pageobjects.circumstances.s2_report_changes.{G1ReportChangesPage, G2SelfEmploymentPage, G3PermanentlyStoppedCaringPage, G4OtherChangeInfoPage}
+import utils.pageobjects.circumstances.s2_report_changes._
 import controllers.circs.s2_report_changes.{G2SelfEmployment, G1ReportChanges}
 
 object CircumstancesPageFactory extends PageFactory {
@@ -19,6 +19,7 @@ object CircumstancesPageFactory extends PageFactory {
       case G2SelfEmploymentPage.title => G2SelfEmploymentPage (ctx)
       case G3PermanentlyStoppedCaringPage.title => G3PermanentlyStoppedCaringPage (ctx)
       case G4OtherChangeInfoPage.title => G4OtherChangeInfoPage (ctx)
+      case G5PaymentChangePage.title => G5PaymentChangePage (ctx)
       case G1DeclarationPage.title => G1DeclarationPage (ctx)
       case _ => new UnknownPage(title, ctx)
     }
