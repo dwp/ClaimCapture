763c2d24535ed4b3bd1d5a8f018e41232861d01f
Nedda Kaltcheva <nedda.kaltcheva@gmail.com>
2014-02-17 13:09:47 +0000

    US539 - add new Change of Address page to circs

---
+++ c3/app/models/yesNo/YesNoComplex.scala
@@ -0,0 +1,25 @@
+package models.yesNo
+
+/**
+ * Created by neddakaltcheva on 2/14/14.
+ */
+import controllers.Mappings._
+import models.MultiLineAddress
+import models.yesNo.YesNoWithAddress
+
+case class YesNoComplex(answer: String = "", address: YesNoWithAddress = YesNoWithAddress("", None, None))
+
+object YesNoComplex {
+
+  def validateOnYes(input: YesNoComplex): Boolean = input.answer match {
+    case `yes` => input.address.answer.isDefinedAt(0)
+    case `no` => true
+  }
+
+  def validateOnNo(input: YesNoComplex): Boolean = input.answer match {
+    case `yes` => true
+    case `no` => input.address.answer.isDefinedAt(0)
+  }
+
+  def validateAnswerNotEmpty(input: YesNoComplex): Boolean = !input.answer.isEmpty
+}
