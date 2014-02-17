3f96bc96bb017380e100aa9ea207bef0728156b1
Phil Barton <phil.barton@valtech.co.uk>
2014-02-17 21:41:41 +0000

    US543 - Add request timeout and spoof service unavailable

--- c3/app/services/submission/WebServiceClientComponent.scala
+++ c3/app/services/submission/WebServiceClientComponent.scala
@@ -9,6 +9,7 @@ import play.api.libs.ws.{Response, WS}
 import services.util.CharacterStripper
 import java.net.ConnectException
 import ExecutionContext.Implicits.global
+import java.util.concurrent.TimeoutException
 
 trait WebServiceClientComponent {
 
@@ -20,11 +21,18 @@ trait WebServiceClientComponent {
         Configuration.root().getString("submissionServerUrl", "SubmissionServerEndpointNotSet") + "submit/claim"
       Logger.debug(s"Submission Server : $submissionServerEndpoint")
       val result = WS.url(submissionServerEndpoint)
+        .withRequestTimeout(60000) // wait 1 minute
         .withHeaders(("Content-Type", "text/xml"))
         .post(CharacterStripper.stripNonPdf(claimSubmission.buildString(stripComments = true))) recover {
 
         case e: ConnectException =>
           Logger.error(s"ConnectException ! ${e.getMessage}")
+          // Spoof service unavailable
+          new Response(null) {
+            override def status: Int = http.Status.SERVICE_UNAVAILABLE
+          }
+        case e: TimeoutException =>
+          Logger.error(s"TimeoutException ! ${e.getMessage}")
           // Spoof service unavailable
           new Response(null) {
             override def status: Int = http.Status.SERVICE_UNAVAILABLE
