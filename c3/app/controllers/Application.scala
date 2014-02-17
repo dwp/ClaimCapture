89afd4b0d28717a5e81fb9fee952346868cc2b47
Phil Barton <phil.barton@valtech.co.uk>
2014-02-17 09:39:39 +0000

    US544 - Remove referer toggle

--- c3/app/controllers/Application.scala
+++ c3/app/controllers/Application.scala
@@ -6,18 +6,9 @@ import models.view.CachedClaim
 
 object Application extends Controller with CachedClaim {
 
-  // Feature toggle for new referer mechanism. See US544
-  val gdsPageOk: Boolean = getProperty("gov.uk.start.ok", default = true)
-  val startUrl: String = getProperty("claim.start.page", "/allowance/benefits")
-
   val govUk: String = getProperty("gov.uk.start.page", "https://www.gov.uk/apply-carers-allowance")
 
   def index = Action {
-    if (gdsPageOk) {
-      MovedPermanently(govUk)
-    } else {
-      // Temporary
-      Redirect(startUrl)
-    }
+    MovedPermanently(govUk)
   }
 }
