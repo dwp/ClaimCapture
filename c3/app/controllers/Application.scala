8f9964054978a36b9067bbe718f6caa3e676384d
Phil Barton <phil.barton@valtech.co.uk>
2014-02-28 15:10:46 +0000

    Refactor property reading

--- c3/app/controllers/Application.scala
+++ c3/app/controllers/Application.scala
@@ -2,16 +2,9 @@ package controllers
 
 import app.ConfigProperties._
 import play.api.mvc._
-import models.view.CachedClaim
-import play.api.{Logger, Play}
-import play.api.Play.current
 
-object Application extends Controller with CachedClaim {
-
-  val govUk: String = getProperty("gov.uk.start.page", "https://www.gov.uk/apply-carers-allowance")
-  val welshAvailable = getProperty("galluogi.cymraeg", "false")
-
+object Application extends Controller {
   def index = Action {
-    MovedPermanently(govUk)
+    MovedPermanently(getProperty("gov.uk.start.page", "https://www.gov.uk/apply-carers-allowance"))
   }
 }
