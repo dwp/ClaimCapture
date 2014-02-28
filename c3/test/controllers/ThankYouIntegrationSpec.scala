f473a6cfafd67f8deaf1642ebd79f2e3f90d2db6
Phil Barton <phil.barton@valtech.co.uk>
2014-02-28 15:25:44 +0000

    Fix Test

--- c3/test/controllers/ThankYouIntegrationSpec.scala
+++ c3/test/controllers/ThankYouIntegrationSpec.scala
@@ -6,8 +6,15 @@ import play.api.test.WithBrowser
 class ThankYouIntegrationSpec extends Specification with Tags {
   "Thank You" should {
     "present 'Thank You' page" in new WithBrowser with BrowserMatchers {
-      browser.goTo("/thankyou")
-      titleMustEqual("GOV.UK - The best place to find government services and information")
+      browser.goTo("/thankyou/apply-carers")
+      titleMustEqual("Application complete")
+    }
+  } section "integration"
+
+  "Change Thank You" should {
+    "present 'Thank You' page" in new WithBrowser with BrowserMatchers {
+      browser.goTo("/thankyou/change-carers")
+      titleMustEqual("Change complete")
     }
   } section "integration"
 }
