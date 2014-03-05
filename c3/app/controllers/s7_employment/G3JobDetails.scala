604f262f2bd4d72d17303b80c1d3f53a11ebfd22
Mark Hunter <mark@huntersix.co.uk>
2014-03-05 11:32:13 +0000

    US536 - Past/Present labels for employment job details

--- c3/app/controllers/s7_employment/G3JobDetails.scala
+++ c3/app/controllers/s7_employment/G3JobDetails.scala
@@ -46,12 +46,8 @@ object G3JobDetails extends Controller with CachedClaim with Navigable {
   def submit = claimingWithCheckInJob { jobID => implicit claim => implicit request => implicit lang =>
     form.bindEncrypted.fold(
       formWithErrors =>{
-        val pastPresent = formWithErrors("finishedThisJob").value match {
-          case Some(v) => if (v.toLowerCase == no) "do you" else "did you"
-          case _ => "do you"
-        }
         val form = formWithErrors.replaceError("", "lastWorkDate", FormError("lastWorkDate", "error.required"))
-        .replaceError("hoursPerWeek","number.invalid",FormError("hoursPerWeek","number.invalid", Seq(pastPresent)))
+        .replaceError("hoursPerWeek","number.invalid",FormError("hoursPerWeek","number.invalid", Seq(labelForEmployment(claim, "hoursPerWeek", jobID))))
         BadRequest(views.html.s7_employment.g3_jobDetails(form))
       },jobDetails => claim.update(jobs.update(jobDetails)) -> Redirect(routes.G4EmployerContactDetails.present(jobID)))
   }
