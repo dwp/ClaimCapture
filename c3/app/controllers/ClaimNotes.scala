c9f8e5f10e0ac814de6546d6a07eab939082abd0
Mark Hunter <mark@huntersix.co.uk>
2014-03-05 16:38:19 +0000

    US536 - Start to convert claim pack notes to use messages for language switching

--- c3/app/controllers/ClaimNotes.scala
+++ c3/app/controllers/ClaimNotes.scala
@@ -1,15 +1,18 @@
 package controllers
 
-import play.api.mvc.{Controller, Action}
+import app.ConfigProperties._
+import play.api.mvc.{AnyContent, Request, Controller, Action}
 import com.github.rjeschke.txtmark.Processor
 import play.api.templates.Html
 import views.html.claimNotes.markdownWrapper
+import models.view.CachedClaim
+import play.api.i18n.Lang
 
 
-object ClaimNotes extends Controller{
+object ClaimNotes extends Controller with CachedClaim {
 
-  def notes() = Action{
-    implicit request =>
+  def notes() = claiming {
+    implicit claim => implicit request => implicit lang =>
 
     val t = views.html.claimNotes.main()
     val string = t.body
