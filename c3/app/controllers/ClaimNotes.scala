d88529adbcc3fb8fc946558fc0b32aad970f2c27
Phil Barton <phil.barton@valtech.co.uk>
2014-03-13 11:15:36 +0000

    DE489 - claim-pack notes url is restricted to the carers site only.

--- c3/app/controllers/ClaimNotes.scala
+++ c3/app/controllers/ClaimNotes.scala
@@ -1,18 +1,15 @@
 package controllers
 
-import app.ConfigProperties._
-import play.api.mvc.{AnyContent, Request, Controller, Action}
+import play.api.mvc.{Controller, Action}
 import com.github.rjeschke.txtmark.Processor
 import play.api.templates.Html
 import views.html.claimNotes.markdownWrapper
 import models.view.CachedClaim
-import play.api.i18n.Lang
 
 
 object ClaimNotes extends Controller with CachedClaim {
 
-  def notes() = claiming {
-    implicit claim => implicit request => implicit lang =>
+  def notes() = Action {
 
     val t = views.html.claimNotes.main()
     val string = t.body
