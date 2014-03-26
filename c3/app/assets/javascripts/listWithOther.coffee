49d2a923319488f4f54875556ffa07b6c4270f87
prafullac <prafulla.chandra@valtech.co.uk>
2014-03-26 21:27:00 +0000

    US538: Added Functional tests

--- c3/app/assets/javascripts/listWithOther.coffee
+++ c3/app/assets/javascripts/listWithOther.coffee
@@ -1,10 +1,10 @@
 $ ->
 
   $("input[listWithOther=true]").each ->
-    $(this).closest("ul").parents("li").next().next().hide() if (($(this).val() == "Somewhere_else") && ($(this).is("checked") == false))
+    $(this).closest("ul").parents("li").next().next().hide() if (($(this).val() == "Somewhere else") && ($(this).is("checked") == false))
 
   $("input[listWithOther=true]").change ->
-    if $(this).val() is "Somewhere_else"
+    if $(this).val() is "Somewhere else"
       $(this).closest("ul").parents("li").next().next().slideDown()
     else
       textArea = $(this).closest("ul").parents("li").next().next().find("textarea")
--- c3/test/app/circumstances/FunctionalTestCase24Spec.scala
+++ c3/test/app/circumstances/FunctionalTestCase24Spec.scala
@@ -7,14 +7,14 @@ import utils.pageobjects.circumstances.s1_about_you.G1ReportAChangeInYourCircums
 import utils.pageobjects.xml_validation.{XMLCircumstancesBusinessValidation, XMLBusinessValidation}
 
 
-class FunctionalTestCase22Spec extends FunctionalTestCommon {
+class FunctionalTestCase24Spec extends FunctionalTestCommon {
   isolated
 
   "The application Circumstances" should {
-    "Successfully run absolute Circumstances Test Case 22 for Break from caring" in new WithBrowser with PageObjects {
+    "Successfully run absolute Circumstances Test Case 24 for Break from caring" in new WithBrowser with PageObjects {
 
       val page = G1ReportAChangeInYourCircumstancesPage(context)
-      val circs = TestData.readTestDataFromFile("/functional_scenarios/circumstances/TestCase22.csv")
+      val circs = TestData.readTestDataFromFile("/functional_scenarios/circumstances/TestCase24.csv")
       page goToThePage()
 
       val lastPage = page runClaimWith(circs, XmlPage.title)
@@ -28,4 +28,5 @@ class FunctionalTestCase22Spec extends FunctionalTestCommon {
       }
     }
   } section "functional"
+
 }
