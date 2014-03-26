909dc9db1c75a2d2427eff033e87bf01e3f68a31
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-03-26 10:43:43 +0000

    US543 Improving tests a little bit. Adding SUCCESS case.

--- c3/test/services/submission/AsyncClaimSubmissionServiceSpec.scala
+++ c3/test/services/submission/AsyncClaimSubmissionServiceSpec.scala
@@ -22,8 +22,10 @@ class AsyncClaimSubmissionServiceSpec extends Specification with Mockito with Ta
     </response>
   }
 
-  def asyncService( status:Int=200, transactionId:String, result: String= "", correlationID: String="", messageClass:String="", errorCode: String="", pollEndpoint: String ="") =
-    new AsyncClaimSubmissionService with ClaimTransactionComponent with WebServiceClientComponent {
+  def asyncService( status:Int=200, transactionId:String, result: String= "",
+                    correlationID: String="", messageClass:String="",
+                    errorCode: String="", pollEndpoint: String ="") = new AsyncClaimSubmissionService with ClaimTransactionComponent
+                                                                                                      with WebServiceClientComponent {
       import ExecutionContext.Implicits.global
       val webServiceClient = mock[WebServiceClient]
       val response = mock[Response]
@@ -35,75 +37,78 @@ class AsyncClaimSubmissionServiceSpec extends Specification with Mockito with Ta
       org.mockito.Mockito.doReturn(transactionId).when(claimTransaction).generateId
   }
 
-  "claim submission should record the correct status based on WS call results" should {
+  val transactionId = "1234567"
+
+  "claim submission" should {
     "record BAD_REQUEST" in new WithApplicationAndDB {
-      val transactionId = "1234567"
+
       val service = asyncService(http.Status.BAD_REQUEST,transactionId)
 
       DBTests.createId(transactionId)
       service.submission(new Claim with FullClaim)
 
-      Thread.sleep(5000)
+      Thread.sleep(500)
       val transactionStatus = DBTests.getId(transactionId)
-
-      transactionStatus must not beEmpty
 
       transactionStatus mustEqual Some(TransactionStatus(transactionId,AsyncClaimSubmissionService.BAD_REQUEST_ERROR,1,Some(0),None,Some("en")))
 
     }
-  }
 
-  "claim submission should record the correct status based on WS call results" should {
+    "record SUCCESS" in new WithApplicationAndDB {
+
+      val service = asyncService(http.Status.OK,transactionId,result = "response")
+
+      DBTests.createId(transactionId)
+      service.submission(new Claim with FullClaim)
+
+      Thread.sleep(500)
+      val transactionStatus = DBTests.getId(transactionId)
+
+      transactionStatus mustEqual Some(TransactionStatus(transactionId,AsyncClaimSubmissionService.SUCCESS,1,Some(0),None,Some("en")))
+
+    }
+
     "record SERVICE_UNAVAILABLE" in new WithApplicationAndDB {
-      val transactionId = "1234567"
+
       val service = asyncService(http.Status.SERVICE_UNAVAILABLE,transactionId)
 
       DBTests.createId(transactionId)
       service.submission(new Claim with FullClaim)
 
-      Thread.sleep(5000)
+      Thread.sleep(500)
       val transactionStatus = DBTests.getId(transactionId)
-
-      transactionStatus must not beEmpty
 
       transactionStatus mustEqual Some(TransactionStatus(transactionId,AsyncClaimSubmissionService.SERVICE_UNAVAILABLE,1,Some(0),None,Some("en")))
 
     }
-  }
 
-  "claim submission should record the correct status based on WS call results" should {
     "record REQUEST_TIMEOUT_ERROR" in new WithApplicationAndDB {
-      val transactionId = "1234567"
+
       val service = asyncService(http.Status.REQUEST_TIMEOUT,transactionId)
 
       DBTests.createId(transactionId)
       service.submission(new Claim with FullClaim)
 
-      Thread.sleep(5000)
+      Thread.sleep(500)
       val transactionStatus = DBTests.getId(transactionId)
-
-      transactionStatus must not beEmpty
 
       transactionStatus mustEqual Some(TransactionStatus(transactionId,AsyncClaimSubmissionService.REQUEST_TIMEOUT_ERROR,1,Some(0),None,Some("en")))
 
     }
-  }
 
-  "claim submission should record the correct status based on WS call results" should {
     "record SERVER_ERROR" in new WithApplicationAndDB {
-      val transactionId = "1234567"
+
       val service = asyncService(http.Status.INTERNAL_SERVER_ERROR,transactionId)
 
       DBTests.createId(transactionId)
       service.submission(new Claim with FullClaim)
 
-      Thread.sleep(5000)
+      Thread.sleep(500)
       val transactionStatus = DBTests.getId(transactionId)
-
-      transactionStatus must not beEmpty
 
       transactionStatus mustEqual Some(TransactionStatus(transactionId,AsyncClaimSubmissionService.SERVER_ERROR,1,Some(0),None,Some("en")))
 
     }
   }
+
 }
