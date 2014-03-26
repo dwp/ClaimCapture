4c8897e330199f2262431fdc96474593cd4fde9c
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-03-26 16:00:26 +0000

    US543 Added lost flow redirections.

--- c3/app/controllers/submission/StatusRoutingController.scala
+++ c3/app/controllers/submission/StatusRoutingController.scala
@@ -15,16 +15,16 @@ object StatusRoutingController extends Controller with CachedClaim with ClaimTra
   }
 
   def submit = claiming { implicit claim => implicit request => implicit lang =>
-
+    import AsyncClaimSubmissionService._
     val transactionStatus = claimTransaction.getTransactionStatusById(claim.transactionId.getOrElse(""))
 
     //TODO: Check for other cases where we submit to thankyou page
     transactionStatus match {
-      case Some(ts) if ts.status == AsyncClaimSubmissionService.SUCCESS => Redirect(controllers.routes.ClaimEnding.thankyou)
+      case Some(ts) if ts.status == SUCCESS   || ts.status == ACKNOWLEDGED          => Redirect(controllers.routes.ClaimEnding.thankyou)
+      case Some(ts) if ts.status == GENERATED || ts.status == SUBMITTED             => Redirect(controllers.submission.routes.StatusRoutingController.present)
       case Some(ts) if ts.status == AsyncClaimSubmissionService.SERVICE_UNAVAILABLE => Redirect(controllers.submission.routes.StatusRoutingController.errorRetry)
-      case Some(ts) if ts.status == AsyncClaimSubmissionService.GENERATED => Redirect(controllers.submission.routes.StatusRoutingController.present)
-      case None => Redirect(controllers.routes.ClaimEnding.timeout)
-      case _ => Redirect(controllers.submission.routes.StatusRoutingController.error)
+      case None                                                                     => Redirect(controllers.routes.ClaimEnding.timeout)
+      case _                                                                        => Redirect(controllers.submission.routes.StatusRoutingController.error)
     }
 
   }
--- c3/app/services/submission/AsyncClaimSubmissionService.scala
+++ c3/app/services/submission/AsyncClaimSubmissionService.scala
@@ -84,14 +84,14 @@ trait AsyncClaimSubmissionService {
 }
 
 object AsyncClaimSubmissionService{
-  val GENERATED = "0100"
-  val SUBMITTED = "0000"
-  val ACKNOWLEDGED = "0001"
-  val SUCCESS = "0002"
-  val UNKNOWN_ERROR = "9001"
-  val BAD_REQUEST_ERROR = "9002"
-  val REQUEST_TIMEOUT_ERROR = "9003"
-  val SERVER_ERROR = "9004"
-  val COMMUNICATION_ERROR = "9005"
-  val SERVICE_UNAVAILABLE = "9006"
+  val GENERATED = "0100" //submitting
+  val SUBMITTED = "0000" //submitting
+  val ACKNOWLEDGED = "0001" //thankyou
+  val SUCCESS = "0002" //thankyou
+  val UNKNOWN_ERROR = "9001" //error
+  val BAD_REQUEST_ERROR = "9002" //error
+  val REQUEST_TIMEOUT_ERROR = "9003"//error
+  val SERVER_ERROR = "9004"//error
+  val COMMUNICATION_ERROR = "9005"//error
+  val SERVICE_UNAVAILABLE = "9006"//errorretry
 }
