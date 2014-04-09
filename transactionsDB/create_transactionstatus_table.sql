87fa01a5b2d397bad18a0fdb1ccd0428c76dc9aa
Phil Barton <phil.barton@valtech.co.uk>
2014-04-09 09:57:16 +0100

    US666 - record lang in db - add to create as well as update

--- transactionsDB/create_transactionstatus_table.sql
+++ transactionsDB/create_transactionstatus_table.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS "public"."transactionstatus" CASCADE;
 CREATE TABLE transactionstatus
-    ( transaction_id CHARACTER(7) NOT NULL, createdon TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT now() NOT NULL, status CHARACTER VARYING(10), type INTEGER,circs_type INTEGER, PRIMARY KEY(transaction_id), CONSTRAINT transaction_fk FOREIGN KEY(transaction_id) REFERENCES transactionids(transaction_id)
+    ( transaction_id CHARACTER(7) NOT NULL, createdon TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT now() NOT NULL, status CHARACTER VARYING(10), type INTEGER,circs_type INTEGER, lang CHARACTER VARYING(10), PRIMARY KEY(transaction_id), CONSTRAINT transaction_fk FOREIGN KEY(transaction_id) REFERENCES transactionids(transaction_id)
     ) ;
