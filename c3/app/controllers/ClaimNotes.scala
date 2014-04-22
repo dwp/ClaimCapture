8f09d625cf1d856aa55366bfdf44276963b59a3c
Phil Barton <phil.barton@valtech.co.uk>
2014-04-22 12:57:31 +0100

    DE503 - Click Claim Pack notes in Change of Circs leads to "Session Timeout"

--- c3/app/controllers/ClaimNotes.scala
+++ c3/app/controllers/ClaimNotes.scala
@@ -4,13 +4,13 @@ import play.api.mvc.{SimpleResult, Action, Controller}
 import com.github.rjeschke.txtmark.Processor
 import play.api.templates.Html
 import views.html.claimNotes.markdownWrapper
-import models.view.CachedClaim
+import play.api.i18n.Lang
 
 
-object ClaimNotes extends Controller with CachedClaim {
+object ClaimNotes extends Controller {
 
-  def internalNotes() = claiming {
-    implicit claim => implicit request => implicit lang =>
+  def internalNotes(languageCode:String) = Action {
+      implicit var lang = Lang(languageCode)
       val t = views.html.claimNotes.main()
       buildResponse(t)
   }
