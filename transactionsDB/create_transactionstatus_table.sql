9713e7b2d35e1c0a997901d8e2d23c2838db639e
Jorge Migueis <jorge.migueis@valtech.co.uk>
2014-07-01 15:01:36 +0100

    Fixed issue transactionstatus table did not have thirdparty defined in create script but is in alter script.

--- transactionsDB/create_transactionstatus_table.sql
+++ transactionsDB/create_transactionstatus_table.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS "public"."transactionstatus" CASCADE;
 CREATE TABLE transactionstatus
-    ( transaction_id CHARACTER(7) NOT NULL, createdon TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT now() NOT NULL, status CHARACTER VARYING(10), type INTEGER,circs_type INTEGER, lang CHARACTER VARYING(10), js_enabled INTEGER, PRIMARY KEY(transaction_id), CONSTRAINT transaction_fk FOREIGN KEY(transaction_id) REFERENCES transactionids(transaction_id)
+    ( transaction_id CHARACTER(7) NOT NULL, createdon TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT now() NOT NULL, status CHARACTER VARYING(10), type INTEGER, thirdparty INTEGER, circs_type INTEGER, lang CHARACTER VARYING(10), js_enabled INTEGER, PRIMARY KEY(transaction_id), CONSTRAINT transaction_fk FOREIGN KEY(transaction_id) REFERENCES transactionids(transaction_id)
     ) ;
