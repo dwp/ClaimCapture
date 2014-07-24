aecd4786f6c9405cc0f508c9f948bab927c7c53c
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-07-24 13:26:24 +0100

    US863

--- c3/app/monitoring/ClaimReceivedConnectionCheck.scala
+++ c3/app/monitoring/ClaimReceivedConnectionCheck.scala
@@ -18,11 +18,14 @@ import scala.language.{implicitConversions, postfixOps}
  * Try to ping the ClaimReceived service in IL3.
  */
 class ClaimReceivedConnectionCheck extends HealthCheck {
+
+  implicit def stringGetWrapper(s:String) = new HttpMethodWrapper(s,ConfigProperties.getProperty("cr.timeout",60000).milliseconds)
+
   override def check(): Result = {
     val submissionServerEndpoint: String =
       Configuration.root().getString("submissionServerUrl", "SubmissionServerEndpointNotSet") + "ping"
 
-    new HttpMethodWrapper(submissionServerEndpoint, ConfigProperties.getProperty("cr.timeout",60000).milliseconds)  get { response: Response =>
+    submissionServerEndpoint get { response: Response =>
       response.status match {
         case Status.OK =>
           Result.healthy
