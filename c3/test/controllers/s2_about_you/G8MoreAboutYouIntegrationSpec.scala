ba426201ae4fbc8aee1e0d2a6bb4ca87423f5d4b
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-08-29 16:01:15 +0100

    DE681:Fixed - More about you

--- c3/test/controllers/s2_about_you/G8MoreAboutYouIntegrationSpec.scala
+++ c3/test/controllers/s2_about_you/G8MoreAboutYouIntegrationSpec.scala
@@ -37,5 +37,16 @@ class G8MoreAboutYouIntegrationSpec extends Specification with Tags {
       page goToThePage()
       page runClaimWith (claim, G1YourPartnerPersonalDetailsPage.title)
     }
+
+    "have you had a spouse must be visble when you navigate back" in new WithBrowser with PageObjects{
+      val page =  G1YourDetailsPage(context)
+      val claim = ClaimScenarioFactory.s2AboutYouWithTimeOutside
+      page goToThePage()
+      page runClaimWith (claim, G1YourPartnerPersonalDetailsPage.title)
+      page goBack()
+      browser.find("#hadPartnerSinceClaimDate").size() mustEqual 1
+    }
+
+
   } section("integration", models.domain.AboutYou.id)
 }
