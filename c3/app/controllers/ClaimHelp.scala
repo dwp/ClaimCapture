2694852b716d64650f3c1562866f4d41def92a80
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-09-29 12:15:58 +0100

    DE716 DE717

--- c3/app/controllers/ClaimHelp.scala
+++ c3/app/controllers/ClaimHelp.scala
@@ -1,23 +1,32 @@
 package controllers
 
-import play.api.mvc.{SimpleResult, Action, Controller}
+import play.api.mvc.{Results, SimpleResult, Action, Controller}
 import com.github.rjeschke.txtmark.Processor
 import play.api.templates.Html
 import views.html.claimNotes.markdownWrapper
 import play.api.i18n.Lang
-import models.view.{Navigable, CachedClaim}
+import models.view.{CachedChangeOfCircs, Navigable, CachedClaim}
 
+object Help {
+  def buildResponse(t: Html): SimpleResult = {
+    val string = t.body
+    val html = Processor.process(string)
+    Results.Ok(t)
+  }
+}
 
 object ClaimHelp extends Controller with CachedClaim with Navigable {
 
   def notes() = claiming { implicit claim => implicit request => implicit lang =>
-    val t = views.html.claimHelp.claimHelpNotes()
-    buildResponse(t)
+    Help.buildResponse(views.html.claimHelp.claimHelpNotes())
   }
 
-  private def buildResponse(t: Html): SimpleResult = {
-    val string = t.body
-    val html = Processor.process(string)
-    Ok(t)
+}
+
+object CofcHelp extends Controller with CachedChangeOfCircs with Navigable {
+
+  def notes() = claiming { implicit claim => implicit request => implicit lang =>
+    Help.buildResponse(views.html.claimHelp.claimHelpNotes())
   }
+
 }
