db43a5a0e69da7dec779e8654745d37002d16c7c
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-10-14 14:43:20 +0100

    US966 - Update existing preview -Employment

---
+++ c3/test/controllers/preview/PreviewPageEmploymentContentSpec.scala
@@ -0,0 +1,75 @@
+package controllers.preview
+
+import org.specs2.mutable.{Tags, Specification}
+import play.api.test.WithBrowser
+import utils.pageobjects.{PageObjectsContext, PageObjects}
+import utils.pageobjects.preview.PreviewPage
+import utils.pageobjects.s1_2_claim_date.G1ClaimDatePage
+import controllers.ClaimScenarioFactory
+import utils.pageobjects.s7_employment.G1EmploymentPage
+
+
+class PreviewPageEmploymentContentSpec extends Specification with Tags {
+
+  "Preview Page" should {
+    "display employment data" in new WithBrowser with PageObjects{
+
+      fillEmploymentSection(context)
+      val page =  PreviewPage(context)
+      page goToThePage()
+      val source = page.source()
+
+      source.contains("Employment and Self Employment") must beTrue
+      source.contains("Have you been employed at any time since 10/04/2014") must beTrue
+      source.contains("Yes") must beTrue
+      source.contains("Employment") must beTrue
+      source.contains("Details provided for Tesco's, including expenses") must beTrue
+      source.contains("Have you been self employed at any time since 03/10/2014") must beTrue
+      source.contains("Yes - Details provided including expenses") must beTrue
+      source.contains("Type of work or business?") must beTrue
+      source.contains("Some type of business") must beTrue
+    }
+  }
+
+  def fillEmploymentSection (context:PageObjectsContext) = {
+
+    val employmentData = ClaimScenarioFactory.s7Employment()
+    val selfEmploymentData = ClaimScenarioFactory.s9SelfEmployment
+
+    val claimDatePage = G1ClaimDatePage(context)
+    claimDatePage goToThePage()
+    val claimDate = ClaimScenarioFactory.s12ClaimDate()
+    claimDatePage fillPageWith claimDate
+    claimDatePage submitPage()
+
+    val employmentPage = G1EmploymentPage(context)
+    employmentPage goToThePage ()
+    employmentPage fillPageWith employmentData
+
+    val selfEmploymentPage = employmentPage submitPage()
+    selfEmploymentPage fillPageWith selfEmploymentData
+
+    val selfEmployedAccountsPage = selfEmploymentPage submitPage()
+    selfEmployedAccountsPage fillPageWith selfEmploymentData
+
+    val selfEmployedPensionsPage = selfEmployedAccountsPage submitPage()
+    selfEmployedPensionsPage fillPageWith selfEmploymentData
+
+    val jobDetailsPage = selfEmployedPensionsPage submitPage()
+    jobDetailsPage fillPageWith employmentData
+
+    val lastWagePage = jobDetailsPage submitPage()
+    lastWagePage fillPageWith employmentData
+
+    val pensionsPage = lastWagePage submitPage()
+    pensionsPage fillPageWith employmentData
+
+    val beenEmployedPage = pensionsPage submitPage()
+    employmentData.EmploymentHaveYouBeenEmployedAtAnyTime_1 = "No"
+    beenEmployedPage fillPageWith employmentData
+
+    beenEmployedPage submitPage()
+
+  }
+
+}
