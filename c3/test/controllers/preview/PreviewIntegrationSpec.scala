58af4e3d53689f73c817298e2539b204cbddf723
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-10-15 17:39:43 +0100

    US968 - Added tests for back button navigation

--- c3/test/controllers/preview/PreviewIntegrationSpec.scala
+++ c3/test/controllers/preview/PreviewIntegrationSpec.scala
@@ -7,6 +7,8 @@ import utils.pageobjects.preview.PreviewPage
 import utils.pageobjects.s10_information.G1AdditionalInfoPage
 import controllers.ClaimScenarioFactory
 import utils.pageobjects.s12_consent_and_declaration.G2DisclaimerPage
+import utils.pageobjects.s9_other_money.G1AboutOtherMoneyPage
+import utils.pageobjects.s11_pay_details.G1HowWePayYouPage
 
 
 class PreviewIntegrationSpec extends Specification with Tags {
@@ -33,5 +35,33 @@ class PreviewIntegrationSpec extends Specification with Tags {
        disclaimerPage must beAnInstanceOf[G2DisclaimerPage]
     }
 
-  }
+    "navigate back to preview page clicking next" in new WithBrowser with PageObjects {
+      val additionalInfoPage = G1AdditionalInfoPage(context)
+      val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
+      additionalInfoPage goToThePage ()
+      additionalInfoPage fillPageWith additionalInfoData
+      val previewPage = additionalInfoPage submitPage()
+      previewPage must beAnInstanceOf[PreviewPage]
+      val additionalPage = previewPage goBack()
+      additionalPage must beAnInstanceOf[G1AdditionalInfoPage]
+      additionalPage submitPage () must beAnInstanceOf[PreviewPage]
+    }
+
+    "navigate back to how we pay you page" in new WithBrowser with PageObjects {
+      val howWePayYouPage = G1HowWePayYouPage(context)
+      howWePayYouPage goToThePage()
+      howWePayYouPage fillPageWith ClaimScenarioFactory.s6PayDetails
+      howWePayYouPage submitPage()
+      val additionalInfoPage = G1AdditionalInfoPage(context)
+      val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
+      additionalInfoPage goToThePage ()
+      additionalInfoPage fillPageWith additionalInfoData
+      val previewPage = additionalInfoPage submitPage()
+      previewPage must beAnInstanceOf[PreviewPage]
+      val additionalPage = previewPage goBack()
+      additionalPage must beAnInstanceOf[G1AdditionalInfoPage]
+      additionalPage goBack () must beAnInstanceOf[G1HowWePayYouPage]
+    }
+
+  }section "preview"
 }
