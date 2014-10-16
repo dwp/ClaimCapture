ddb7bcfb04d4425f720c2fa6378dc9d8bf7fecee
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-10-16 12:44:24 +0100

    US968 - Added navigation for your details

--- c3/test/controllers/s2_about_you/G1YourDetailsIntegrationSpec.scala
+++ c3/test/controllers/s2_about_you/G1YourDetailsIntegrationSpec.scala
@@ -6,7 +6,7 @@ import utils.pageobjects.preview.PreviewPage
 import utils.pageobjects.s2_about_you._
 import utils.pageobjects.s1_carers_allowance.G6ApprovePage
 import controllers.ClaimScenarioFactory
-import utils.pageobjects.{TestData, PageObjectsContext, ClaimPageFactory, PageObjects}
+import utils.pageobjects._
 import utils.pageobjects.s1_2_claim_date.G1ClaimDatePage
 
 class G1YourDetailsIntegrationSpec extends Specification with Tags {
@@ -47,23 +47,15 @@ class G1YourDetailsIntegrationSpec extends Specification with Tags {
       g2 must beAnInstanceOf[G2ContactDetailsPage]
     }
 
-    "Modify title, name, middlename and last name on preview page" in new WithBrowser with PageObjects{
+    "Modify title, name, middlename and last name from preview page" in new WithBrowser with PageObjects{
 
-      val claimDatePage = G1ClaimDatePage(context)
-      claimDatePage goToThePage()
-      val claimDate = ClaimScenarioFactory.s12ClaimDate()
-      claimDatePage fillPageWith claimDate
+      val id = "about_you_full_name"
 
-      val page =  claimDatePage submitPage()
-      val claim = ClaimScenarioFactory.yourDetailsWithNotTimeOutside()
-      page goToThePage()
-      page fillPageWith claim
-      page submitPage()
+      val answerText = textFromXPath(id, _:Page)
 
-      val previewPage = PreviewPage(context)
-      previewPage goToThePage()
-      previewPage source() must contain("Mr John Appleseed")
-      val aboutYou = ClaimPageFactory.buildPageFromFluent(previewPage.click("#about_you_full_name"))
+      val previewPage = goToPreviewPage(context)
+      answerText(previewPage) mustEqual "Mr John Appleseed"
+      val aboutYou = ClaimPageFactory.buildPageFromFluent(previewPage.click(s"#$id"))
 
       aboutYou must beAnInstanceOf[G1YourDetailsPage]
       val modifiedData = new TestData
@@ -75,8 +67,68 @@ class G1YourDetailsIntegrationSpec extends Specification with Tags {
       val previewPageModified = aboutYou submitPage()
 
       previewPageModified must beAnInstanceOf[PreviewPage]
-      previewPageModified source() must contain("Mrs Jane Pearson")
+      answerText(previewPageModified) mustEqual "Mrs Jane Pearson"
 
     }
+
+    "Modify national insurance number from preview page" in new WithBrowser with PageObjects{
+      val previewPage = goToPreviewPage(context)
+      val id = "about_you_nino"
+      val answerText = textFromXPath(id, _:Page)
+
+      answerText(previewPage) mustEqual "AB 12 34 56 C"
+      val aboutYou = ClaimPageFactory.buildPageFromFluent(previewPage.click(s"#$id"))
+
+      aboutYou must beAnInstanceOf[G1YourDetailsPage]
+      val modifiedData = new TestData
+      modifiedData.AboutYouNINO = "AB123456D"
+
+      aboutYou fillPageWith modifiedData
+      val previewPageModified = aboutYou submitPage()
+
+      previewPageModified must beAnInstanceOf[PreviewPage]
+      answerText(previewPageModified) mustEqual "AB 12 34 56 D"
+    }
+
+    "Modify date of birth from preview page" in new WithBrowser with PageObjects{
+      val previewPage = goToPreviewPage(context)
+      val id = "about_you_dob"
+      val answerText = textFromXPath(id, _:Page)
+
+      answerText(previewPage) mustEqual "03 April, 1950"
+      val aboutYou = ClaimPageFactory.buildPageFromFluent(previewPage.click(s"#$id"))
+
+      aboutYou must beAnInstanceOf[G1YourDetailsPage]
+      val modifiedData = new TestData
+      modifiedData.AboutYouDateOfBirth = "03/04/1952"
+
+      aboutYou fillPageWith modifiedData
+      val previewPageModified = aboutYou submitPage()
+
+      previewPageModified must beAnInstanceOf[PreviewPage]
+      answerText(previewPageModified) mustEqual "03 April, 1952"
+    }
+
+
   } section("integration", models.domain.AboutYou.id)
+
+  def goToPreviewPage(context:PageObjectsContext):Page = {
+    val claimDatePage = G1ClaimDatePage(context)
+    claimDatePage goToThePage()
+    val claimDate = ClaimScenarioFactory.s12ClaimDate()
+    claimDatePage fillPageWith claimDate
+
+    val page =  claimDatePage submitPage()
+    val claim = ClaimScenarioFactory.yourDetailsWithNotTimeOutside()
+    page goToThePage()
+    page fillPageWith claim
+    page submitPage()
+
+    val previewPage = PreviewPage(context)
+    previewPage goToThePage()
+  }
+
+  def textFromXPath(id:String,previewPage:Page):String = {
+    previewPage.xpath(s"//dt[./a[@id='$id']]/following-sibling::dd").getText
+  }
 }
