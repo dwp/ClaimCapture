e905856621975de1e7d0ae26e06fd870bf71548b
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-10-17 13:01:00 +0100

    US968 - Added navigation for your details - Claim Date

--- c3/test/controllers/s1_2_claim_date/G1ClaimDateIntegrationSpec.scala
+++ c3/test/controllers/s1_2_claim_date/G1ClaimDateIntegrationSpec.scala
@@ -2,7 +2,11 @@ package controllers.s1_2_claim_date
 
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
-import controllers.{Formulate, BrowserMatchers}
+import controllers.{ClaimScenarioFactory, PreviewTestUtils, Formulate, BrowserMatchers}
+import utils.pageobjects._
+import utils.pageobjects.s2_about_you.G4NationalityAndResidencyPage
+import utils.pageobjects.preview.PreviewPage
+import utils.pageobjects.s1_2_claim_date.G1ClaimDatePage
 
 class G1ClaimDateIntegrationSpec extends Specification with Tags {
 
@@ -20,5 +24,35 @@ class G1ClaimDateIntegrationSpec extends Specification with Tags {
       titleMustEqual("Your details - About you - the carer")
     }
 
+    "Modify claim date from preview page" in new WithBrowser with PageObjects{
+      val previewPage = goToPreviewPage(context)
+      val id = "about_you_claimdate"
+      val answerText = PreviewTestUtils.answerText(id, _:Page)
+
+      answerText(previewPage) mustEqual "10 October, 2014"
+      val claimDatePage = ClaimPageFactory.buildPageFromFluent(previewPage.click(s"#$id"))
+
+      claimDatePage must beAnInstanceOf[G1ClaimDatePage]
+      val modifiedData = new TestData
+      modifiedData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "11/10/2014"
+
+      claimDatePage fillPageWith modifiedData
+      val previewPageModified = claimDatePage submitPage()
+
+      previewPageModified must beAnInstanceOf[PreviewPage]
+      answerText(previewPageModified) mustEqual "11 October, 2014"
+    }
+
   } section("unit", models.domain.YourClaimDate.id)
+
+  def goToPreviewPage(context:PageObjectsContext):Page = {
+    val claimDatePage = G1ClaimDatePage(context)
+    claimDatePage goToThePage()
+    val claimDate = ClaimScenarioFactory.s12ClaimDate()
+    claimDatePage fillPageWith claimDate
+    claimDatePage submitPage()
+
+    val previewPage = PreviewPage(context)
+    previewPage goToThePage()
+  }
 }
