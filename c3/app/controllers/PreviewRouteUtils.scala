d4573cb3fbe3615ea6fc98934bcc8115ca3b0fa3
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-10-21 14:03:25 +0100

    US972 - Added navigation for education - refactored tests to reflect page object style

---
+++ c3/app/controllers/PreviewRouteUtils.scala
@@ -0,0 +1,47 @@
+package controllers
+
+
+object PreviewRouteUtils {
+
+  def yourDetailsRoute = {
+    val g1YourDetailsRoute = controllers.s2_about_you.routes.G1YourDetails.present.toString
+    val g2ContactDetailsRoute = controllers.s2_about_you.routes.G2ContactDetails.present.toString
+    val g4NationalityRoute = controllers.s2_about_you.routes.G4NationalityAndResidency.present.toString
+    val g7OtherEEARoute = controllers.s2_about_you.routes.G7OtherEEAStateOrSwitzerland.present.toString
+
+    val routesMap = Map("about_you_full_name" -> g1YourDetailsRoute,
+                        "about_you_nino" -> g1YourDetailsRoute,
+                        "about_you_dob" -> g1YourDetailsRoute,
+                        "about_you_address" -> g2ContactDetailsRoute,
+                        "about_you_contact" -> g2ContactDetailsRoute,
+                        "about_you_marital_status" -> g4NationalityRoute,
+                        "about_you_claimDate" -> controllers.s1_2_claim_date.routes.G1ClaimDate.present.toString,
+                        "about_you_nationality" -> g4NationalityRoute,
+                        "about_you_abroad" -> controllers.s2_about_you.routes.G5AbroadForMoreThan52Weeks.present.toString,
+                        "about_you_benefitsFromEEA" -> g7OtherEEARoute,
+                        "about_you_workingForEEA" -> g7OtherEEARoute)
+
+    routesMap
+
+  }
+
+  def otherMoneyRoute = {
+    val g1AboutOtherMoneyRoute = controllers.s9_other_money.routes.G1AboutOtherMoney.present.toString
+    val idList = Seq("other_money_anyPaymentsSinceClaimDate", "other_money_statutoryPay", "other_money_otherStatutoryPay")
+
+    val routesMap = Map(idList map {id => (id, g1AboutOtherMoneyRoute)} : _*)
+
+    routesMap
+  }
+
+  def educationRoute = {
+    val g1YourCourseDetailsRoute = controllers.s6_education.routes.G1YourCourseDetails.present.toString
+    val idList = Seq("education_beenInEducationSinceClaimDate", "education_courseTitle", "education_nameOfSchool",
+      "education_nameOfTutor", "education_contactNumber", "education_startEndDates")
+
+    val routesMap = Map(idList map {id => (id, g1YourCourseDetailsRoute)} : _*)
+
+    routesMap
+  }
+
+}
--- c3/app/controllers/PreviewUtils.scala
+++ c3/app/controllers/PreviewUtils.scala
@@ -11,36 +11,7 @@ class PreviewUtils {
 
 object PreviewUtils {
 
-  var routesMap = yourDetailsRoute ++ otherMoneyRoute
-
-  def yourDetailsRoute = {
-    val g1YourDetailsRoute = controllers.s2_about_you.routes.G1YourDetails.present.toString
-    val g2ContactDetailsRoute = controllers.s2_about_you.routes.G2ContactDetails.present.toString
-    val g4NationalityRoute = controllers.s2_about_you.routes.G4NationalityAndResidency.present.toString
-    val g7OtherEEARoute = controllers.s2_about_you.routes.G7OtherEEAStateOrSwitzerland.present.toString
-
-    val routesMap = Map("about_you_full_name" -> g1YourDetailsRoute,
-                        "about_you_nino" -> g1YourDetailsRoute,
-                        "about_you_dob" -> g1YourDetailsRoute,
-                        "about_you_address" -> g2ContactDetailsRoute,
-                        "about_you_contact" -> g2ContactDetailsRoute,
-                        "about_you_marital_status" -> g4NationalityRoute,
-                        "about_you_claimDate" -> controllers.s1_2_claim_date.routes.G1ClaimDate.present.toString,
-                        "about_you_nationality" -> g4NationalityRoute,
-                        "about_you_abroad" -> controllers.s2_about_you.routes.G5AbroadForMoreThan52Weeks.present.toString,
-                        "about_you_benefitsFromEEA" -> g7OtherEEARoute,
-                        "about_you_workingForEEA" -> g7OtherEEARoute)
-    routesMap
-  }
-
-  def otherMoneyRoute = {
-    val g1AboutOtherMoneyRoute = controllers.s9_other_money.routes.G1AboutOtherMoney.present.toString
-
-    val routesMap = Map("other_money_anyPaymentsSinceClaimDate" -> g1AboutOtherMoneyRoute,
-                        "other_money_statutoryPay" -> g1AboutOtherMoneyRoute,
-                        "other_money_otherStatutoryPay" -> g1AboutOtherMoneyRoute)
-    routesMap
-  }
+  var routesMap = PreviewRouteUtils.yourDetailsRoute ++ PreviewRouteUtils.otherMoneyRoute ++ PreviewRouteUtils.educationRoute
 
   def fieldWithLink(id:String, name:String, value:String)(implicit lang:Lang) = {
     val label = Html(Messages(name))
@@ -51,3 +22,5 @@ object PreviewUtils {
   def apply() = new PreviewUtils()
 
 }
+
+
