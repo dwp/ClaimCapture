6eb6e1bcdf8e6fad969ad628b3438583818e0822
Andrew <email@andrewrea.co.uk>
2015-06-18 12:52:12 +0100

    Updating the C3 version to 2.21

--- c3/app/models/view/ClaimHandling.scala
+++ c3/app/models/view/ClaimHandling.scala
@@ -23,7 +23,7 @@ object ClaimHandling {
   type ClaimResult = (Claim, Result)
   // Versioning
   val C3VERSION = "C3Version"
-  val C3VERSION_VALUE = "2.20.1"
+  val C3VERSION_VALUE = "2.21"
   val applicationFinished = "application-finished"
 
 }
