0256979689742dc7b41a8ac14d9e6685ad2a5ae2
HenryTK <henry130486@gmail.com>
2015-06-29 11:24:57 +0100

    US1231 - upgraded to Universal Analytics. See full commit message
    Universal Analytics tracking code moved to just before the </head> tag.
    Customisations for tracking code made as per documentation:
    https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#create
    Session timeout (currently set at 90 minutes) now set in Google Analytics admin account,
    not in configuration files.
    nonInteraction now set to true by default.
    Updated custom.js to use Universal Analytics functions.

--- c3/public/javascripts/custom.js
+++ c3/public/javascripts/custom.js
@@ -110,17 +110,21 @@ function areCookiesEnabled(){
 
 }
 
-function trackEvent(category, action, label, value, noninteraction){
-    _gaq.push(['_trackEvent',category,action].concat(opt(label)).concat(opt(value)).concat(opt(noninteraction)));
+function trackEvent(category, action, label, value){
+    ga('send', 'event', getConfigurationObject(category, action, label, value));
+}
+
+function getConfigurationObject(category, action, label, value){
+    var configurationObject = {};
+    if (typeof category != 'undefined') configurationObject.eventCategory = category;
+    if (typeof action != 'undefined') configurationObject.eventAction = action
+    if (typeof label != 'undefined') configurationObject.eventLabel = label
+    if (typeof value != 'undefined') configurationObject.eventValue = value
+    return configurationObject;
 }
 
 function trackVirtualPageView(category){
-    _gaq.push(['_trackPageview',category]);
-}
-
-function opt(v){
-    if (typeof v == 'undefined') return [];
-    else return[v];
+    ga('send', 'pageview', category)
 }
 
 function getCookie(c_name) {
@@ -140,9 +144,13 @@ function setCookie(c_name,value) {
     document.cookie=c_name + "=" + c_value;
 }
 
-function trackTiming(category,variable,timemeasured){
+function trackTiming(category, variable, timemeasured){
     if (0 < timemeasured && timemeasured <  36000000) {
-        _gaq.push(['_trackTiming',category,variable,timemeasured]);
+        ga('send', 'timing', {
+          'timingCategory': category,
+          'timingVar': variable,
+          'timingValue': timemeasured
+        });
     }
 }
 
