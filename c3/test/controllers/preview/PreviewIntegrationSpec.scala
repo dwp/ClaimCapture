a40ba82efb013186581034bee8e0a72e18ff49cb
HenryTK <henry130486@gmail.com>
2015-08-07 14:15:00 +0100

    US1300 - Added nextButton view and added it to about you section pages

--- c3/test/controllers/preview/PreviewIntegrationSpec.scala
+++ c3/test/controllers/preview/PreviewIntegrationSpec.scala
@@ -1,23 +1,25 @@
 package controllers.preview
 
 import org.specs2.mutable.{Tags, Specification}
-import utils.WithBrowser
+import utils.WithJsBrowser
 import utils.pageobjects.PageObjects
 import utils.pageobjects.preview.PreviewPage
+import utils.pageobjects.s_about_you.GYourDetailsPage
 import utils.pageobjects.s_information.GAdditionalInfoPage
-import controllers.ClaimScenarioFactory
+import controllers.{BrowserMatchers, ClaimScenarioFactory}
 import utils.pageobjects.s_pay_details.GHowWePayYouPage
 import utils.pageobjects.s_consent_and_declaration.GDeclarationPage
+import play.api.i18n.{MMessages => Messages}
 
 
 class PreviewIntegrationSpec extends Specification with Tags {
   "Preview" should{
-    "be presented" in new WithBrowser with PageObjects{
+    "be presented" in new WithJsBrowser with PageObjects{
       val page =  PreviewPage(context)
       page goToThePage()
     }
 
-    "navigate back to Additional Info page" in new WithBrowser with PageObjects{
+    "navigate back to Additional Info page" in new WithJsBrowser with PageObjects{
 
       val additionalInfoPage = GAdditionalInfoPage(context)
       val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
@@ -28,14 +30,14 @@ class PreviewIntegrationSpec extends Specification with Tags {
       previewPage goBack() must beAnInstanceOf[GAdditionalInfoPage]
     }
 
-    "navigate to Declaration page" in new WithBrowser with PageObjects {
+    "navigate to Declaration page" in new WithJsBrowser with PageObjects {
        val previewPage = PreviewPage(context)
        previewPage goToThePage()
        val declarationPage = previewPage submitPage()
       declarationPage must beAnInstanceOf[GDeclarationPage]
     }
 
-    "navigate back to preview page clicking next" in new WithBrowser with PageObjects {
+    "navigate back to preview page clicking next" in new WithJsBrowser with PageObjects {
       val additionalInfoPage = GAdditionalInfoPage(context)
       val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
       additionalInfoPage goToThePage ()
@@ -47,7 +49,7 @@ class PreviewIntegrationSpec extends Specification with Tags {
       additionalPage submitPage () must beAnInstanceOf[PreviewPage]
     }
 
-    "navigate back to how we pay you page" in new WithBrowser with PageObjects {
+    "navigate back to how we pay you page" in new WithJsBrowser with PageObjects {
       val howWePayYouPage = GHowWePayYouPage(context)
       howWePayYouPage goToThePage()
       howWePayYouPage fillPageWith ClaimScenarioFactory.s6PayDetails
@@ -61,6 +63,15 @@ class PreviewIntegrationSpec extends Specification with Tags {
       val additionalPage = previewPage goBack()
       additionalPage must beAnInstanceOf[GAdditionalInfoPage]
       additionalPage goBack () must beAnInstanceOf[GHowWePayYouPage]
+    }
+
+    "change Next button text to 'Return to summary'" in new WithJsBrowser with PageObjects {
+
+      val previewPage = PreviewPage(context) goToThePage()
+      previewPage must beAnInstanceOf[PreviewPage]
+      browser.findFirst("#about_you_contact").click()
+      browser.findFirst("button[value='next']").getText mustEqual Messages("form.returnToSummary")
+
     }
 
   }section "preview"
