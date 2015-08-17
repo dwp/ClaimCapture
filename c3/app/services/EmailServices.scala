bc1670f5fc4db061eae896e91ca1f10e0fa3d341
Tudor Malene <tudor.malene@gmail.com>
2015-08-17 13:54:54 +0100

    US1335 - change email subject line

--- c3/app/services/EmailServices.scala
+++ c3/app/services/EmailServices.scala
@@ -1,9 +1,12 @@
 package services
 
 import app.ConfigProperties._
+import app.XMLValues
+import app.XMLValues._
 import controllers.mappings.Mappings
 import models.domain._
 import models.view.CachedClaim
+import models.yesNo.YesNoWithText
 import play.api.Logger
 import play.api.i18n.Lang
 import services.mail.{EmailWrapper, EmailActors}
@@ -13,17 +16,18 @@ import play.modules.mailer._
 import play.api.i18n.{MMessages => Messages}
 
 object CadsEmail {
-  def send(transactionID:String, subject:String,body:String, r:String*) = {
+  def send(transactionID: String, subject: String, body: String, r: String*) = {
     //EmailAddress object uses javax.mail.internet.InternetAddress behind scenes. On parameters EmailAddress("Peter","peter@gmail.com") will generate "Peter <peter@gmail.com>" on the email sending message
     //As we don't want to send emails with the chevrons, I tried "","email@email.com" but that produces " <email@email.com>", so we have to send null in the first parameter so the email on the output is just what we are sending.
-    val recipients = r.map(to => Recipient(RecipientType.TO,EmailAddress(null,to)))
+    val recipients = r.map(to => Recipient(RecipientType.TO, EmailAddress(null, to)))
     Logger.info(s"Sending email for transactionId [$transactionID]")
-    EmailActors.manager ! EmailWrapper(transactionID,Email(subject = subject,from = EmailAddress(null,getProperty("mailer.from","noreply@lab.3cbeta.co.uk")),text = "",htmlText = body,None,recipients))
+    EmailActors.manager ! EmailWrapper(transactionID, Email(subject = subject, from = EmailAddress(null, getProperty("mailer.from", "noreply@lab.3cbeta.co.uk")), text = "", htmlText = body, None, recipients))
   }
 }
+
 object EmailServices {
 
-  private def isEmployment(claim:Claim) = {
+  private def isEmployment(claim: Claim) = {
     (claim.questionGroup[Employment] match {
       case Some(employment) => employment.beenEmployedSince6MonthsBeforeClaim == Mappings.yes || employment.beenSelfEmployedSince1WeekBeforeClaim == Mappings.yes
       case _ => false
@@ -33,33 +37,40 @@ object EmailServices {
 
   }
 
-  private def sendClaimEmail(claim:Claim) = {
+  private def sendClaimEmail(claim: Claim) = {
     claim.questionGroup[ContactDetails] -> claim.questionGroup[AdditionalInfo] match {
-      case (Some(contactDetails),Some(additionalInfo)) if contactDetails.email.isDefined =>
+      case (Some(contactDetails), Some(additionalInfo)) if contactDetails.email.isDefined =>
         val isWelsh = additionalInfo.welshCommunication == Mappings.yes
-        implicit val lang = if(isWelsh) Lang("cy") else Lang("en")
+        implicit val lang = if (isWelsh) Lang("cy") else Lang("en")
 
-        CadsEmail.send(claim.transactionId.getOrElse(""),subject = Messages("subject.claim"),body = views.html.mail(claim,isClaim = true,isEmployment(claim)).body,contactDetails.email.get)
-      case (Some(contactDetails),Some(additionalInfo)) if contactDetails.email.isEmpty => //We do nothing in this case, they have selected not to send email
+        CadsEmail.send(claim.transactionId.getOrElse(""), subject = claimEmailSubject(claim), body = views.html.mail(claim, isClaim = true, isEmployment(claim)).body, contactDetails.email.get)
+      case (Some(contactDetails), Some(additionalInfo)) if contactDetails.email.isEmpty => //We do nothing in this case, they have selected not to send email
       case _ => Logger.error(s"Can't send claim email, email not present for transactionId[${claim.transactionId.getOrElse("id not present")}]")
     }
   }
 
-  private def sendCofcEmail(claim:Claim) = {
+  def claimEmailSubject(claim: Claim) = (claim.questionGroup[Employment], claim.questionGroup[SelfEmploymentPensionsAndExpenses]) match {
+    case (Some(Employment(XMLValues.yes, _)), _) | (Some(Employment(_, XMLValues.yes)), _) => Messages("subject.claim.employed")
+    case (_, Some(SelfEmploymentPensionsAndExpenses(YesNoWithText(XMLValues.yes, _), _))) => Messages("subject.claim.employed")
+    case _ => Messages("subject.claim.notemployed")
+  }
+
+
+  private def sendCofcEmail(claim: Claim) = {
     claim.questionGroup[CircumstancesReportChange] -> claim.lang match {
-      case (Some(circumstancesRepChange),language) if circumstancesRepChange.email.isDefined =>
+      case (Some(circumstancesRepChange), language) if circumstancesRepChange.email.isDefined =>
         implicit val lang = language.getOrElse(Lang("en"))
 
-        CadsEmail.send(claim.transactionId.getOrElse(""), subject = Messages("subject.cofc"),body = views.html.mail(claim,isClaim = false,isEmployment(claim)).body,circumstancesRepChange.email.get)
-      case (Some(circumstancesDecl),language) if circumstancesDecl.email.isEmpty => //We do nothing in this case, they have selected not to send email
+        CadsEmail.send(claim.transactionId.getOrElse(""), subject = Messages("subject.cofc"), body = views.html.mail(claim, isClaim = false, isEmployment(claim)).body, circumstancesRepChange.email.get)
+      case (Some(circumstancesDecl), language) if circumstancesDecl.email.isEmpty => //We do nothing in this case, they have selected not to send email
       case _ => Logger.error(s"Can't send changes email, email not present for transactionId[${claim.transactionId.getOrElse("id not present")}]")
     }
   }
 
-  def sendEmail(claim:Claim) = {
+  def sendEmail(claim: Claim) = {
     claim.key match {
-      case CachedClaim.key => EmailServices.sendClaimEmail(claim)
-      case _ => EmailServices.sendCofcEmail(claim)
+      case CachedClaim.key => sendClaimEmail(claim)
+      case _ => sendCofcEmail(claim)
     }
   }
 
---
+++ c3/test/services/mail/EmailServicesSpec.scala
@@ -0,0 +1,41 @@
+package services.mail
+
+import app.XMLValues
+import app.XMLValues._
+import models.domain.{SelfEmploymentPensionsAndExpenses, QuestionGroup, Employment, Claim}
+import models.view.CachedClaim
+import models.yesNo.YesNoWithText
+import org.specs2.mutable.Specification
+import play.api.i18n.Messages
+import services.EmailServices
+import utils.WithApplication
+
+/**
+ * Created by tudormalene on 17/08/15.
+ */
+class EmailServicesSpec extends Specification {
+
+  "The email subject" should {
+
+    """When i answer Yes to "Have you been employed at any time since <<dd/MON/yyyy>> (this is six months before your claim date: <<dd/MON/yyyy)?"
+      |or "Have you been self-employed at any time since <<dd/MON/yyyy>>"  or <<Do>>, >> or << Did>> you pay into a pension?"
+      |Then the text displayed in the subject line must read "Carer's Allowance application: next steps"""" in new WithApplication {
+
+      verifyEmployedSubject(Employment(no, yes))
+      verifyEmployedSubject(Employment(yes, no))
+      verifyEmployedSubject(SelfEmploymentPensionsAndExpenses(YesNoWithText(yes, None)))
+
+    }
+
+    """When i answer No to "Have you been employed at any time since <<dd/MON/yyyy>> (this is six months before your claim date: <<dd/MON/yyyy)?"
+      |And I answer No to "Have you been self-employed at any time since <<dd/MON/yyyy>>"
+      |Then the text in the subject line must read "Carer's Allowance application received"""" in new WithApplication {
+
+      verifyNotEmployedSubject(Employment(no, no))
+    }
+
+    def verifyEmployedSubject(qg: QuestionGroup) = "Carer's Allowance application: next steps" must_== EmailServices.claimEmailSubject(Claim(CachedClaim.key).update(qg))
+    def verifyNotEmployedSubject(qg: QuestionGroup) = "Carer's Allowance application received" must_== EmailServices.claimEmailSubject(Claim(CachedClaim.key).update(qg))
+
+  }
+}
