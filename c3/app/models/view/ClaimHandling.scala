a8a6bea33d33c92cc5be8eb3d212ec759b2af3eb
Andrew <email@andrewrea.co.uk>
2015-08-21 12:57:19 +0100

    Upgrading the version and the cookie number to 2.25

--- c3/app/models/view/ClaimHandling.scala
+++ c3/app/models/view/ClaimHandling.scala
@@ -23,7 +23,7 @@ object ClaimHandling {
   type ClaimResult = (Claim, Result)
   // Versioning
   val C3VERSION = "C3Version"
-  val C3VERSION_VALUE = "2.24.1"
+  val C3VERSION_VALUE = "2.25"
   val applicationFinished = "application-finished"
 
 }
