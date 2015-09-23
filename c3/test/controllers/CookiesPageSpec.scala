6dd99ce803abedfd43f64657fe8c55fca0cb9929
HenryTK <henry130486@gmail.com>
2015-09-23 17:05:46 +0100

    CADS-50 - Added new cookies page and content. Changed links from gov.uk cookies page to ours. Added GA tracking to links

---
+++ c3/test/controllers/CookiesPageSpec.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import org.specs2.mutable.Specification
+import play.api.test.FakeRequest
+import play.api.test.Helpers.OK
+import play.api.test.Helpers.status
+import utils.WithApplication
+import akka.util.Timeout
+import java.util.concurrent.TimeUnit
+
+class CookiesPageSpec extends Specification {
+
+  implicit val timeout = Timeout(10, TimeUnit.SECONDS)
+
+  "Cookies page" should {
+    "Present" in new WithApplication {
+      val request = FakeRequest()
+      val result = controllers.Cookies.page("en")(request)
+      status(result) mustEqual OK
+    }
+  } section "unit"
+
+}
