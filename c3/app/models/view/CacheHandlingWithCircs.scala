a91115e1b7700650a467e1611c34901ad6944acb
HenryTK <henry130486@gmail.com>
2015-10-06 14:53:56 +0100

    CADS-52 - Introduced CacheHandlingWithClaim and CacheHandlingWithCircs for dynamic cache handling with the Language controller

---
+++ c3/app/models/view/CacheHandlingWithCircs.scala
@@ -0,0 +1,10 @@
+package models.view
+
+/**
+ * Creates an object which has access to methods for saving to and getting
+ * from the cache. The CachedChangeOfCircs trait implements the cacheKey method required
+ * for getting from the cache.
+ * See Language (controller) and LanguageSpec (tests) for example
+ * usage.
+ */
+class CacheHandlingWithCircs extends CacheHandling with CachedChangeOfCircs {}
--- c3/test/controllers/LanguageSpec.scala
+++ c3/test/controllers/LanguageSpec.scala
@@ -3,63 +3,68 @@ package controllers
 import org.specs2.mutable.{Tags, Specification}
 import utils.WithApplication
 import play.api.test.FakeRequest
-import models.view.{CachedClaim, CachedChangeOfCircs}
+import models.view._
 import models.domain.{Claim, MockForm}
 import play.api.test.Helpers._
 import play.api.i18n.Lang
 import play.api.cache.Cache
 
 class LanguageSpec extends Specification with Tags {
+
   "Language - Change Language" should {
     "change of claim present - change language to English" in new WithApplication with MockForm {
-      val request = FakeRequest().withSession(CachedClaim.key -> claimKey)
-      Cache.set(claimKey, Claim(claimKey, List.empty))
+      val cacheHandler = new CacheHandlingWithClaim()
+      val request = FakeRequest().withSession(cacheHandler.cacheKey -> claimKey)
+      cacheHandler.saveInCache(Claim(cacheHandler.cacheKey, List.empty, System.currentTimeMillis(), None, claimKey))
 
       val expectedLangAsString = "en"
       val expectedLang = Lang(expectedLangAsString)
       val result = controllers.Language.change(expectedLangAsString)(request)
       status(result) mustEqual SEE_OTHER
 
-      val claim = Cache.getAs[Claim](claimKey).get
+      val claim = cacheHandler.fromCache(request).get
       claim.lang.get mustEqual expectedLang
     }
 
     "change of circs present - change language to English" in new WithApplication with MockForm {
-      val request = FakeRequest().withSession(CachedChangeOfCircs.key -> claimKey).withHeaders(REFERER -> "http://localhost:9000/circumstances/identification/about-you")
-      Cache.set(claimKey, Claim(claimKey, List.empty))
+      val cacheHandler = new CacheHandlingWithCircs()
+      val request = FakeRequest().withSession(cacheHandler.cacheKey -> claimKey).withHeaders(REFERER -> "http://localhost:9000/circumstances/identification/about-you")
+      cacheHandler.saveInCache(Claim(cacheHandler.cacheKey, List.empty, System.currentTimeMillis(), None, claimKey))
 
       val expectedLangAsString = "en"
       val expectedLang = Lang(expectedLangAsString)
       val result = controllers.Language.change(expectedLangAsString)(request)
       status(result) mustEqual SEE_OTHER
 
-      val claim = Cache.getAs[Claim](claimKey).get
+      val claim = cacheHandler.fromCache(request).get
       claim.lang.get mustEqual expectedLang
     }
 
     "change of claim present - change language to Welsh" in new WithApplication with MockForm {
-      val request = FakeRequest().withSession(CachedClaim.key -> claimKey)
-      Cache.set(claimKey, Claim(claimKey, List.empty))
+      val cacheHandler = new CacheHandlingWithClaim()
+      val request = FakeRequest().withSession(cacheHandler.cacheKey -> claimKey)
+      cacheHandler.saveInCache(Claim(cacheHandler.cacheKey, List.empty, System.currentTimeMillis(), None, claimKey))
 
       val expectedLangAsString = "cy"
       val expectedLang = Lang(expectedLangAsString)
       val result = controllers.Language.change(expectedLangAsString)(request)
       status(result) mustEqual SEE_OTHER
 
-      val claim = Cache.getAs[Claim](claimKey).get
+      val claim = cacheHandler.fromCache(request).get
       claim.lang.get mustEqual expectedLang
     }
 
     "change of circs present - change language to Welsh" in new WithApplication with MockForm {
-      val request = FakeRequest().withSession(CachedChangeOfCircs.key -> claimKey).withHeaders(REFERER -> "http://localhost:9000/circumstances/identification/about-you")
-      Cache.set(claimKey, Claim(claimKey, List.empty))
+      val cacheHandler = new CacheHandlingWithCircs()
+      val request = FakeRequest().withSession(cacheHandler.cacheKey -> claimKey).withHeaders(REFERER -> "http://localhost:9000/circumstances/identification/about-you")
+      cacheHandler.saveInCache(Claim(cacheHandler.cacheKey, List.empty, System.currentTimeMillis(), None, claimKey))
 
       val expectedLangAsString = "cy"
       val expectedLang = Lang(expectedLangAsString)
       val result = controllers.Language.change(expectedLangAsString)(request)
       status(result) mustEqual SEE_OTHER
 
-      val claim = Cache.getAs[Claim](claimKey).get
+      val claim = cacheHandler.fromCache(request).get
       claim.lang.get mustEqual expectedLang
     }
 
