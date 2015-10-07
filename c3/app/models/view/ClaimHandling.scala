ef3a3b74cc6ff36d1fe9b46c84143800e8f98618
Peter Whitehead <peter@scrumconnect.com>
2015-10-07 16:00:13 +0100

    CADS-60 : Circs: Change 'Back button page' url so picked up in Circs filter for GA. - Added circs back button function

--- c3/app/models/view/ClaimHandling.scala
+++ c3/app/models/view/ClaimHandling.scala
@@ -33,6 +33,7 @@ trait ClaimHandling extends RequestHandling with CacheHandling {
   protected def claimNotValid(claim: Claim): Boolean
   protected def newInstance(newuuid: String = randomUUID.toString): Claim
   protected def copyInstance(claim: Claim): Claim
+  protected def backButtonPage : Call
 
 
   //============================================================================================================
@@ -184,7 +185,7 @@ trait ClaimHandling extends RequestHandling with CacheHandling {
         Logger.info("User already completed claim. Redirection to back button page.")
         true
       case _ => false
-    }) Redirect(controllers.routes.Application.backButtonPage())
+    }) Redirect(backButtonPage)
     else otherwise
 
   protected def action(claim: Claim, request: Request[AnyContent], lang: Lang)(f: (Claim) => Request[AnyContent] => Lang => Either[Result, ClaimResult]): Result = {
