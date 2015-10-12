66f2362d8ed3702d5cbba7fa47d83716bce974e4
Peter Whitehead <peter@scrumconnect.com>
2015-10-12 09:50:09 +0100

    CADS-240 : If user selects 'other' in title field displays incorrectly in CYA

--- c3/test/controllers/preview/PreviewPageAboutYouContentSpec.scala
+++ c3/test/controllers/preview/PreviewPageAboutYouContentSpec.scala
@@ -51,6 +51,37 @@ class PreviewPageAboutYouContentSpec extends Specification with Tags {
       source must contain("Marital status")
       source must contain("Single")
     }
+
+    "display about you - the carer data with nationality as British and other title" in new WithBrowser with PageObjects{
+      val claim = ClaimScenarioFactory.yourDetailsWithNotTimeOutside()
+      claim.AboutYouTitleOther = "Rev"
+      claim.AboutYouTitle = "Other"
+      fillAboutYouTheCarerSection(context, claim)
+
+      val page =  PreviewPage(context)
+      page goToThePage()
+      val source = page.source
+
+      source must contain("Name")
+      source must contain("Rev John middlename Appleseed")
+      source must contain("Date of birth")
+      source must contain("03 April, 1950")
+      source must contain("Address")
+      source must contain("101 Clifton Street, Blackpool FY1 2RW")
+      source must contain("Contact phone or mobile number")
+      source must contain("01772 888901")
+      source must contain("Claim date")
+      source must contain("10 October, 2016")
+      source must contain("Your nationality")
+      source must contain("British")
+      source must contain("Do you normally live in England, Scotland or Wales?")
+      source must contain("Yes - Details provided")
+      source must contain("Have you or anyone in your close family claimed or been paid any benefits or pensions from an EEA country since your claim date?")
+      source must contain("Yes - Details provided")
+      source must contain("Have you or anyone in your close family worked or paid national insurance in an EEA country since your claim date?")
+      source must contain("Yes - Details provided")
+    }
+
   } section "preview"
 
   def fillAboutYouTheCarerSection(context:PageObjectsContext, claim:TestData = ClaimScenarioFactory.yourDetailsWithNotTimeOutside()) = {
