5ff6b0a69cceed221a053e9a1c2979574b14a9e2
Peter Whitehead <peter@scrumconnect.com>
2015-10-15 14:06:04 +0100

    CADS-164 : Before You Start / Disclaimer - revised shortened version

--- c3/app/xml/claim/Disclaimer.scala
+++ c3/app/xml/claim/Disclaimer.scala
@@ -18,12 +18,9 @@ object  Disclaimer extends XMLComponent{
         <Content>{Messages("disclaimer.1")}</Content>
         <Content>{Messages("disclaimer.2")}</Content>
         <Content>{Messages("disclaimer.3")}</Content>
-        <Content>{Messages("disclaimer.4")}</Content>
-        <Content>{Messages("disclaimer.5")}</Content>
-        <Content>{Messages("disclaimer.6")}</Content>
-        <Content>{Messages("disclaimer.7", "", "")}</Content>
-        <Content>{Messages("disclaimer.8", "", "")}</Content>
-        <Content>{Messages("disclaimer.9", "", "")}</Content>
+        <Content>{Messages("disclaimer.4", "", "")}</Content>
+        <Content>{Messages("disclaimer.5", "", "")}</Content>
+        <Content>{Messages("disclaimer.6", "", "")}</Content>
       </DisclaimerStatement>
       {question(<DisclaimerQuestion/>,"read", disclaimer.read)}
     </Disclaimer>
