b879ccce7649c135c4c58d2a06405bfdf28b3178
Colin Greenwood <colingreenwood@scrumconnect.com>
2015-11-16 16:03:58 +0000

    CADS-378 Back button removal from Preview screen - remove 3 redundant tests.

--- c3/test/controllers/preview/PreviewIntegrationSpec.scala
+++ c3/test/controllers/preview/PreviewIntegrationSpec.scala
@@ -19,50 +19,11 @@ class PreviewIntegrationSpec extends Specification with Tags {
       page goToThePage()
     }
 
-    "navigate back to Additional Info page" in new WithJsBrowser with PageObjects{
-
-      val additionalInfoPage = GAdditionalInfoPage(context)
-      val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
-      additionalInfoPage goToThePage ()
-      additionalInfoPage fillPageWith additionalInfoData
-      val previewPage = additionalInfoPage submitPage()
-      previewPage must beAnInstanceOf[PreviewPage]
-      previewPage goBack() must beAnInstanceOf[GAdditionalInfoPage]
-    }
-
     "navigate to Declaration page" in new WithJsBrowser with PageObjects {
        val previewPage = PreviewPage(context)
        previewPage goToThePage()
        val declarationPage = previewPage submitPage()
       declarationPage must beAnInstanceOf[GDeclarationPage]
-    }
-
-    "navigate back to preview page clicking next" in new WithJsBrowser with PageObjects {
-      val additionalInfoPage = GAdditionalInfoPage(context)
-      val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
-      additionalInfoPage goToThePage ()
-      additionalInfoPage fillPageWith additionalInfoData
-      val previewPage = additionalInfoPage submitPage()
-      previewPage must beAnInstanceOf[PreviewPage]
-      val additionalPage = previewPage goBack()
-      additionalPage must beAnInstanceOf[GAdditionalInfoPage]
-      additionalPage submitPage () must beAnInstanceOf[PreviewPage]
-    }
-
-    "navigate back to how we pay you page" in new WithJsBrowser with PageObjects {
-      val howWePayYouPage = GHowWePayYouPage(context)
-      howWePayYouPage goToThePage()
-      howWePayYouPage fillPageWith ClaimScenarioFactory.s6PayDetails
-      howWePayYouPage submitPage()
-      val additionalInfoPage = GAdditionalInfoPage(context)
-      val additionalInfoData = ClaimScenarioFactory.s11ConsentAndDeclaration
-      additionalInfoPage goToThePage ()
-      additionalInfoPage fillPageWith additionalInfoData
-      val previewPage = additionalInfoPage submitPage()
-      previewPage must beAnInstanceOf[PreviewPage]
-      val additionalPage = previewPage goBack()
-      additionalPage must beAnInstanceOf[GAdditionalInfoPage]
-      additionalPage goBack () must beAnInstanceOf[GHowWePayYouPage]
     }
 
     "change Next button text to 'Return to summary'" in new WithJsBrowser with PageObjects {
