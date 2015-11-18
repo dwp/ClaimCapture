8ba341cb6c5134a3977609cd4593781e861ae301
Peter Whitehead <peter@scrumconnect.com>
2015-11-18 15:55:54 +0000

    CADS-297 : added withApplication

--- c3/test/utils/pageobjects/tests/PageFactorySpec.scala
+++ c3/test/utils/pageobjects/tests/PageFactorySpec.scala
@@ -1,6 +1,7 @@
 package utils.pageobjects.tests
 
 import org.specs2.mutable._
+import utils.WithApplication
 import utils.pageobjects.{PageObjectsContext, CircumstancesPageFactory, UnknownPage, ClaimPageFactory}
 import utils.pageobjects.s_eligibility.GBenefitsPage
 import utils.pageobjects.circumstances.s1_start_of_process.G2ReportAChangeInYourCircumstancesPage
@@ -13,17 +14,17 @@ import utils.pageobjects.circumstances.s1_start_of_process.G2ReportAChangeInYour
 class PageFactorySpec extends Specification {
 
   "The PageFactory" should {
-    "Return an UnknownPage if it does not recognise title" in new MockPageContext {
+    "Return an UnknownPage if it does not recognise title" in new WithApplication with MockPageContext {
       val newPage = ClaimPageFactory buildPageFromUrl("/unknown",PageObjectsContext(browser))
       newPage must beAnInstanceOf[UnknownPage]
     }  
     
-    "Return a BenefitPage if provided Benefits page title" in new MockPageContext {
+    "Return a BenefitPage if provided Benefits page title" in new WithApplication with MockPageContext {
       val newPage = ClaimPageFactory buildPageFromUrl(GBenefitsPage.url,PageObjectsContext(browser))
       newPage must beAnInstanceOf[GBenefitsPage]
     }
 
-    "Return an AboutYouPage if provided AboutYouPage page title" in new MockPageContext {
+    "Return an AboutYouPage if provided AboutYouPage page title" in new WithApplication with MockPageContext {
       val newPage = CircumstancesPageFactory buildPageFromUrl(G2ReportAChangeInYourCircumstancesPage.url,PageObjectsContext(browser))
       newPage must beAnInstanceOf[G2ReportAChangeInYourCircumstancesPage]
     }
