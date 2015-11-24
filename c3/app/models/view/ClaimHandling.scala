1689994596270606ab8e65ddd328e31d5d38c86a
Peter Whitehead <peter@scrumconnect.com>
2015-11-24 09:26:54 +0000

    CADS-297 : changed to 3.0

--- c3/app/models/view/ClaimHandling.scala
+++ c3/app/models/view/ClaimHandling.scala
@@ -26,7 +26,7 @@ object ClaimHandling {
   type ClaimResult = (Claim, Result)
   // Versioning
   val C3VERSION = "C3Version"
-  val C3VERSION_VALUE = "2.30"
+  val C3VERSION_VALUE = "3.00"
   val applicationFinished = "application-finished"
 
 }
