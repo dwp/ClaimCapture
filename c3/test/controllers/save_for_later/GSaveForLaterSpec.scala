0fa4927eebdae48147c59c8d0562b20312a1b910
Colin Greenwood <colingreenwood@scrumconnect.com>
2015-11-27 13:04:06 +0000

    CADS-319 : Save for later initial int tests and tweaks for test.

---
+++ c3/test/controllers/save_for_later/GSaveForLaterSpec.scala
@@ -0,0 +1,32 @@
+package controllers.save_for_later
+
+import org.specs2.mutable._
+import utils.WithJsBrowser
+import utils.pageobjects.s_about_you.GContactDetailsPage
+import utils.pageobjects.save_for_later.GSaveForLaterPage
+import utils.pageobjects.{PageObjects, TestData}
+
+class GSaveForLaterSpec extends Specification {
+
+  "Save for later page" should {
+
+    "be shown after clicking Save in nationality" in new WithJsBrowser with PageObjects {
+      val page = GContactDetailsPage(context)
+      page goToThePage()
+      page fillPageWith SaveForLaterScenarioFactory.WithEmailSet()
+
+      val nationalityPage = page submitNextPage()
+      nationalityPage.source must contain("Save for later")
+      nationalityPage.source must contain("id=\"save\"")
+      val savePage=nationalityPage.clickLinkOrButton("#save")
+      savePage.url mustEqual GSaveForLaterPage.url
+      savePage.source must contain("Your progress has been saved")
+
+      savePage.source must contain("Continue your application" )
+      savePage.source must contain("id=\"continue\"")
+    }
+  }
+
+  section("integration", models.domain.AboutYou.id)
+
+}
