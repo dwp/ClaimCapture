9a511ec96802b7640668f5cf75b961af2b95ebea
Colin Greenwood <colingreenwood@scrumconnect.com>
2015-11-30 13:54:11 +0000

    CADS-319 : Save for later amend not validated parameters to use map on Cache.

--- c3/app/controllers/save_for_later/GSaveForLater.scala
+++ c3/app/controllers/save_for_later/GSaveForLater.scala
@@ -1,5 +1,10 @@
 package controllers.save_for_later
 
+import controllers.CarersForms._
+import controllers.mappings.Mappings._
+import controllers.mappings.NINOMappings._
+import controllers.s_about_you.GNationalityAndResidency._
+import controllers.s_about_you.routes
 import models.domain._
 import models.view.{CachedClaim, Navigable}
 import play.api.Play._
@@ -7,41 +12,34 @@ import play.api.data.{Form}
 import play.api.data.Forms._
 import play.api.i18n._
 import play.api.mvc.{Controller}
+import utils.helpers.CarersCrypto
 import utils.helpers.CarersForm._
-
+import app.ConfigProperties._
 import scala.language.reflectiveCalls
 
 object GSaveForLater extends Controller with CachedClaim with Navigable with I18nSupport {
 
   override val messagesApi: MessagesApi = current.injector.instanceOf[MMessages]
 
-  val resideInUKMapping =
-    "resideInUK" -> mapping(
-      "answer" -> optional(text),
-      "text" -> optional(text)
-    )(ResideInUK.apply)(ResideInUK.unapply)
-
-  val form = Form(mapping(
-    "nationality" -> optional(text),
-    "actualnationality" -> optional(text),
-    resideInUKMapping,
-    "anyTrips"-> optional(text),
-    "tripDetails"-> optional(text)
-  )(SaveForLater.apply)(SaveForLater.unapply))
-
   def present = claimingWithCheck { implicit claim => implicit request => implicit lang =>
-    Ok(views.html.save_for_later.saveClaimSuccess(lang))
+  Ok(views.html.save_for_later.saveClaimSuccess(lang))
   }
 
   def submit = claimingWithCheck { implicit claim => implicit request => implicit lang =>
-    form.bindEncrypted.fold(
-      formWithErrors => {
-         BadRequest(views.html.save_for_later.saveClaimSuccess(lang))
-      },
-      saveform =>{
-        val updatedClaim=claim.copy(saveForLater=saveform)(claim.navigation)
-        updatedClaim->Redirect(routes.GSaveForLater.present())
-      }
-    )
+    //val saveForLaterPageData = request.queryString.map { case (k,v) => CarersCrypto.decryptAES(k) -> v.mkString }
+
+    val params=request.body.asFormUrlEncoded.get
+    var paramsmap: Map[String,String]=Map()
+    params.keys.foreach { key =>
+      paramsmap+=(key->params.get(key).get(0))
+    }
+
+    if( getProperty("saveForLaterEnabled",default = false) ){
+      var updatedClaim=claim.update(paramsmap)
+      updatedClaim->Redirect( controllers.save_for_later.routes.GSaveForLater.present())
+    }
+    else{
+      BadRequest(views.html.save_for_later.saveClaimSuccess(lang))
+    }
   }
 }
