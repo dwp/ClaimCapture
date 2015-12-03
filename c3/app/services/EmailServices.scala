ccf41aeaf1cd1c99066498f5c4e21734b3866ab5
Peter Whitehead <peter@scrumconnect.com>
2015-12-03 13:44:43 +0000

    CADS-404 :  Save for later - send email

--- c3/app/services/EmailServices.scala
+++ c3/app/services/EmailServices.scala
@@ -88,7 +88,7 @@ object EmailServices extends I18nSupport {
     claim.questionGroup[ContactDetails] match {
       case Some(contactDetails) => {
         if (contactDetails.email.isEmpty) Logger.info(s"Not sending claim email because the user didn't input an address for transid: [${claim.transactionId.getOrElse("id not present")}]")
-        else CadsEmail.send(claim.transactionId.getOrElse(""), subject = saveForLaterEmailSubject, body = views.html.savedMail.body, contactDetails.email.get)
+        else CadsEmail.send(claim.transactionId.getOrElse(""), subject = saveForLaterEmailSubject, body = views.html.savedMail().body, contactDetails.email.get)
       }
     }
   }
