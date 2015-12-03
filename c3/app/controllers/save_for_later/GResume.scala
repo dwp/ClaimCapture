dd3194a9a5b4f592caf870b5b84ba1aa3a2c5c7b
Colin Greenwood <colingreenwood@scrumconnect.com>
2015-12-03 09:53:38 +0000

    CADS-329 : Resume errors for retries.

--- c3/app/controllers/save_for_later/GResume.scala
+++ c3/app/controllers/save_for_later/GResume.scala
@@ -18,31 +18,32 @@ import play.api.{Logger}
 object GResume extends Controller with CachedClaim with Navigable with I18nSupport {
   override val messagesApi: MessagesApi = current.injector.instanceOf[MMessages]
 
-  val form=Form(mapping(
+  val form = Form(mapping(
     "firstName" -> carersNonEmptyText(maxLength = 17),
     "surname" -> carersNonEmptyText(maxLength = Name.maxLength),
-    "nationalInsuranceNumber" -> nino.verifying(filledInNino,validNino),
+    "nationalInsuranceNumber" -> nino.verifying(filledInNino, validNino),
     "dateOfBirth" -> dayMonthYear.verifying(validDate),
     "uuid" -> text
   )(ResumeSaveForLater.apply)(ResumeSaveForLater.unapply))
 
   def present = newClaim { implicit claim => implicit request => implicit lang =>
-    var savekeyuuid=createParamsMap(request.queryString).getOrElse("savekey", "")
-    if( savekeyuuid.equals("")){
+    var savekeyuuid = createParamsMap(request.queryString).getOrElse("savekey", "")
+    if (savekeyuuid.equals("")) {
       Logger.debug("Error - no savekey parameter passed to resume")
       Ok(views.html.save_for_later.resumeNotExist(lang))
     }
-    else{
-      val f2=form.fill(ResumeSaveForLater(uuid=savekeyuuid))
+    else {
+      val f2 = form.fill(ResumeSaveForLater(uuid = savekeyuuid))
 
       // using an intermediate variable for status to allow easier testing to amend the status in debug
-      val status=checkSaveForLaterInCache(savekeyuuid)
-      status match{
+      val status = checkSaveForLaterInCache(savekeyuuid)
+      status match {
         case "OK" => Ok(views.html.save_for_later.resumeClaim(f2))
         case "NO-CLAIM" => Ok(views.html.save_for_later.resumeNotExist(lang))
         case "FAILED-FINAL" => Ok(views.html.save_for_later.resumeFailedFinal(lang))
         case "EXPIRED" => Ok(views.html.save_for_later.resumeExpired(lang))
-        case s => {BadRequest(views.html.save_for_later.resumeClaim(form.withError("surname", "Failure retrieving claim bad status:"+s)))
+        case s => {
+          BadRequest(views.html.save_for_later.resumeClaim(form.withError("surname", "Failure retrieving claim bad status:" + s)))
         }
       }
     }
@@ -54,32 +55,20 @@ object GResume extends Controller with CachedClaim with Navigable with I18nSuppo
         BadRequest(views.html.save_for_later.resumeClaim(formWithErrors))
       },
       resumeSaveForLater => {
-        println("resume submit got uuid from form:"+resumeSaveForLater.uuid)
-        val resume=resumeSaveForLaterFromCache(resumeSaveForLater, resumeSaveForLater.uuid)
-        resume match{
-          case Some(sfl) =>{
-            println( "resume found matching claim and unlock status is :"+sfl.status+" trys left:"+sfl.remainingAuthenticationAttempts+" app version:"+sfl.appVersion)
-            if(sfl.status.equals("OK")){
-              println("Trying to load resumed claim:"+resumeSaveForLater.uuid)
-              val updatedClaim = claim.update(newkey=resumeSaveForLater.uuid)
-              claim->Redirect(routes.GResume.resume()+"?uuid="+resumeSaveForLater.uuid)
-            }
-            else{
-              Ok(views.html.save_for_later.resumeClaim(form))
-            }
-          }
-          case _ => {
-            println( "No claim was found .. present failed final error")
-            Ok(views.html.save_for_later.resumeNotExist(lang))
-          }
+        val retrievedClaim = resumeSaveForLaterFromCache(resumeSaveForLater, resumeSaveForLater.uuid)
+        retrievedClaim match {
+          case Some(sfl) if sfl.status.equals("OK") => claim -> Redirect(routes.GResume.resume() + "?uuid=" + resumeSaveForLater.uuid)
+          case Some(sfl) if sfl.status.equals("FAILED-RETRY") && sfl.remainingAuthenticationAttempts == 2 => BadRequest(views.html.save_for_later.resumeClaim(form.fill(resumeSaveForLater).withGlobalError("Your details don't match this application. You have 2 attempts left.")))
+          case Some(sfl) if sfl.status.equals("FAILED-RETRY") && sfl.remainingAuthenticationAttempts == 1 => BadRequest(views.html.save_for_later.resumeClaim(form.fill(resumeSaveForLater).withGlobalError("Your details don't match this application.You have 1 attempt left.")))
+          case Some(sfl) if sfl.status.equals("FAILED-FINAL") => Ok(views.html.save_for_later.resumeFailedFinal(lang))
+          case _ => Ok(views.html.save_for_later.resumeClaim(form))
         }
       })
   }
 
   def resume = claimingWithResume { implicit claim => implicit request => implicit lang =>
-      Logger.debug("resumeClaim got claim uuid:"+claim.uuid+" and redirecting to:"+claim.navigation.current.toString())
-      claim->Redirect(claim.navigation.current.toString())
+    Logger.debug("resumeClaim got claim uuid:" + claim.uuid + " and redirecting to:" + claim.navigation.current.toString())
+    claim -> Redirect(claim.navigation.current.toString())
   }
-
 }
 
