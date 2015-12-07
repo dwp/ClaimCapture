82e6f066bdbf132a216ed6660c638743c61b43fb
Colin Greenwood <colingreenwood@scrumconnect.com>
2015-12-07 23:55:42 +0000

    CADS-329 : Fixups tests.

--- c3/test/controllers/save_for_later/GResumeSpec.scala
+++ c3/test/controllers/save_for_later/GResumeSpec.scala
@@ -1,6 +1,8 @@
 package controllers.save_for_later
 
 import models.domain._
+import models.view.CachedClaim
+import models.view.cache.{EncryptedCacheHandling, CacheHandling}
 import org.specs2.mutable._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -9,12 +11,6 @@ import utils.{LightFakeApplication, WithApplication}
 class GResumeSpec extends Specification {
 
   "Resume controller" should {
-    "present resume screen" in new WithApplication with Claiming {
-      val request = FakeRequest()
-      val result = GResume.present(request)
-      status(result) mustEqual OK
-    }
-
     "block present when switched off" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "false"))) with Claiming {
       val request = FakeRequest()
       val result = GResume.present(request)
@@ -32,7 +28,6 @@ class GResumeSpec extends Specification {
       status(result) mustEqual BAD_REQUEST
     }
 
-/*
 
     "return error present when switched off" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "false"))) with Claiming {
       val request = FakeRequest()
@@ -42,13 +37,39 @@ class GResumeSpec extends Specification {
       status(result) mustEqual BAD_REQUEST
     }
 
-    "allow submit" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "true"))) with Claiming {
+
+    "return validation error not found when no uuid is passed to present()" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "true"))) with Claiming {
       val request = FakeRequest()
-        .withFormUrlEncodedBody("anyTrips" -> "yes")
-      val result = GResume.submit(request)
-      status(result) mustEqual OK
+      val result = GResume.present(request)
+      val bodyText: String = contentAsString(result)
+      println("Submit result:"+bodyText)
+      println( "Status:"+status(result))
+      bodyText must contain("This application can't be found")
+      status(result) mustEqual BAD_REQUEST
     }
 
+    "return validation error not found when claim not found" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "true"))) with Claiming {
+      val request=FakeRequest(GET, "?savekey=1234")
+      val result=GResume.present(request)
+      val bodyText: String = contentAsString(result)
+      println("Submit result:"+bodyText)
+      println( "Status:"+status(result))
+      bodyText must contain("This application can't be found")
+      status(result) mustEqual BAD_REQUEST
+    }
+/*
+    "return validation error when no fields completed" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "true"))) with Claiming {
+      var claim = new Claim(CachedClaim.key, uuid="123456")
+      val encryptedCacheHandling = new EncryptedCacheHandling() { val cacheKey = "123456" }
+      encryptedCacheHandling.saveForLaterInCache(claim, "/lastlocation")
+
+      val request = FakeRequest().withFormUrlEncodedBody("savekey" -> "123456")
+      val result = GResume.present(request)
+      val bodyText: String = contentAsString(result)
+      println("Submit result:"+bodyText)
+      println( "Status:"+status(result))
+      status(result) mustEqual BAD_REQUEST
+    }
 */
   }
   section("unit", models.domain.YourPartner.id)
