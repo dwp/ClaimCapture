7d1702f84f6121cf4665b2509dba45a7b1be8c41
Colin Greenwood <colingreenwood@scrumconnect.com>
2015-12-09 09:01:21 +0000

    CADS-329 : Save for later fixup test for expired status and typo in expired screen "appication" wording..

--- c3/test/controllers/save_for_later/GResumeSpec.scala
+++ c3/test/controllers/save_for_later/GResumeSpec.scala
@@ -5,10 +5,10 @@ import models.domain._
 import models.view.CachedClaim
 import models.view.cache.{EncryptedCacheHandling, CacheHandling}
 import org.specs2.mutable._
-import play.api.i18n.Lang
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import utils.{LightFakeApplication, WithApplication}
+import utils.{SaveForLaterEncryption, LightFakeApplication, WithApplication}
+import scala.concurrent.duration._
 
 class GResumeSpec extends Specification {
 
@@ -28,7 +28,6 @@ class GResumeSpec extends Specification {
       bodyText must contain("This service is currently switched off")
       status(result) mustEqual BAD_REQUEST
     }
-
 
     "return error present when switched off" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "false"))) with Claiming {
       val request = FakeRequest()
@@ -65,12 +64,25 @@ class GResumeSpec extends Specification {
       val request=FakeRequest(GET, "?savekey=123456")
       val result = GResume.present(request)
       val bodyText: String = contentAsString(result)
-      status(result) mustEqual OK
       bodyText must contain( "Enter your details to resume your application")
+      status(result) mustEqual OK
     }
 
-    /* HOW TEST EXPIRED CLAIM ??? This needs setting by back end process*/
     "return sfl claim expired screen when claim is found in sfl cache with status of EXPIRED" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "true"))) with Claiming {
+      // Inject the saved claim directly to cache so we can set the status to EXPIRED
+      var claim = new Claim(CachedClaim.key, uuid="223344")
+      val details = new YourDetails("",None, "John",None, "Green",NationalInsuranceNumber(Some("AB123456D")), DayMonthYear(1, 1, 1970))
+      claim=claim+details
+      val encryptedCacheHandling = new EncryptedCacheHandling() { val cacheKey = "223344" }
+      val key=encryptedCacheHandling.createSaveForLaterKey(claim)
+      val saveForLater=new SaveForLater(SaveForLaterEncryption.encryptClaim(claim,key), "/about-you/nationality-and-residency", 3, "EXPIRED", -1, -1, "V1.00"  )
+      encryptedCacheHandling.cache.set("SFL-223344", saveForLater, Duration(CacheHandling.saveForLaterCacheExpiry + CacheHandling.saveForLaterGracePeriod, DAYS))
+
+      val request=FakeRequest(GET, "?savekey=223344")
+      val result = GResume.present(request)
+      val bodyText: String = contentAsString(result)
+      bodyText must contain( "This application has been deleted")
+      status(result) mustEqual BAD_REQUEST
     }
 
     "return sfl claim screen with 2 retries left when claim has been tried once" in new WithApplication(app = LightFakeApplication(additionalConfiguration = Map("saveForLaterResumeEnabled" -> "true"))) with Claiming {
