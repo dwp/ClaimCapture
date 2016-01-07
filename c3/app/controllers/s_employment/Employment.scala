c6bb3ff0ec1872e2b14a923639e40cc0ce4c825c
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-01-07 13:13:51 +0000

    CADS-517 : Save for later on employment. Fix population of temp page data from saved cache. Employment uses specific fillWithJobId form filler that needs to take from saveForLaterCurrentPageData.

--- c3/app/controllers/s_employment/Employment.scala
+++ c3/app/controllers/s_employment/Employment.scala
@@ -13,20 +13,26 @@ import models.view.{Navigable, CachedClaim}
 import models.domain._
 import play.api.i18n._
 
-object Employment extends Controller with CachedClaim  with Navigable with I18nSupport {
+object Employment extends Controller with CachedClaim with Navigable with I18nSupport {
   override val messagesApi: MessagesApi = current.injector.instanceOf[MMessages]
+
   implicit def jobFormFiller[Q <: QuestionGroup](form: Form[Q])(implicit classTag: ClassTag[Q]) = new {
     def fillWithJobID(qi: QuestionGroup.Identifier, iterationID: String)(implicit claim: Claim): Form[Q] = {
-      claim.questionGroup(Jobs).getOrElse(Jobs()).asInstanceOf[Jobs].jobs.find(_.iterationID == iterationID).getOrElse(Iteration("", List())).find(_.identifier.id == qi.id) match {
-        case Some(q: Q) => form.fill(q)
-        case _ => form
+      claim.saveForLaterCurrentPageData.isEmpty match {
+        case true => {
+          claim.questionGroup(Jobs).getOrElse(Jobs()).asInstanceOf[Jobs].jobs.find(_.iterationID == iterationID).getOrElse(Iteration("", List())).find(_.identifier.id == qi.id) match {
+            case Some(q: Q) => form.fill(q)
+            case _ => form
+          }
+        }
+        case false => form.copy[Q](data=claim.saveForLaterCurrentPageData)
       }
     }
   }
 
   def completedQuestionGroups(questionGroupIdentifier: QuestionGroup.Identifier, iterationID: String)(implicit claim: Claim): List[QuestionGroup] = {
-    (for { jobs <- claim.questionGroup[Jobs]
-           job <- jobs.find(_.iterationID == iterationID) }
+    (for {jobs <- claim.questionGroup[Jobs]
+          job <- jobs.find(_.iterationID == iterationID)}
       yield job.questionGroups.filter(_.identifier.index < questionGroupIdentifier.index)).getOrElse(Nil)
   }
 
@@ -36,7 +42,7 @@ object Employment extends Controller with CachedClaim  with Navigable with I18nS
   }
 
 
-  def fillForm(implicit request:Request[_],claim:Claim)= {
+  def fillForm(implicit request: Request[_], claim: Claim) = {
     claim.questionGroup[BeenEmployed] match {
       case Some(qg) => GBeenEmployed.form.fill(qg)
       case None => GBeenEmployed.form
@@ -47,20 +53,20 @@ object Employment extends Controller with CachedClaim  with Navigable with I18nS
     "deleteId" -> nonEmptyText
   )(DeleteId.apply)(DeleteId.unapply))
 
-  def deleteRedirect(updatedJobs:Jobs) = {
+  def deleteRedirect(updatedJobs: Jobs) = {
 
 
   }
 
-  def delete = claimingWithCheck {implicit claim => implicit request => implicit request2lang =>
+  def delete = claimingWithCheck { implicit claim => implicit request => implicit request2lang =>
 
     deleteForm.bindEncrypted.fold(
-      errors    =>  BadRequest(views.html.s_employment.g_beenEmployed(fillForm)),
-      deleteForm=>  {
+      errors => BadRequest(views.html.s_employment.g_beenEmployed(fillForm)),
+      deleteForm => {
         val updatedJobs = jobs.delete(deleteForm.id)
         if (updatedJobs.jobs == jobs.jobs) BadRequest(views.html.s_employment.g_beenEmployed(fillForm))
-        else claim.update(updatedJobs) -> (if(updatedJobs.jobs.size == 0 ) Redirect(controllers.s_self_employment.routes.GEmployment.present())
-                                          else Redirect(controllers.s_employment.routes.GBeenEmployed.present()))
+        else claim.update(updatedJobs) -> (if (updatedJobs.jobs.size == 0) Redirect(controllers.s_self_employment.routes.GEmployment.present())
+        else Redirect(controllers.s_employment.routes.GBeenEmployed.present()))
       }
     )
   }
