3c2d67737e52c87b17916c7073d60245cd3c3867
Peter Whitehead <peter@scrumconnect.com>
2016-01-13 14:46:30 +0000

    CADS-542 : Change links on preview page do not have GA events on them. Lowered version numbers

--- c3/app/models/view/ClaimHandling.scala
+++ c3/app/models/view/ClaimHandling.scala
@@ -26,7 +26,7 @@ object ClaimHandling {
   type ClaimResult = (Claim, Result)
   // Versioning
   val C3VERSION = "C3Version"
-  val C3VERSION_VALUE = "3.3"
+  val C3VERSION_VALUE = "3.2"
   val applicationFinished = "application-finished"
 
 }
