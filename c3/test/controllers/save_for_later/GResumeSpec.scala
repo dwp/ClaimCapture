a7cbfd3be2e48f619b6f90653a054d9e41177ad6
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-01-17 23:24:01 +0000

    CADS-521 : Save for later expiry properties using seconds rather than days or milliseconds. Fixup bad test import by moving tests to new class.

--- c3/test/controllers/save_for_later/GResumeSpec.scala
+++ c3/test/controllers/save_for_later/GResumeSpec.scala
@@ -98,50 +98,6 @@ class GResumeSpec extends Specification {
       bodyText must contain( "This application has been deleted")
       status(result) mustEqual BAD_REQUEST
     }
-/* COLING temp removed as import conflict ... will fix Sun 15/01/2016
-    // Save a claim, resume, sleep a sec, resume again and confirm that the expiryDates have increased between the first and second resumes
-    "set the expiry date correctly on the claim and be visible when successfully resumed" in new
-        WithApplication(app = LightFakeApplication(additionalConfiguration =
-          Map("cache.saveForLaterCacheExpirySecs" -> "1000", "cache.saveForLaterGracePeriodSecs" -> "2000", "saveForLaterResumeEnabled" -> "true", "saveForLater.uuid.secret.key" -> encryptkey))) with Claiming {
-      var claim = new Claim(CachedClaim.key, uuid = uuid)
-      val details = new YourDetails("Mr", "John", None, "Green", NationalInsuranceNumber(Some("AB123456D")), DayMonthYear(1, 1, 1970))
-      claim = claim + details
-      val encryptedCacheHandling = new EncryptedCacheHandling() {
-        val cacheKey = uuid
-      }
-      encryptedCacheHandling.saveForLaterInCache(claim, "/lastlocation")
-
-      val resumeSaveForLater=new ResumeSaveForLater("John","Green",NationalInsuranceNumber(Some("AB123456D")), DayMonthYear(1, 1, 1970), decodeint)
-      val resume1 = resumeSaveForLaterFromCache(resumeSaveForLater, claim.getDecryptedUuid(resumeSaveForLater.uuid))
-      Thread.sleep(1000)
-      val resume2 = resumeSaveForLaterFromCache(resumeSaveForLater, claim.getDecryptedUuid(resumeSaveForLater.uuid))
-      resume2.get.applicationExpiry > resume1.get.applicationExpiry must beTrue
-      resume2.get.cacheExpiryPeriod > resume1.get.cacheExpiryPeriod must beTrue
-    }
-
-    // Save a claim, attempt to resume with bad credentials, sleep a sec and attempt resume again and confirm that the expiryDates have not increased
-    // between the first and second resumes
-    "renew the expiry date on the claim when successfully resumed" in new
-        WithApplication(app = LightFakeApplication(additionalConfiguration =
-          Map("cache.saveForLaterCacheExpirySecs" -> "1000", "cache.saveForLaterGracePeriodSecs" -> "2000", "saveForLaterResumeEnabled" -> "true", "saveForLater.uuid.secret.key" -> encryptkey))) with Claiming {
-      var claim = new Claim(CachedClaim.key, uuid = uuid)
-      val details = new YourDetails("Mr", "John", None, "Green", NationalInsuranceNumber(Some("AB123456D")), DayMonthYear(1, 1, 1970))
-      claim = claim + details
-      val encryptedCacheHandling = new EncryptedCacheHandling() {
-        val cacheKey = uuid
-      }
-      encryptedCacheHandling.saveForLaterInCache(claim, "/lastlocation")
-
-      val badResumeSaveForLater=new ResumeSaveForLater("BadJohn","Green",NationalInsuranceNumber(Some("AB123456D")), DayMonthYear(1, 1, 1970), decodeint)
-      val badResume1 = resumeSaveForLaterFromCache(badResumeSaveForLater, claim.getDecryptedUuid(badResumeSaveForLater.uuid))
-      Thread.sleep(1000)
-      val badResume2 = resumeSaveForLaterFromCache(badResumeSaveForLater, claim.getDecryptedUuid(badResumeSaveForLater.uuid))
-      badResume2.get.applicationExpiry == badResume1.get.applicationExpiry must beTrue
-      badResume2.get.cacheExpiryPeriod == badResume1.get.cacheExpiryPeriod must beTrue
-    }
-
-  */
   }
-
   section("unit", models.domain.YourDetails.id)
 }
