63b0da837f4c4c9d4a6cbc2df6824ed2cde5d789
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-02-02 10:44:59 +0000

    CADS-535 : Feedback store data in IL2 memcache. Add separate Cache tests for EhCache and Memcache.

---
+++ c3/test/models/view/CacheHandlingMemcacheSpec.scala
@@ -0,0 +1,45 @@
+package models.view
+
+import gov.dwp.carers.play2.resilientmemcached.MemcachedCacheApi
+import models.domain.Claiming
+import models.view.cache.EncryptedCacheHandling
+import org.specs2.mutable._
+import play.api.Play._
+import utils.WithApplication
+
+class CacheHandlingMemcacheSpec extends Specification {
+  section("unit")
+  "Memcache Key List Handling" should {
+    "concat keys into single cs string list" in new WithApplication() with Claiming {
+      override val cache: MemcachedCacheApi = current.injector.instanceOf[MemcachedCacheApi]
+      cache.isInstanceOf[MemcachedCacheApi] mustEqual true
+
+      val cacheHandling = new EncryptedCacheHandling() {
+        val cacheKey = "12345678"
+      }
+      cacheHandling.createFeedbackInList("ABCD-1234")
+      cacheHandling.createFeedbackInList("WXYZ-4321")
+      cacheHandling.createFeedbackInList("ABCD-1234")
+
+      val keyList = cacheHandling.getFeedbackList
+      keyList mustEqual ("ABCD-1234,WXYZ-4321")
+    }
+
+    "remove duplicate entries" in new WithApplication() with Claiming {
+      override val cache: MemcachedCacheApi = current.injector.instanceOf[MemcachedCacheApi]
+      cache.isInstanceOf[MemcachedCacheApi] mustEqual true
+
+      val cacheHandling = new EncryptedCacheHandling() {
+        val cacheKey = "12345678"
+      }
+      cacheHandling.createFeedbackInList("ABCD-1234")
+      cacheHandling.createFeedbackInList("WXYZ-4321")
+      cacheHandling.createFeedbackInList("ABCD-1234")
+
+      val keyList = cacheHandling.getFeedbackList
+      keyList mustEqual ("ABCD-1234,WXYZ-4321")
+    }
+  }
+  section("unit")
+}
+
