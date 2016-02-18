3b2a62e572c198b4b3f9530ffa95e2dff131d672
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-02-18 16:24:08 +0000

    CADS-689 : Messages with single apostrophe are broken so added test to pick up any such instances.

--- c3/test/app/MessageFilesSpec.scala
+++ c3/test/app/MessageFilesSpec.scala
@@ -11,6 +11,7 @@ class MessageFilesSpec extends Specification {
     source.close()
     val enFiles = linesFromSource.filter(line => line.contains(".en.")).map(fullEnFilename => fullEnFilename.substring(0, fullEnFilename.indexOf(".en.")))
     val cyFiles = linesFromSource.filter(line => line.contains(".cy.")).map(fullEnFilename => fullEnFilename.substring(0, fullEnFilename.indexOf(".cy.")))
+    val singleAposPattern = ".*[^']'[^'].*"
 
     val enKeys = enFiles.map(
       enFile => {
@@ -33,6 +34,42 @@ class MessageFilesSpec extends Specification {
       }
     ).flatMap(y => y).map(a => a(0))
 
+    val enValues = {
+      enFiles.map(
+        enFile => {
+          val enS = scala.io.Source.fromFile("conf/en/%s.en.properties".format(enFile))
+          val linesFromEnS = enS.getLines.filterNot(_.isEmpty).filterNot(s => s.startsWith("#")).toList
+          val enKeys = linesFromEnS.map(pair => pair.split("=").map(k => k.trim))
+          enS.close()
+          enKeys
+        }
+      ).flatMap(y => y).map(a => if(a.length>1)a(1))
+    }
+
+    val cyValues = {
+      enFiles.map(
+        enFile => {
+          val enS = scala.io.Source.fromFile("conf/cy/%s.cy.properties".format(enFile))
+          val linesFromEnS = enS.getLines.filterNot(_.isEmpty).filterNot(s => s.startsWith("#")).toList
+          val enKeys = linesFromEnS.map(pair => pair.split("=").map(k => k.trim))
+          enS.close()
+          enKeys
+        }
+      ).flatMap(y => y).map(a => if(a.length>1)a(1))
+    }
+
+    val niValues = {
+      enFiles.map(
+        enFile => {
+          val enS = scala.io.Source.fromFile("conf/en/niDirect.en.properties")
+          val linesFromEnS = enS.getLines.filterNot(_.isEmpty).filterNot(s => s.startsWith("#")).toList
+          val enKeys = linesFromEnS.map(pair => pair.split("=").map(k => k.trim))
+          enS.close()
+          enKeys
+        }
+      ).flatMap(y => y).map(a => if(a.length>1)a(1))
+    }
+
     "have corresponding Welsh file for each English file" in {
       enFiles.filterNot(enFile => cyFiles.contains(enFile)) must beEmpty
     }
@@ -47,6 +84,25 @@ class MessageFilesSpec extends Specification {
 
     "each key in a Welsh property file must have a corresponding key in the English file" in {
       cyKeys.filterNot(cyKey => enKeys.contains(cyKey)) must beEmpty
+    }
+
+    "not contain any single apostrophes in english files" in {
+      val badMessages=enValues.filter(value=>value.toString.matches(singleAposPattern))
+      badMessages.foreach( message => println("ERROR - Bad en message single apostrophe in message:"+message))
+      badMessages must beEmpty
+    }
+
+    "not contain any single apostrophes in welsh files" in {
+      val badMessages=cyValues.filter(value=>value.toString.matches(singleAposPattern))
+      badMessages.foreach( message => println("ERROR - Bad cy message single apostrophe in message:"+message))
+      badMessages must beEmpty
+    }
+
+    "not contain any single apostrophes in ni files" in {
+      niValues.size >0 must beTrue
+      val badMessages=niValues.filter(value=>value.toString.matches(singleAposPattern))
+      badMessages.foreach( message => println("ERROR - Bad ni message single apostrophe in message:"+message))
+      badMessages must beEmpty
     }
   }
   section("integration")
