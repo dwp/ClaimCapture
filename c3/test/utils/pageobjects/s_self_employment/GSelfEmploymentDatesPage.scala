137b6b7976a139d7daff808818ade5dcbc3b9cdc
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-03-29 13:51:06 +0100

    CADS-313 : Self employment redesign. Fix tests following dropping SelfEmploymentYourAccounts.

--- c3/test/utils/pageobjects/s_self_employment/GSelfEmploymentDatesPage.scala
+++ c3/test/utils/pageobjects/s_self_employment/GSelfEmploymentDatesPage.scala
@@ -3,21 +3,22 @@ package utils.pageobjects.s_self_employment
 import utils.WithBrowser
 import utils.pageobjects._
 
-final class GAboutSelfEmploymentPage(ctx:PageObjectsContext) extends ClaimPage(ctx, GAboutSelfEmploymentPage.url) {
-  declareYesNo("#areYouSelfEmployedNow", "SelfEmployedAreYouSelfEmployedNow")
+final class GSelfEmploymentDatesPage(ctx:PageObjectsContext) extends ClaimPage(ctx, GAboutSelfEmploymentPage.url) {
+/*  declareYesNo("#areYouSelfEmployedNow", "SelfEmployedAreYouSelfEmployedNow")
   declareDate("#whenDidYouStartThisJob", "SelfEmployedWhenDidYouStartThisJob")
   declareDate("#whenDidTheJobFinish", "SelfEmployedWhenDidTheJobFinish")
   declareYesNo("#haveYouCeasedTrading", "SelfEmployedHaveYouCeasedTrading")
   declareInput("#natureOfYourBusiness", "SelfEmployedNatureofYourBusiness")
+  */
 }
 
-object GAboutSelfEmploymentPage {
-  val url = "/self-employment/about-self-employment"
+object GSelfEmploymentDatesPage {
+  val url = "/self-employment/self-employment-dates"
 
   def apply(ctx:PageObjectsContext) = new GAboutSelfEmploymentPage(ctx)
 }
 
-trait GAboutSelfEmploymentPageContext extends PageContext {
+trait GSelfEmploymentDatesPageContext extends PageContext {
   this: WithBrowser[_] =>
 
   val page = GAboutSelfEmploymentPage (PageObjectsContext(browser))
