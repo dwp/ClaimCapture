bf27ac02dbb9bdb9b3748dd2ae37f5008e132239
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-03-29 23:54:51 +0100

    CADS-313 : Self employment redesign. Fix tests following dropping SelfEmploymentYourAccounts.

--- c3/test/utils/pageobjects/s_self_employment/GSelfEmploymentDatesPage.scala
+++ c3/test/utils/pageobjects/s_self_employment/GSelfEmploymentDatesPage.scala
@@ -3,21 +3,16 @@ package utils.pageobjects.s_self_employment
 import utils.WithBrowser
 import utils.pageobjects._
 
-/*
-                                stillSelfEmployed: String = "",
-                                moreThanYearAgo: Option[String] = None,
-                                startThisWork: Option[DayMonthYear] = None,
-                                haveAccounts: Option[String] = None,
-                                knowTradingYear: Option[String] = None,
-                                tradingYearStart: Option[DayMonthYear] = None,
-                                paidMoney: Option[String] = None,
-                                paidMoneyDate: Option[DayMonthYear] = None,
-                                finishThisWork: Option[DayMonthYear] = None
- */
 final class GSelfEmploymentDatesPage(ctx:PageObjectsContext) extends ClaimPage(ctx, GSelfEmploymentDatesPage.url) {
   declareYesNo("#stillSelfEmployed", "SelfEmployedAreYouSelfEmployedNow")
   declareDate("#startThisWork", "SelfEmployedWhenDidYouStartThisJob")
   declareDate("#finishThisWork", "SelfEmployedWhenDidTheJobFinish")
+  declareYesNo("#moreThanYearAgo", "SelfEmployedMoreThanYearAgo")
+  declareYesNo("#haveAccounts", "SelfEmployedHaveAccounts")
+  declareYesNo("#knowTradingYear", "SelfEmployedKnowTradingYear")
+  declareDate("#tradingYearStart", "SelfEmployedTradingYearStart")
+  declareYesNo("#paidMoney", "SelfEmployedPaidMoney")
+  declareDate("#paidMoneyDate", "SelfEmployedPaidMoneyDate")
 }
 
 object GSelfEmploymentDatesPage {
