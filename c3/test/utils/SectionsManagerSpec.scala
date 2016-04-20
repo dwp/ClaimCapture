15af483f715cc5c1daf597af0f3e4cbd3770f280
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-04-20 17:11:45 +0100

    CADS-863 : Nationality section shuffle. Fixup sections test.

--- c3/test/utils/SectionsManagerSpec.scala
+++ c3/test/utils/SectionsManagerSpec.scala
@@ -9,27 +9,27 @@ class SectionsManagerSpec extends Specification {
   section("unit")
   "Sections Manager" should {
     "Retrieve the correct number of sections" in {
-      SectionsManager.claimSectionsNum(Claim(CachedClaim.key)) mustEqual 10
+      SectionsManager.claimSectionsNum(Claim(CachedClaim.key)) mustEqual 11
     }
 
     "Retrieve the correct section position for some Sections" in {
       implicit val claim = Claim(CachedClaim.key)
       SectionsManager.currentSection(AboutYou) mustEqual 3
-      SectionsManager.currentSection(YourIncome) mustEqual 8
+      SectionsManager.currentSection(YourIncome) mustEqual 9
     }
 
     "Retrieve the correct section positions when there are hidden sections" in new WithApplication {
       implicit val claim = Claim(CachedClaim.key).showHideSection(visible = false, PayDetails)
 
       SectionsManager.currentSection(AboutYou) mustEqual 3
-      SectionsManager.currentSection(Information) mustEqual 9
+      SectionsManager.currentSection(Information) mustEqual 10
     }
 
     "Retrieve the correct section positions when there are hidden employment" in new WithApplication {
       implicit val claim = Claim(CachedClaim.key).showHideSection(visible = false,SelfEmployment)
 
-      SectionsManager.currentSection(YourIncome) mustEqual 8
-      SectionsManager.currentSection(PayDetails) mustEqual 9
+      SectionsManager.currentSection(YourIncome) mustEqual 9
+      SectionsManager.currentSection(PayDetails) mustEqual 10
     }
   }
   section("unit")
