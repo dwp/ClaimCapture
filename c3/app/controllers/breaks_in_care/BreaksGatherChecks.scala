5917e389c9cc83d3de639badec3588d4bd520d1e
Peter Whitehead <peter@scrumconnect.com>
2016-08-08 16:04:36 +0100

    CADS-1151 : Claim: About the care you provide - Respite Care gather

--- c3/app/controllers/breaks_in_care/BreaksGatherChecks.scala
+++ c3/app/controllers/breaks_in_care/BreaksGatherChecks.scala
@@ -87,8 +87,8 @@ trait BreaksGatherChecks {
 
   def requiredStartDateNotAfterEndDate(): Constraint[Break] = Constraint[Break]("constraint.breaksInCareDateRange") { break =>
     break.whoWasAway match {
-      case BreaksInCareGatherOptions.You if (break.whenWereYouAdmitted.isDefined && break.yourStayEnded.isDefined && break.yourStayEnded.get.answer == Mappings.yes) => checkDatesAfter(break.whenWereYouAdmitted, break.yourStayEnded.get.date, "whenWereYouAdmitted.invalidDateRange")
-      case BreaksInCareGatherOptions.DP if (break.whenWasDpAdmitted.isDefined && break.dpStayEnded.isDefined && break.dpStayEnded.get.answer == Mappings.yes) => checkDatesAfter(break.whenWasDpAdmitted, break.dpStayEnded.get.date, "whenWasDpAdmitted.invalidDateRange")
+      case BreaksInCareGatherOptions.You if (break.whenWereYouAdmitted.isDefined && break.yourStayEnded.isDefined && break.yourStayEnded.get.answer == Mappings.yes) => checkDatesAfter(break.whenWereYouAdmitted, break.yourStayEnded.get.date, "yourStayEnded.invalidDateRange")
+      case BreaksInCareGatherOptions.DP if (break.whenWasDpAdmitted.isDefined && break.dpStayEnded.isDefined && break.dpStayEnded.get.answer == Mappings.yes) => checkDatesAfter(break.whenWasDpAdmitted, break.dpStayEnded.get.date, "dpStayEnded.invalidDateRange")
       case _ => Valid
     }
   }
