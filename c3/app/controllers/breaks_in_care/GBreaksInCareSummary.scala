60ff0280f4a0f03c8f7428e31b8ca0fc6c4a4cbb
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-08-11 14:47:29 +0100

    CADS-1153 : BreaksInCare Summary page - Fixup dates for Other not ended / caring-restarted-date aka break-end-date. :)

--- c3/app/controllers/breaks_in_care/GBreaksInCareSummary.scala
+++ c3/app/controllers/breaks_in_care/GBreaksInCareSummary.scala
@@ -51,7 +51,7 @@ object GBreaksInCareSummary extends Controller with CachedClaim with I18nSupport
   private def nextPage(breaksInCareType: BreaksInCareType)(implicit claim: Claim, request: Request[_]) = {
     if (breaksInCareType.hospital.isDefined) routes.GBreaksInCareHospital.present(IterationID(form))
     else if (breaksInCareType.carehome.isDefined) routes.GBreaksInCareRespite.present(IterationID(form))
-    else if (breaksInCareType.other.isDefined) routes.GBreaksInCareOther.present(IterationID(form))
+    else if (breaksInCareType.other.equals(Some(Mappings.yes))) routes.GBreaksInCareOther.present(IterationID(form))
     else controllers.s_education.routes.GYourCourseDetails.present
   }
 
