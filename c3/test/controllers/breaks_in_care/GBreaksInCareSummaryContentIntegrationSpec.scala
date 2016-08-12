a5d5e4db34bcc9c9a4df552fa2a3d9f4d3144d3e
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-08-12 09:49:29 +0100

    CADS-1153 : BreaksInCare Summary page - Fixup Summary page int tests following previous move of gTypeBreak checkboxes into summary page

--- c3/test/controllers/breaks_in_care/GBreaksInCareSummaryContentIntegrationSpec.scala
+++ c3/test/controllers/breaks_in_care/GBreaksInCareSummaryContentIntegrationSpec.scala
@@ -9,6 +9,12 @@ import utils.pageobjects.s_claim_date.GClaimDatePage
 import utils.{WithBrowser, WithJsBrowser}
 
 class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
+  // 3 rows in breaks summary table for single break ... 1) header 2) data row 3) hidden delete confirm prompt
+  // 6 columns in breaks data row Who/Where/From/To/Change-link/Delete-link
+  val ROWSINHEADER = 1
+  val ROWSPERBREAK = 2
+  val COLSINBREAKROW = 6
+
   section("integration", models.domain.YourIncomes.id)
   "Breaks in care summary page" should {
     "be presented" in new WithBrowser with PageObjects {
@@ -17,38 +23,27 @@ class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
       page must beAnInstanceOf[GBreaksInCareSummaryPage]
     }
 
-    "be presented with correct dynamic question labels" in new WithBrowser with PageObjects with WithBrowserHelper {
+    "be presented with correct dynamic type-checkbox and other-yesno question labels" in new WithBrowser with PageObjects with WithBrowserHelper {
       GClaimDatePage.fillClaimDate(context, testData => {
         testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
       })
       GTheirPersonalDetailsPage.fillDpDetails(context, testData => {})
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
-      $("#breaksummary_other_questionLabel").getText() mustEqual ("Since 1 October 2016, were there any times you or Albert Johnson have been in hospital, respite or a care home for at least a week or where you've not provided care for 35 hours a week?")
+      $("#breaktype_questionLabel").getText() mustEqual ("Since 1 October 2016 have there been any times you or Albert Johnson have been in hospital, respite or care home for at least a week?")
+      $("#breaktype_other_questionLabel").getText() mustEqual ("Have there been any times you've not provided care for Albert Johnson for 35 hours a week?")
     }
 
-    "be presented with correct radio options when select yes and no existing breaks" in new WithBrowser with PageObjects with WithBrowserHelper {
+    "be presented with correct checkbox options" in new WithBrowser with PageObjects with WithBrowserHelper {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
 
-      // A time when you have not provided care for 35 hours a week
-      $("#breaksummary_answer").find("span", 0).getText() shouldEqual "Hospital admission"
-      $("#breaksummary_answer").find("span", 1).getText() shouldEqual "Respite or care home admission"
-      $("#breaksummary_answer").find("span", 2).getText() shouldEqual "A time when you have not provided care for 35 hours a week"
+      $("[for=breaktype_hospital]").find("span").getText() shouldEqual "Hospital"
+      $("[for=breaktype_carehome]").find("span").getText() shouldEqual "Respite or care home"
+      $("[for=breaktype_none]").find("span").getText() shouldEqual "None"
     }
 
-    "be presented with correct radio options when select yes and got existing breaks" in new WithBrowser with PageObjects with WithBrowserHelper {
-      GBreaksInCareHospitalPage.fillDetails(context, testData => {})
-      val page = GBreaksInCareSummaryPage(context)
-      page goToThePage()
-
-      // Another time when you have not provided care for 35 hours a week
-      $("#breaksummary_answer").find("span", 0).getText() shouldEqual "Hospital admission"
-      $("#breaksummary_answer").find("span", 1).getText() shouldEqual "Respite or care home admission"
-      $("#breaksummary_answer").find("span", 2).getText() shouldEqual "Another time when you have not provided care for 35 hours a week"
-    }
-
-    "present 1 error with correct wording if no fields are populated and no existing breaks" in new WithJsBrowser with PageObjects {
+    "present 2 errors with correct dynamic wording if no fields are populated and no existing breaks" in new WithJsBrowser with PageObjects {
       GClaimDatePage.fillClaimDate(context, testData => {
         testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
       })
@@ -57,11 +52,12 @@ class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
       val errors = page.submitPage().listErrors
-      errors.size mustEqual 1
-      errors(0) mustEqual ("Since 1 October 2016, were there any times you or Albert Johnson have been in hospital, respite or a care home for at least a week or where you've not provided care for 35 hours a week? - You must complete this section")
+      errors.size mustEqual 2
+      errors(0) mustEqual ("Since 1 October 2016 have there been any times you or Albert Johnson have been in hospital, respite or care home for at least a week? - You must select 'None' if only None applies.")
+      errors(1) mustEqual ("Have there been any times you've not provided care for Albert Johnson for 35 hours a week? - You must complete this section")
     }
 
-    "present 1 error with correct wording if no fields are populated and got existing hospital break" in new WithJsBrowser with PageObjects {
+    "present 2 errors with correct dynamic wording if no fields are populated and got existing breaks" in new WithJsBrowser with PageObjects {
       GClaimDatePage.fillClaimDate(context, testData => {
         testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
       })
@@ -71,11 +67,12 @@ class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
       val errors = page.submitPage().listErrors
-      errors.size mustEqual 1
-      errors(0) mustEqual ("Since 1 October 2016, were there any other times you or Albert Johnson have been in hospital, respite or a care home for at least a week or where you've not provided care for 35 hours a week? - You must complete this section")
+      errors.size mustEqual 2
+      errors(0) mustEqual ("Since 1 October 2016 have there been any other times you or Albert Johnson have been in hospital, respite or care home for at least a week? - You must select 'None' if only None applies.")
+      errors(1) mustEqual ("Have there been any other times you've not provided care for Albert Johnson for 35 hours a week? - You must complete this section")
     }
 
-    "present 1 error with correct wording when select Yes-got-other-times but not selected break-type radio option" in new WithJsBrowser with PageObjects {
+    "present YesNo-Other error with correct wording when submit after select Hospital checkbox but not selected YesNo-got-other-times option" in new WithJsBrowser with PageObjects {
       GClaimDatePage.fillClaimDate(context, testData => {
         testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
       })
@@ -84,17 +81,73 @@ class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
 
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
-      browser.click("#breaksummary_other_yes")
+      browser.click("#breaktype_hospital")
       val errors = page.submitPage().listErrors
       errors.size mustEqual 1
-      errors(0) mustEqual ("What do you want to add? - You must complete this section")
+      errors(0) mustEqual ("Have there been any other times you've not provided care for Albert Johnson for 35 hours a week? - You must complete this section")
     }
 
-    // 3 rows in breaks summary table for single break ... 1) header 2) data row 3) hidden delete confirm prompt
-    // 6 columns in breaks data row Who/Where/From/To/Change-link/Delete-link
-    val ROWSINHEADER=1
-    val ROWSPERBREAK=2
-    val COLSINBREAKROW=6
+    "present YesNo-Other error with correct wording when submit after select Care Home checkbox but not selected YesNo-got-other-times option" in new WithJsBrowser with PageObjects {
+      GClaimDatePage.fillClaimDate(context, testData => {
+        testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
+      })
+      GTheirPersonalDetailsPage.fillDpDetails(context, testData => {})
+      GBreaksInCareHospitalPage.fillDetails(context, testData => {})
+
+      val page = GBreaksInCareSummaryPage(context)
+      page goToThePage()
+      browser.click("#breaktype_carehome")
+      val errors = page.submitPage().listErrors
+      errors.size mustEqual 1
+      errors(0) mustEqual ("Have there been any other times you've not provided care for Albert Johnson for 35 hours a week? - You must complete this section")
+    }
+
+    "present YesNo-Other error with correct wording when submit after select None checkbox but not selected YesNo-got-other-times option" in new WithJsBrowser with PageObjects {
+      GClaimDatePage.fillClaimDate(context, testData => {
+        testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
+      })
+      GTheirPersonalDetailsPage.fillDpDetails(context, testData => {})
+      GBreaksInCareHospitalPage.fillDetails(context, testData => {})
+
+      val page = GBreaksInCareSummaryPage(context)
+      page goToThePage()
+      browser.click("#breaktype_none")
+      val errors = page.submitPage().listErrors
+      errors.size mustEqual 1
+      errors(0) mustEqual ("Have there been any other times you've not provided care for Albert Johnson for 35 hours a week? - You must complete this section")
+    }
+
+    "present Too-Many-Options error with correct wording when submit after select Hospital and None" in new WithJsBrowser with PageObjects {
+      GClaimDatePage.fillClaimDate(context, testData => {
+        testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
+      })
+      GTheirPersonalDetailsPage.fillDpDetails(context, testData => {})
+
+      val page = GBreaksInCareSummaryPage(context)
+      page goToThePage()
+      browser.click("#breaktype_hospital")
+      browser.click("#breaktype_none")
+      browser.click("#breaktype_other_no")
+      val errors = page.submitPage().listErrors
+      errors.size mustEqual 1
+      errors(0) mustEqual ("Since 1 October 2016 have there been any times you or Albert Johnson have been in hospital, respite or care home for at least a week? - You must select 'Hospital','Respite or care home' or 'None'.")
+    }
+
+    "present Too-Many-Options error with correct wording when submit after select Respite and None" in new WithJsBrowser with PageObjects {
+      GClaimDatePage.fillClaimDate(context, testData => {
+        testData.ClaimDateWhenDoYouWantYourCarersAllowanceClaimtoStart = "01/10/2016"
+      })
+      GTheirPersonalDetailsPage.fillDpDetails(context, testData => {})
+
+      val page = GBreaksInCareSummaryPage(context)
+      page goToThePage()
+      browser.click("#breaktype_carehome")
+      browser.click("#breaktype_none")
+      browser.click("#breaktype_other_no")
+      val errors = page.submitPage().listErrors
+      errors.size mustEqual 1
+      errors(0) mustEqual ("Since 1 October 2016 have there been any times you or Albert Johnson have been in hospital, respite or care home for at least a week? - You must select 'Hospital','Respite or care home' or 'None'.")
+    }
 
     "display summary table with just hospital break added" in new WithJsBrowser with PageObjects {
       GBreaksInCareHospitalPage.fillDetails(context, testData => {
@@ -104,16 +157,16 @@ class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
 
-      val summaryTableRows=browser.find("#summary-table tr")
-      summaryTableRows.size() mustEqual ROWSINHEADER+ROWSPERBREAK
-      val breakDataRow=browser.find("#summary-table tr", 1)
+      val summaryTableRows = browser.find("#summary-table tr")
+      summaryTableRows.size() mustEqual ROWSINHEADER + ROWSPERBREAK
+      val breakDataRow = browser.find("#summary-table tr", 1)
       breakDataRow.find("td").size() mustEqual COLSINBREAKROW
-      breakDataRow.find("td",0).getText() mustEqual("You")
-      breakDataRow.find("td",1).getText() mustEqual("Hospital")
-      breakDataRow.find("td",2).getText() mustEqual("01/01/2010")
-      breakDataRow.find("td",3).getText() mustEqual("Not ended")
-      breakDataRow.find("td",4).find("input").getAttribute("value") mustEqual( "Change")
-      breakDataRow.find("td",5).find("input").getAttribute("value") mustEqual( "Delete")
+      breakDataRow.find("td", 0).getText() mustEqual ("You")
+      breakDataRow.find("td", 1).getText() mustEqual ("Hospital")
+      breakDataRow.find("td", 2).getText() mustEqual ("01/01/2010")
+      breakDataRow.find("td", 3).getText() mustEqual ("Not ended")
+      breakDataRow.find("td", 4).find("input").getAttribute("value") mustEqual ("Change")
+      breakDataRow.find("td", 5).find("input").getAttribute("value") mustEqual ("Delete")
     }
 
     "display summary table with just respite break added" in new WithJsBrowser with PageObjects {
@@ -122,37 +175,34 @@ class GBreaksInCareSummaryContentIntegrationSpec extends Specification {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
 
-      val summaryTableRows=browser.find("#summary-table tr")
-      summaryTableRows.size() mustEqual ROWSINHEADER+ROWSPERBREAK
-      val breakDataRow=browser.find("#summary-table tr", 1)
+      val summaryTableRows = browser.find("#summary-table tr")
+      summaryTableRows.size() mustEqual ROWSINHEADER + ROWSPERBREAK
+      val breakDataRow = browser.find("#summary-table tr", 1)
       breakDataRow.find("td").size() mustEqual COLSINBREAKROW
-      breakDataRow.find("td",0).getText() mustEqual("You")
-      breakDataRow.find("td",1).getText() mustEqual("Respite or care home")
-      breakDataRow.find("td",2).getText() mustEqual("01/10/2015")
-      breakDataRow.find("td",3).getText() mustEqual("Not ended")
-      breakDataRow.find("td",4).find("input").getAttribute("value") mustEqual( "Change")
-      breakDataRow.find("td",5).find("input").getAttribute("value") mustEqual( "Delete")
+      breakDataRow.find("td", 0).getText() mustEqual ("You")
+      breakDataRow.find("td", 1).getText() mustEqual ("Respite or care home")
+      breakDataRow.find("td", 2).getText() mustEqual ("01/10/2015")
+      breakDataRow.find("td", 3).getText() mustEqual ("Not ended")
+      breakDataRow.find("td", 4).find("input").getAttribute("value") mustEqual ("Change")
+      breakDataRow.find("td", 5).find("input").getAttribute("value") mustEqual ("Delete")
     }
 
     "display summary table with just other-care break added" in new WithJsBrowser with PageObjects {
       GTheirPersonalDetailsPage.fillDpDetails(context, testData => {})
-
-      // WAITING ON OtherPage.fillDetails ...
-/*      GBreaksInCareOtherPage.fillDetails(context, testData => {})
+      GBreaksInCareOtherPage.fillDetails(context, testData => {})
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
 
-      val summaryTableRows=browser.find("#summary-table tr")
-      summaryTableRows.size() mustEqual ROWSINHEADER+ROWSPERBREAK
-      val breakDataRow=browser.find("#summary-table tr", 1)
+      val summaryTableRows = browser.find("#summary-table tr")
+      summaryTableRows.size() mustEqual ROWSINHEADER + ROWSPERBREAK
+      val breakDataRow = browser.find("#summary-table tr", 1)
       breakDataRow.find("td").size() mustEqual COLSINBREAKROW
-      breakDataRow.find("td",0).getText() mustEqual("You")
-      breakDataRow.find("td",1).getText() mustEqual("Respite or care home")
-      breakDataRow.find("td",2).getText() mustEqual("01/10/2015")
-      breakDataRow.find("td",3).getText() mustEqual("Not ended")
-      breakDataRow.find("td",4).find("input").getAttribute("value") mustEqual( "Change")
-      breakDataRow.find("td",5).find("input").getAttribute("value") mustEqual( "Delete")
-      */
+      breakDataRow.find("td", 0).getText() mustEqual ("You")
+      breakDataRow.find("td", 1).getText() mustEqual ("Other")
+      breakDataRow.find("td", 2).getText() mustEqual ("01/10/2015")
+      breakDataRow.find("td", 3).getText() mustEqual ("Not ended")
+      breakDataRow.find("td", 4).find("input").getAttribute("value") mustEqual ("Change")
+      breakDataRow.find("td", 5).find("input").getAttribute("value") mustEqual ("Delete")
     }
   }
   section("integration", models.domain.YourIncomes.id)
--- c3/test/controllers/breaks_in_care/GBreaksInCareSummaryNavigationIntegrationSpec.scala
+++ c3/test/controllers/breaks_in_care/GBreaksInCareSummaryNavigationIntegrationSpec.scala
@@ -1,16 +1,18 @@
 package controllers.breaks_in_care
 
-import controllers.mappings.Mappings
-import models.domain.Breaks
 import org.specs2.mutable._
 import utils.pageobjects._
 import utils.pageobjects.breaks_in_care._
-import utils.pageobjects.s_care_you_provide.GTheirPersonalDetailsPage
-import utils.pageobjects.s_claim_date.GClaimDatePage
 import utils.pageobjects.s_education.GYourCourseDetailsPage
 import utils.{WithBrowser, WithJsBrowser}
 
 class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
+  // 3 rows in breaks summary table for single break ... 1) header 2) data row 3) hidden delete confirm prompt
+  // 6 columns in breaks data row Who/Where/From/To/Change-link/Delete-link
+  val ROWSINHEADER = 1
+  val ROWSPERBREAK = 2
+  val COLSINBREAKROW = 6
+
   section("integration", models.domain.YourIncomes.id)
   "Breaks in care summary page" should {
     "be presented" in new WithBrowser with PageObjects {
@@ -19,13 +21,13 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
       page must beAnInstanceOf[GBreaksInCareSummaryPage]
     }
 
-    "navigate to hospital details page on valid submission i.e. hosptial selected" in new WithJsBrowser with PageObjects {
+    "navigate to hospital details page on valid submission i.e. hospital selected" in new WithJsBrowser with PageObjects {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
 
       val claim = new TestData
-      claim.BreakSummaryOtherYesNo = Mappings.yes
-      claim.BreakSummaryAnswer = Breaks.hospital
+      claim.BreaktypeHospitalCheckbox = "true"
+      claim.BreaktypeOtherYesNo = "no"
       page fillPageWith claim
       val nextPage = page submitPage()
 
@@ -37,50 +39,44 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
       page goToThePage()
 
       val claim = new TestData
-      claim.BreakSummaryOtherYesNo = Mappings.yes
-      claim.BreakSummaryAnswer = Breaks.carehome
+
+      claim.BreaktypeCareHomeCheckbox = "true"
+      claim.BreaktypeOtherYesNo = "no"
       page fillPageWith claim
       val nextPage = page submitPage()
 
       nextPage must beAnInstanceOf[GBreaksInCareRespitePage]
     }
 
-    "navigate to other-care details page on valid submission i.e. another selected" in new WithJsBrowser with PageObjects {
+    "navigate to other-care details page on valid submission i.e. another = yes selected" in new WithJsBrowser with PageObjects {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
 
       val claim = new TestData
-      claim.BreakSummaryOtherYesNo = Mappings.yes
-      claim.BreakSummaryAnswer = Breaks.another
+      claim.BreaktypeNoneCheckbox = "true"
+      claim.BreaktypeOtherYesNo = "yes"
       page fillPageWith claim
       val nextPage = page submitPage()
 
       nextPage must beAnInstanceOf[GBreaksInCareOtherPage]
     }
 
-    "navigate to education page on valid submission - no further details" in new WithJsBrowser with PageObjects {
+    "navigate to education page on valid submission - no further breaks to add" in new WithJsBrowser with PageObjects {
       val page = GBreaksInCareSummaryPage(context)
       page goToThePage()
-      browser.click("#breaksummary_other_no")
 
       val claim = new TestData
-      claim.BreakSummaryOtherYesNo = Mappings.no
+      claim.BreaktypeNoneCheckbox = "true"
+      claim.BreaktypeOtherYesNo = "no"
       page fillPageWith claim
       val nextPage = page submitPage()
 
       nextPage must beAnInstanceOf[GYourCourseDetailsPage]
     }
 
-
-    // 3 rows in breaks summary table for single break ... 1) header 2) data row 3) hidden delete confirm prompt
-    // 6 columns in breaks data row Who/Where/From/To/Change-link/Delete-link
-    val ROWSINHEADER = 1
-    val ROWSPERBREAK = 2
-    val COLSINBREAKROW = 6
-
     "navigate to correct 2nd break hospital page from 3 hospital breaks when click change" in new WithJsBrowser with PageObjects {
       val numBreaks = 3
-      val targetBreak=2
+      val targetBreak = 2
 
       GBreaksInCareHospitalPage.fillDetails(context, testData => {
         testData.AboutTheCareYouProvideBreakWhenWereYouAdmitted_1 = "01/01/2009"
@@ -97,7 +93,7 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
       val summaryTableRows = browser.find("#summary-table tr")
       summaryTableRows.size() mustEqual ROWSINHEADER + numBreaks * ROWSPERBREAK
       // table rows .... 0=header 1/2=break1 3/4=break2 where 3 is the data shown and 4 is the confirm delete yes/no
-      val breakDataRow = browser.find("#summary-table tr", ROWSINHEADER+(targetBreak-1)*2)
+      val breakDataRow = browser.find("#summary-table tr", ROWSINHEADER + (targetBreak - 1) * 2)
       breakDataRow.find("td").size() mustEqual COLSINBREAKROW
       // table cols ... 0=who 1=where 2=from 3=to 4=change 5=delete
       val changeLink = breakDataRow.find("td", 4).find("input")
@@ -111,7 +107,7 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
 
     "navigate to correct 2nd break respite page when click change" in new WithJsBrowser with PageObjects {
       val numBreaks = 3
-      val targetBreak=2
+      val targetBreak = 2
 
       GBreaksInCareHospitalPage.fillDetails(context, testData => {
         testData.AboutTheCareYouProvideBreakWhenWereYouAdmitted_1 = "01/01/2000"
@@ -128,7 +124,7 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
       val summaryTableRows = browser.find("#summary-table tr")
       summaryTableRows.size() mustEqual ROWSINHEADER + numBreaks * ROWSPERBREAK
       // table rows .... 0=header 1/2=break1 3/4=break2 where 3 is the data shown and 4 is the confirm delete yes/no
-      val breakDataRow = browser.find("#summary-table tr", ROWSINHEADER+(targetBreak-1)*2)
+      val breakDataRow = browser.find("#summary-table tr", ROWSINHEADER + (targetBreak - 1) * 2)
       breakDataRow.find("td").size() mustEqual COLSINBREAKROW
       // table cols ... 0=who 1=where 2=from 3=to 4=change 5=delete
       val changeLink = breakDataRow.find("td", 4).find("input")
@@ -140,11 +136,36 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
       hospitalAdmittedYear mustEqual "2001"
     }
 
-    /*
-        "navigate to other page when click change" in new WithJsBrowser with PageObjects {
+    "navigate to other page which is 3rd is breaks list when click change" in new WithJsBrowser with PageObjects {
+      val numBreaks = 3
+      val targetBreak = 3
 
-        }
-    */
+      GBreaksInCareHospitalPage.fillDetails(context, testData => {
+        testData.AboutTheCareYouProvideBreakWhenWereYouAdmitted_1 = "01/01/2003"
+      })
+      GBreaksInCareRespitePage.fillDetails(context, testData => {
+        testData.AboutTheCareYouProvideBreakWhenWereYouAdmitted_1 = "01/01/2004"
+      })
+      GBreaksInCareOtherPage.fillDetails(context, testData => {
+        testData.AboutTheCareYouProvideBreakEndDate_1 = "01/01/2005"
+      })
+      val page = GBreaksInCareSummaryPage(context)
+      page goToThePage()
+
+      val summaryTableRows = browser.find("#summary-table tr")
+      summaryTableRows.size() mustEqual ROWSINHEADER + numBreaks * ROWSPERBREAK
+      // table rows .... 0=header 1/2=break1 3/4=break2 where 3 is the data shown and 4 is the confirm delete yes/no
+      val breakDataRow = browser.find("#summary-table tr", ROWSINHEADER + (targetBreak - 1) * 2)
+      breakDataRow.find("td").size() mustEqual COLSINBREAKROW
+      // table cols ... 0=who 1=where 2=from 3=to 4=change 5=delete
+      val changeLink = breakDataRow.find("td", 4).find("input")
+      changeLink.getAttribute("value") mustEqual ("Change")
+      changeLink.click()
+
+      browser.url must contain(GBreaksInCareOtherPage.url)
+      val hospitalAdmittedYear = browser.find("#caringEnded_date_year").getValue
+      hospitalAdmittedYear mustEqual "2005"
+    }
 
     "delete hospital 2nd break from 3 breaks when click delete and confirm yes" in new WithJsBrowser with PageObjects {
       val numBreaks = 3
@@ -237,8 +258,26 @@ class GBreaksInCareSummaryNavigationIntegrationSpec extends Specification {
       fromDate2 mustEqual "01/01/2006"
     }
 
-    "delete other break when click delete" in new WithJsBrowser with PageObjects {
+    "delete other break from single break when click delete" in new WithJsBrowser with PageObjects {
+      val numBreaks = 1
+      val breakToDelete = 1
 
+      GBreaksInCareOtherPage.fillDetails(context, testData => {})
+
+      val page = GBreaksInCareSummaryPage(context)
+      page goToThePage()
+
+      browser.find("#summary-table tr").size() mustEqual ROWSINHEADER + numBreaks * ROWSPERBREAK
+      val deleteLink = browser.find("#summary-table tr", ROWSINHEADER + (breakToDelete - 1) * 2).find("td", 5).find("input")
+      val confirmYesLink = browser.find("#summary-table tr", ROWSINHEADER + (breakToDelete - 1) * 2 + 1).find("td", 0).find("input", 1)
+      confirmYesLink.isDisplayed mustEqual (false)
+      deleteLink.click()
+      confirmYesLink.isDisplayed mustEqual (true)
+      confirmYesLink.click()
+
+      // Check that returned to summary page with no summary table
+      browser.url must contain(GBreaksInCareSummaryPage.url)
+      browser.find("#summary-table").size() mustEqual 0
     }
 
     "clicking delete break then selecting no does not delete break" in new WithJsBrowser with PageObjects {
