8fdcf0ea389cb1734e9c0e264de0983003249b61
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-08-12 10:30:30 +0100

    CADS-1153 : BreaksInCare - Remove old breaks in care.

--- c3/app/controllers/PreviewRouteUtils.scala
+++ c3/app/controllers/PreviewRouteUtils.scala
@@ -72,16 +72,6 @@ object PreviewRouteUtils {
     routesMap
   }
 
-  def oldBreaks = {
-    val gBreaksInCareRoute = controllers.s_breaks.routes.GOldBreaksInCare.present.toString
-
-    val breaksInCareList = Seq("care_you_provide_anyBreaks")
-
-    val routesMap = Map(breaksInCareList map{id => (id,gBreaksInCareRoute)}: _*)
-
-    routesMap
-  }
-
   def breaksRoute = {
     val route = controllers.breaks_in_care.routes.GBreaksInCareSummary.present.toString
     val list = Seq("breaks_hospital", "breaks_carehome", "breaks_breaktype_other", "breaks_breaktype")
