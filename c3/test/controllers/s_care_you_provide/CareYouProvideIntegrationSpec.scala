7cdc2af7185a91820baba6a5f8b7d22ac8069c37
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-08-12 14:08:49 +0100

    CADS-1153 : BreaksInCare - Remove old breaks in care.

--- c3/test/controllers/s_care_you_provide/CareYouProvideIntegrationSpec.scala
+++ c3/test/controllers/s_care_you_provide/CareYouProvideIntegrationSpec.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable._
 import utils.WithBrowser
 import controllers.ClaimScenarioFactory
 import utils.pageobjects.PageObjects
-import utils.pageobjects.s_breaks.GBreaksInCarePage
+import utils.pageobjects.breaks_in_care.GBreaksInCareSummaryPage
 import utils.pageobjects.s_care_you_provide.GTheirPersonalDetailsPage
 import utils.pageobjects.s_education.GYourCourseDetailsPage
 
@@ -20,7 +20,7 @@ class CareYouProvideIntegrationSpec extends Specification {
       val claim = ClaimScenarioFactory.s4CareYouProvide(true,true)
       val page = GTheirPersonalDetailsPage(context)
       page goToThePage()
-      val breakPage = page runClaimWith(claim,GBreaksInCarePage.url)
+      val breakPage = page runClaimWith(claim,GBreaksInCareSummaryPage.url)
       breakPage.fillYesNo("#answer","no")
       breakPage.submitPage().url mustEqual GYourCourseDetailsPage.url
     }
