23f219ed4770537523a741c3f8cb9136d106a71c
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-08-17 15:16:14 +0100

    Up version to 3.17

--- c3/project/Build.scala
+++ c3/project/Build.scala
@@ -11,7 +11,7 @@ import com.typesafe.sbt.packager.SettingsHelper._
 
 object ApplicationBuild extends Build {
   val appName = "c3"
-  val appVersion = "3.16-SNAPSHOT"
+  val appVersion = "3.17-SNAPSHOT"
 
   processConfFiles(Seq("conf/application-info.conf"), Seq("application.version" -> appVersion, "application.name" -> appName))
 
