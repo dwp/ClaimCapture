b5586b9c4d8a82d9f745130126ab4d22dcc40f65
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-09-23 18:41:46 +0100

    CADS-1413 : ( and CADS-1414 ) Workflow assistance over65 check comes after none-show-table checks and lived-in-uk more than 3 years produces none-show-table decision. Differentiate none-show-table from no-decision-sofar.

--- c3/test/xml/AssistedDecisionAgeChecksSpec.scala
+++ c3/test/xml/AssistedDecisionAgeChecksSpec.scala
@@ -1,7 +1,7 @@
 package xml
 
 import controllers.mappings.Mappings
-import models.DayMonthYear
+import models.{SortCode, DayMonthYear}
 import models.domain._
 import models.view.CachedClaim
 import models.yesNo._
@@ -12,20 +12,24 @@ import xml.claim.AssistedDecision
 import org.specs2.mock.Mockito
 
 class AssistedDecisionAgeChecksSpec extends Specification with Mockito {
-  // Do not reformat this xml it breaks the tests ... !!!
-  val emptyAssistedDecisionNode = <AssistedDecision><Reason>None</Reason><RecommendedDecision>None,show table</RecommendedDecision></AssistedDecision>
-
   lazy val moreAboutTheCare = MoreAboutTheCare(Some(Mappings.yes))
   lazy val paymentsFromAbroad = PaymentsFromAbroad(guardQuestion = YesNoWith2MandatoryFieldsOnYes(answer = Mappings.no, field1 = Some(YesNoWith1MandatoryFieldOnYes(answer = Mappings.no)), field2 = Some(YesNoWith1MandatoryFieldOnYes(answer = Mappings.no))))
   lazy val benefits = Benefits(benefitsAnswer = Benefits.pip)
   lazy val yourCourseDetails = YourCourseDetails(beenInEducationSinceClaimDate = Mappings.no)
-  lazy val happyClaim = Claim(CachedClaim.key).update(moreAboutTheCare).update(paymentsFromAbroad).update(benefits).update(yourCourseDetails)
+  lazy val someBankDetails=HowWePayYou(likeToBePaid = Mappings.yes,
+    bankDetails=Some(BankBuildingSocietyDetails(accountHolderName="Smith", bankFullName="HSBC", sortCode=SortCode("20","56","57"), accountNumber="12341234")),
+    paymentFrequency="Weekly")
+  lazy val noBankDetails=HowWePayYou(likeToBePaid = Mappings.no)
+  lazy val happyClaim = Claim(CachedClaim.key).update(moreAboutTheCare).update(paymentsFromAbroad).update(benefits).update(yourCourseDetails).update(someBankDetails)
 
   lazy val submitDate = DateTime.now()
   lazy val fifteenAndNineInMonths = 15 * 12 + 9
   lazy val claimDateDetails = ClaimDate(DateTime.now)
 
-  lazy val defaultAssistedDecisionReason = "Check CIS for benefits"
+  // Do not reformat this xml it breaks the tests ... !!!
+  lazy val showTableDecisionNode = <AssistedDecision><Reason>None</Reason><RecommendedDecision>None,show table</RecommendedDecision></AssistedDecision>
+  lazy val defaultCheckCISDecisionNode = <AssistedDecision><Reason>Check CIS for benefits. Send Pro517 if relevant.</Reason><RecommendedDecision>Potential award,show table</RecommendedDecision></AssistedDecision>
+  lazy val pensionerCheckCISDecisionNode = <AssistedDecision><Reason>Check CIS for benefits. Send Pro517 if relevant.</Reason><RecommendedDecision>Potential underlying entitlement</RecommendedDecision></AssistedDecision>
 
   section("unit")
   "Assisted section" should {
@@ -33,10 +37,10 @@ class AssistedDecisionAgeChecksSpec extends Specification with Mockito {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusMonths(fifteenAndNineInMonths))
       val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain(defaultAssistedDecisionReason)
+      (xml \\ "AssistedDecision") (0) mustEqual(defaultCheckCISDecisionNode)
     }
 
-    "Create AD if customer LESS THAN 15 and 9 year old today i.e. born 1 day later" in new WithApplication {
+    "Create 'disallowance' AD if customer LESS THAN 15 and 9 year old today i.e. born 1 day later" in new WithApplication {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusMonths(fifteenAndNineInMonths).plusDays(1))
       val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
       val xml = AssistedDecision.xml(claim)
@@ -48,48 +52,60 @@ class AssistedDecisionAgeChecksSpec extends Specification with Mockito {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusMonths(fifteenAndNineInMonths).minusDays(1))
       val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain(defaultAssistedDecisionReason)
+      (xml \\ "AssistedDecision") (0) mustEqual(defaultCheckCISDecisionNode)
     }
 
-    "Create AD if customer EQUALS 65 claim date=today" in new WithApplication {
+    "Create 'show table' AD if no bank details provided" in new WithApplication {
+      val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(30))
+      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails).update(noBankDetails));
+      val xml = AssistedDecision.xml(claim)
+      (xml \\ "Reason").text mustEqual("None")
+      (xml \\ "RecommendedDecision").text must contain("show table")
+    }
+
+    "Create 'underlying entitlement' AD if customer EQUALS 65 claim date=today" in new WithApplication {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(65))
-      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
+      val howWePayYou=HowWePayYou(likeToBePaid = Mappings.no)
+      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails).update(noBankDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain("Check CIS for benefits. Send Pro517 if relevant.")
-      (xml \\ "RecommendedDecision").text must contain("Potential underlying entitlement")
+      (xml \\ "AssistedDecision") (0) mustEqual(pensionerCheckCISDecisionNode)
     }
 
-    "Default AD if customer 65 tomorrow and claim date=today" in new WithApplication {
+    "Default AD if customer 65 tomorrow and claim date=today with bank details" in new WithApplication {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(65).plusDays(1))
-      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
+      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails).update(someBankDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain(defaultAssistedDecisionReason)
+      (xml \\ "AssistedDecision") (0) mustEqual(defaultCheckCISDecisionNode)
     }
 
-    "Create AD if customer 65 yesterday claim date=today" in new WithApplication {
+    "Create 'show table' AD if customer 65 tomorrow and claim date=today without bank details" in new WithApplication {
+      val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(65).plusDays(1))
+      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails).update(noBankDetails));
+      val xml = AssistedDecision.xml(claim)
+      (xml \\ "AssistedDecision") (0) mustEqual(showTableDecisionNode)
+    }
+
+    "Create 'underlying entitlement' AD if customer 65 yesterday claim date=today" in new WithApplication {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(65).minusDays(1))
-      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
+      val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails).update(noBankDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain("Check CIS for benefits. Send Pro517 if relevant.")
-      (xml \\ "RecommendedDecision").text must contain("Potential underlying entitlement")
+      (xml \\ "AssistedDecision") (0) mustEqual(pensionerCheckCISDecisionNode)
     }
 
-    "Create AD if customer is 65 tomorrow but OLDER when claim date is next week" in new WithApplication {
+    "Create 'underlying entitlement' AD if customer is 65 tomorrow but OLDER when claim date is next week" in new WithApplication {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(65).plusDays(1))
       val claimDateDetails = ClaimDate(DateTime.now.plusDays(7))
       val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain("Check CIS for benefits. Send Pro517 if relevant.")
-      (xml \\ "RecommendedDecision").text must contain("Potential underlying entitlement")
+      (xml \\ "AssistedDecision") (0) mustEqual(pensionerCheckCISDecisionNode)
     }
 
     "Happy path Default AD if customer is in the middle say 30 years old" in new WithApplication {
       val yourDetails = YourDetails(dateOfBirth = submitDate.minusYears(30))
       val claim = AssistedDecision.createAssistedDecisionDetails(happyClaim.update(claimDateDetails).update(yourDetails));
       val xml = AssistedDecision.xml(claim)
-      (xml \\ "Reason").text must contain(defaultAssistedDecisionReason)
+      (xml \\ "AssistedDecision") (0) mustEqual(defaultCheckCISDecisionNode)
     }
-}
-
-section ("unit")
+  }
+  section ("unit")
 }
